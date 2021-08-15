ff1bd39309d0bd53f5dd3f03bb63cb7face9508a
Denis Kuznetosv <kuznetsov.den.p@gmail.com>
2021-08-16 01:57:22 +0300

    feat/contrib: split by models

--- dff/condition_handlers.py
+++ dff/condition_handlers.py
@@ -1,11 +1,14 @@
-from typing import ForwardRef
+# from typing import ForwardRef
 from typing import Callable
 
+from pydantic import validate_arguments, BaseModel
 
-from dff.core.context import Context
+from .core.context import Context
 
 
-Actor = ForwardRef("Actor")
+Actor = BaseModel  # ForwardRef("Actor")
 
+
+@validate_arguments()
 def deep_copy_condition_handler(condition: Callable, ctx: Context, actor: Actor, *args, **kwargs):
     return condition(ctx.copy(deep=True), actor.copy(deep=True), *args, **kwargs)
--- dff/core/__init__.py
+++ dff/core/__init__.py
@@ -1,3 +1,4 @@
 # flake8: noqa: F401
 from .context import Context
-from .actor import Actor, Flows, Flow, Node, Transition
+from .actor import Actor
+from .flows import Flows, Flow, Node, Transition
