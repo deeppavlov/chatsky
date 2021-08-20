b039d368629b994488d4c66b1dd20d542b1c2885
Denis Kuznetosv <kuznetsov.den.p@gmail.com>
2021-08-20 04:30:21 +0300

    feat/contrib: upd setup

--- setup.py
+++ setup.py
@@ -1,14 +1,8 @@
-from setuptools import setup, find_packages
 import pathlib
+import setuptools
+
 
 LOCATION = pathlib.Path(__file__).parent.resolve()
-
-# Get the long description from the README file
-readme_file = LOCATION / "README.md"
-
-readme_lines = [line.strip() for line in readme_file.open(encoding="utf-8").readlines()]
-description = [line for line in readme_lines if line and not line.startswith("#")][0]
-long_description = "\n".join(readme_lines)
 
 
 def read_requirements():
@@ -26,42 +20,94 @@ def read_requirements():
     return {"install_requires": names, "dependency_links": links}
 
 
-setup(
+readme_file = LOCATION / "README.md"
+long_description = readme_file.open(encoding="utf8").read()
+
+setuptools.setup(
     name="dff",
     version="0.1.a1",
-    description=description,
+    scripts=[],
+    author="Denis Kuznetsov",
+    author_email="kuznetsov.den.p@gmail.com",
+    description="Library for creating state-machine-based chatbots.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/deepmipt/dialog_flow_framework",
-    author="Denis Kuznetsov",
-    author_email="kuznetsov.den.p@gmail.com",
-    classifiers=[  # Optional
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "Topic :: Software Development :: Build Tools",
+    packages=setuptools.find_packages(exclude=("docs", "examples", "tests")),
+    classifiers=[
+        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3",
-        # "Programming Language :: Python :: 3.6",
-        # "Programming Language :: Python :: 3.7",
-        # "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3 :: Only",
     ],
-    keywords="chatbots",  # Optional
-    package_dir={"": "dff"},  # Optional
-    packages=find_packages(where="dff"),  # Required
-    python_requires=">=3.9, <4",
-    install_requires=["pydantic==1.8.2"],  # Optional
-    # extras_require={"dev": ["check-manifest"], "test": ["coverage"]}, # Optional
-    # package_data={"sample": ["package_data.dat"]}, # Optional
-    # data_files=[("my_data", ["data/data_file"])],  # Optional
-    #
-    # For example, the following would provide a command called `sample` which
-    # executes the function `main` from this package when invoked:
-    # entry_points={"console_scripts": ["sample=sample:main"]},  # Optional
-    # project_urls={},  # Optional
+    package_data={},
+    include_package_data=True,
+    python_requires=">=3.9",
+    **read_requirements()
 )
+# from setuptools import setup, find_packages
+# import pathlib
+
+# LOCATION = pathlib.Path(__file__).parent.resolve()
+
+# # Get the long description from the README file
+# readme_file = LOCATION / "README.md"
+
+# readme_lines = [line.strip() for line in readme_file.open(encoding="utf-8").readlines()]
+# description = [line for line in readme_lines if line and not line.startswith("#")][0]
+# long_description = "\n".join(readme_lines)
+
+
+# def read_requirements():
+#     """parses requirements from requirements.txt"""
+#     reqs_file = LOCATION / "requirements.txt"
+#     reqs = [line.strip() for line in reqs_file.open(encoding="utf8").readlines() if not line.strip().startswith("#")]
+
+#     names = []
+#     links = []
+#     for req in reqs:
+#         if "://" in req:
+#             links.append(req)
+#         else:
+#             names.append(req)
+#     return {"install_requires": names, "dependency_links": links}
+
+
+# setup(
+#     name="dff",
+#     version="0.1.a1",
+#     description=description,
+#     long_description=long_description,
+#     long_description_content_type="text/markdown",
+#     url="https://github.com/deepmipt/dialog_flow_framework",
+#     author="Denis Kuznetsov",
+#     author_email="kuznetsov.den.p@gmail.com",
+#     classifiers=[  # Optional
+#         #   3 - Alpha
+#         #   4 - Beta
+#         #   5 - Production/Stable
+#         "Development Status :: 3 - Alpha",
+#         "Intended Audience :: Developers",
+#         "Topic :: Software Development :: Build Tools",
+#         "License :: OSI Approved :: Apache Software License",
+#         "Operating System :: OS Independent",
+#         "Programming Language :: Python :: 3",
+#         # "Programming Language :: Python :: 3.6",
+#         # "Programming Language :: Python :: 3.7",
+#         # "Programming Language :: Python :: 3.8",
+#         "Programming Language :: Python :: 3.9",
+#         "Programming Language :: Python :: 3 :: Only",
+#     ],
+#     keywords="chatbots",  # Optional
+#     package_dir={"": "dff"},  # Optional
+#     packages=find_packages(where="dff"),  # Required
+#     python_requires=">=3.9, <4",
+#     install_requires=["pydantic==1.8.2"],  # Optional
+#     # extras_require={"dev": ["check-manifest"], "test": ["coverage"]}, # Optional
+#     # package_data={"sample": ["package_data.dat"]}, # Optional
+#     # data_files=[("my_data", ["data/data_file"])],  # Optional
+#     #
+#     # For example, the following would provide a command called `sample` which
+#     # executes the function `main` from this package when invoked:
+#     # entry_points={"console_scripts": ["sample=sample:main"]},  # Optional
+#     # project_urls={},  # Optional
+# )
