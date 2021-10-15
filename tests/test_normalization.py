76bac2c68f8326c4d129e2283e28388a1a0f3801
Denis Kuznetosv <kuznetsov.den.p@gmail.com>
2021-10-16 00:03:14 +0300

    feat/new_dsl/upd_arch: refactor lables

--- tests/test_normalization.py
+++ tests/test_normalization.py
@@ -2,6 +2,10 @@
 from typing import Callable
 
 from dff.core.keywords import GLOBAL, TRANSITIONS, RESPONSE, PROCESSING, MISC
+from dff.core import Actor, Context
+from dff.core.types import NodeLabel3Type
+from dff.labels import repeat
+from dff.conditions import true
 
 # from dff.labels
 from dff.core.normalization import (
@@ -18,16 +22,30 @@ def std_func(ctx, actor, *args, **kwargs):
     pass
 
 
-# def create_env():
-#     plot = {"flow": {"node1": {TRANSITIONS: {repeat(): true()}, RESPONSE: "response"}}}
-#     actor = Actor(plot=plot, start_label=("flow", "node1"), fallback_label=("flow", "node1"))
-#     ctx.add_request("text")
-#     ctx = actor(ctx)
+def create_env():
+    ctx = Context()
+    plot = {"flow": {"node1": {TRANSITIONS: {repeat(): true()}, RESPONSE: "response"}}}
+    actor = Actor(plot=plot, start_label=("flow", "node1"), fallback_label=("flow", "node1"))
+    ctx.add_request("text")
+    return ctx, actor
 
 
 def test_normalize_label():
     # TODO: Add full check for functions
-    assert isinstance(normalize_label(std_func), Callable)
+    ctx, actor = create_env()
+
+    def true_label_func(ctx: Context, actor: Actor, *args, **kwargs) -> NodeLabel3Type:
+        return ("flow", "node1", 1)
+
+    def false_label_func(ctx: Context, actor: Actor, *args, **kwargs) -> NodeLabel3Type:
+        return ("flow", "node2", 1)
+
+    n_l = normalize_label(true_label_func)
+    assert isinstance(n_l, Callable)
+    assert n_l(ctx, actor) == ("flow", "node1", 1)
+    n_l = normalize_label(false_label_func)
+    assert n_l(ctx, actor) is None
+
     assert normalize_label("node", "flow") == ("flow", "node", float("-inf"))
     assert normalize_label(("flow", "node"), "flow") == ("flow", "node", float("-inf"))
     assert normalize_label(("flow", "node", 1.0), "flow") == ("flow", "node", 1.0)
