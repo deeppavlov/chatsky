c9c27f044c9a0bee60342323b89df9d4d05fb7c6
Denis Kuznetosv <kuznetsov.den.p@gmail.com>
2021-11-18 01:38:13 +0300

    dev: upd version

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ def read_requirements():
 
 setup(
     name="dff",
-    version="0.2.b1",
+    version="0.2b2",
     description=description,
     long_description=long_description,
     long_description_content_type="text/markdown",
