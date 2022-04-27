1c1db97573fc11fb31f2af6a5a3ceea6d11df30c
Denis Kuznetosv <kuznetsov.den.p@gmail.com>
2022-04-27 14:01:24 +0300

    dev: upd doc version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = "2021, Denis Kuznetsov"
 author = "Denis Kuznetsov"
 
 # The full version, including alpha/beta/rc tags
-release = "0.8.1"
+release = "0.9.0"
 
 
 # -- General configuration ---------------------------------------------------
