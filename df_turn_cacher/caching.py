da7b03e6b6fde90b03d45666e7392bb50488e4c4
Denis Kuznetsov <kuznetsov.den.p@gmail.com>
2022-07-09 13:23:31 +0300

    dev: add cacing

---
+++ df_turn_cacher/caching.py
@@ -0,0 +1,47 @@
+"""
+Caching
+---------------------------
+"""
+import functools
+
+from df_engine.core import Actor, Context
+from df_engine.core.types import ActorStage
+
+
+class OneTurnCache:
+    """
+    Class that caches the information from the last turn.
+    """
+
+    def __init__(self):
+        self.wrappers = []
+
+    def update_actor_handlers(self, actor: Actor) -> Actor:
+        handlers = actor.handlers.get(ActorStage.CONTEXT_INIT, [])
+        handlers += [self.clear_cache_handler]
+        actor.handlers[ActorStage.CONTEXT_INIT] = handlers
+        return actor
+
+    def clear_cache_handler(self, ctx: Context, actor: Actor, *args, **kwargs):
+        [wrapper.cache_clear() for wrapper in self.wrappers]
+
+    def cache(self, func):
+        @functools.cache
+        def wrapper(*args, **kwargs):
+            return func(*args, **kwargs)
+
+        self.wrappers += [wrapper]
+        return wrapper
+
+    def lru_cache(self, maxsize=128, typed=False):
+        _maxsize = 128 if callable(maxsize) else maxsize
+
+        def decorator(func):
+            @functools.lru_cache(maxsize=_maxsize, typed=typed)
+            def wrapper(*args, **kwargs):
+                return func(*args, **kwargs)
+
+            self.wrappers += [wrapper]
+            return wrapper
+
+        return decorator(maxsize) if callable(maxsize) else decorator
--- tests/test_df_turn_cacher.py
+++ tests/test_df_turn_cacher.py
@@ -1,5 +1,55 @@
-from df_turn_cacher import __version__
+from df_engine.core import Context, Actor
+from df_engine.labels import repeat
+from df_engine.conditions import true
+from df_engine.core.keywords import RESPONSE, TRANSITIONS
+from df_turn_cacher.caching import OneTurnCache
 
 
-def test_version():
-    assert __version__ == "0.1.0"
+def cache_test(cached_response, cache):
+
+    ctx = Context()
+    ctx.add_label(["flow", "node1"])
+
+    def response(ctx: Context, actor: Actor, *args, **kwargs):
+        if ctx.validation:
+            return ""
+        return f"{cached_response(1)}-{cached_response(1)}-{cached_response(1)}-{cached_response(2)}"
+
+    plot = {"flow": {"node1": {TRANSITIONS: {repeat(): true()}, RESPONSE: response}}}
+    actor = Actor(plot=plot, start_label=("flow", "node1"), fallback_label=("flow", "node1"))
+    ctx.add_request("text")
+    ctx = actor(ctx)
+    assert ctx.last_response == "1-1-1-2"
+    ctx.add_request("text")
+    ctx = actor(ctx)
+    assert ctx.last_response == "1-1-1-2"
+    actor = cache.update_actor_handlers(actor)
+    ctx.add_request("text")
+    ctx = actor(ctx)
+    assert ctx.last_response == "3-3-3-4"
+    ctx.add_request("text")
+    ctx = actor(ctx)
+    assert ctx.last_response == "5-5-5-6"
+
+
+def test_caching():
+    cache = OneTurnCache()
+
+    external_data = {"counter": 0}
+
+    @cache.cache
+    def cached_response(arg):
+        external_data["counter"] += 1
+        return external_data["counter"]
+
+    cache_test(cached_response, cache)
+    cache = OneTurnCache()
+
+    external_data = {"counter": 0}
+
+    @cache.lru_cache(maxsize=32)
+    def cached_response(arg):
+        external_data["counter"] += 1
+        return external_data["counter"]
+
+    cache_test(cached_response, cache)
