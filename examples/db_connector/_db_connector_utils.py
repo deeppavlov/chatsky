27546e5b666a124063423600dfa2b03f21d3f8b4
pseusys <shveitsar215@gmail.com>
2022-10-24 17:43:24 +0300

    db connector utils file test_dialog updated

--- examples/db_connector/_db_connector_utils.py
+++ examples/db_connector/_db_connector_utils.py
@@ -42,15 +42,15 @@ script = {
 }
 
 testing_dialog = [
-    ("Hi", "Hi, how are you?"),  # start_node -> node1
-    ("i'm fine, how are you?", "Good. What do you want to talk about?"),  # node1 -> node2
+    ("Hi", "Hi, what is up?"),  # start_node -> node1
+    ("alright", "Good. What do you want to talk about?"),  # node1 -> node2
     ("Let's talk about music.", "Sorry, I can not talk about music now."),  # node2 -> node3
     ("Ok, goodbye.", "bye"),  # node3 -> node4
-    ("Hi", "Hi, how are you?"),  # node4 -> node1
+    ("Hi", "Hi, what is up?"),  # node4 -> node1
     ("stop", "Ooops"),  # node1 -> fallback_node
     ("stop", "Ooops"),  # fallback_node -> fallback_node
-    ("Hi", "Hi, how are you?"),  # fallback_node -> node1
-    ("i'm fine, how are you?", "Good. What do you want to talk about?"),  # node1 -> node2
+    ("Hi", "Hi, what is up?"),  # fallback_node -> node1
+    ("alright", "Good. What do you want to talk about?"),  # node1 -> node2
     ("Let's talk about music.", "Sorry, I can not talk about music now."),  # node2 -> node3
     ("Ok, goodbye.", "bye"),  # node3 -> node4
 ]
