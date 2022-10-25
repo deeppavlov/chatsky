3cd1fce7f3ee0cb31e096261381970b121bc704f
ruthenian8 <ruthenian8@gmail.com>
2022-10-26 00:19:49 +0300

    explicit imports

--- tests/generics/test_df_generic_response.py
+++ tests/generics/test_df_generic_response.py
@@ -9,11 +9,13 @@ from pydantic import BaseModel
 sys.path.insert(0, str(pathlib.Path(__file__).parent.parent.parent / "examples"))
 
 from generics.example_utils import run_test
+from generics import media
+from generics import basics
+from generics import buttons
 
 
-@pytest.mark.parametrize("module_name", ["generics.basics", "generics.buttons", "generics.media"])
-def test_examples(module_name):
-    module = importlib.import_module(f"{module_name}")
+@pytest.mark.parametrize(["module"], [(basics, ), (buttons, ), (media, )])
+def test_examples(module):
     actor = getattr(module, "actor")
     testing_dialog = getattr(module, "testing_dialog")
     run_test(testing_dialog=testing_dialog, actor=actor)
