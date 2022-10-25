065511ed02942c65d113a52f167e80d9551b71ae
ruthenian8 <ruthenian8@gmail.com>
2022-10-25 20:55:47 +0300

    insert path

--- tests/generics/test_df_generic_response.py
+++ tests/generics/test_df_generic_response.py
@@ -6,12 +6,14 @@ import pytest
 from pydantic import BaseModel
 
 
-from .examples.example_utils import run_test
+sys.path.insert(0, str(pathlib.Path(__file__).parent.parent.parent / "examples"))
+
+from generics.example_utils import run_test
 
 
-@pytest.mark.parametrize("module_name", ["examples.basics", "examples.buttons", "examples.media"])
+@pytest.mark.parametrize("module_name", ["generics.basics", "generics.buttons", "generics.media"])
 def test_examples(module_name):
-    module = importlib.import_module(f"tests.generics.{module_name}")
+    module = importlib.import_module(f"{module_name}")
     actor = getattr(module, "actor")
     testing_dialog = getattr(module, "testing_dialog")
     run_test(testing_dialog=testing_dialog, actor=actor)
