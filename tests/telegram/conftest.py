9d2f5946c5f3c12de9b6be58aeb881fb287aab2a
Roman Zlobin <trlkrot@gmail.com>
2022-10-26 14:50:48 +0300

    merge/telegram: remove os.environ check
    It doesn't affect anything since examples are not executed during testing
    
    Need to add a check later that skips example tests if BOT_TOKEN is not set

--- tests/telegram/conftest.py
+++ tests/telegram/conftest.py
@@ -5,9 +5,9 @@ import pytest
 
 from .examples.no_runner.basic_bot import bot, actor
 
-for variable in ["BOT_TOKEN"]:
-    if variable not in os.environ:
-        raise AssertionError(f"{variable} variable needs to be set to continue")
+# for variable in ["BOT_TOKEN"]:
+#     if variable not in os.environ:
+#         raise AssertionError(f"{variable} variable needs to be set to continue")
 
 
 @pytest.fixture(scope="session")
