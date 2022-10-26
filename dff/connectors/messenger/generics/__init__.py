5bd60e0bcaeedf0ff05d81e338516a4ce1cbd3b9
Denis Kuznetsov <kuznetsov.den.p@gmail.com>
2022-10-26 22:06:01 +0300

    merge/generics: refact&fix style into dff/*/generins/*.py

--- dff/connectors/messenger/generics/__init__.py
+++ dff/connectors/messenger/generics/__init__.py
@@ -1,7 +1,19 @@
 # -*- coding: utf-8 -*-
+# flake8: noqa: F401
 
-__author__ = "Denis Kusnetsov"
-__email__ = "kuznetosv.den.p@gmail.com"
-__version__ = "0.1.0"
-
-from .response import *
+from .response import (
+    Session,
+    BaseModel,
+    Command,
+    Location,
+    Attachment,
+    Audio,
+    Video,
+    Image,
+    Document,
+    Attachments,
+    Link,
+    Button,
+    Keyboard,
+    Response,
+)
