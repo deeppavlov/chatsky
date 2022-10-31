d9c118a29b28bdba0680e17ea2cb658f2b709ed2
Roman Zlobin <trlkrot@gmail.com>
2022-10-31 12:49:41 +0300

    Update parser integration tests
    - Test parser tools using their cli
    - Move assert_dirs_equal outside pytest functions

--- tests/parser/test_integration.py
+++ tests/parser/test_integration.py
@@ -1,5 +1,5 @@
 """Test parser as a whole."""
-from io import StringIO
+import sys
 from pathlib import Path
 from filecmp import dircmp
 from copy import copy
@@ -8,9 +8,8 @@ from itertools import product
 import pytest
 
 from dff.script.import_export.parser.dumpers_loaders import yaml_dumper_loader
-from dff.script.import_export.parser.processors.recursive_parser import RecursiveParser
 from dff.script.import_export.parser.utils.exceptions import ScriptValidationError
-from dff.script.import_export.parser import dependencies, py2yaml, yaml2py, py2graph, graph2py
+from dff.script.import_export.parser import dependencies, py2yaml_cli, yaml2py_cli, py2graph_cli, graph2py_cli
 import dff
 
 true_dependencies = copy(dependencies)
@@ -19,17 +18,29 @@ true_dependencies = copy(dependencies)
 current_dir = Path(__file__).parent
 
 
+def assert_dirs_equal(dir1: Path, dir2: Path):
+    def _assert_dir_eq(dir_cmp: dircmp):
+        assert len(dir_cmp.left_only) == 0
+        assert len(dir_cmp.right_only) == 0
+        assert len(dir_cmp.diff_files) == 0
+        for subdir in dir_cmp.subdirs.values():
+            _assert_dir_eq(subdir)
+
+    _assert_dir_eq(dircmp(dir1, dir2))
+
+
 py2yaml_params = [
     *[
         (
             current_dir / "test_py2yaml" / "simple_tests" / f"test_{test_number}" / "python_files",
             current_dir / "test_py2yaml" / "simple_tests" / f"test_{test_number}" / "python_files" / "main.py",
-            current_dir / "test_py2yaml" / "simple_tests" / f"test_{test_number}" / "yaml_files" / "script.yaml",
+            current_dir / "test_py2yaml" / "simple_tests" / f"test_{test_number}" / "yaml_files",
             exception,
         )
         for test_number, exception in zip(
-            range(1, 19),
+            range(0, 19),
             [
+                SystemExit,
                 None,
                 ScriptValidationError,
                 ScriptValidationError,
@@ -55,7 +66,7 @@ py2yaml_params = [
         (
             current_dir / "test_py2yaml" / "complex_tests" / f"test_{test_number}" / "python_files",
             current_dir / "test_py2yaml" / "complex_tests" / f"test_{test_number}" / "python_files" / "main.py",
-            current_dir / "test_py2yaml" / "complex_tests" / f"test_{test_number}" / "yaml_files" / "script.yaml",
+            current_dir / "test_py2yaml" / "complex_tests" / f"test_{test_number}" / "yaml_files",
             exception,
         )
         for test_number, exception in zip(range(1, 4), [None, None, None])
@@ -64,29 +75,31 @@ py2yaml_params = [
 
 
 @pytest.mark.parametrize(
-    "project_root_dir,main_file,script,exception,nx_available",
+    "project_root_dir,main_file,output_dir,exception,nx_available",
     list((*x[0], x[1]) for x in product(py2yaml_params, [True, False]))  # add True or False to each param set
 )
-def test_py2yaml(project_root_dir, main_file, script, exception, nx_available):
+def test_py2yaml(project_root_dir, main_file, output_dir, exception, nx_available, tmp_path, monkeypatch):
     """Test the py2yaml part of the parser."""
     if not true_dependencies["graph"] and nx_available:
         pytest.skip("`networkx` is not installed")
-    def _test_py2yaml():
-        buffer = StringIO()
-        recursive_parser = RecursiveParser(Path(project_root_dir))
-        recursive_parser.parse_project_dir(Path(main_file))
-        yaml_dumper_loader.dump(recursive_parser.to_dict(), buffer)
-        buffer.seek(0)
-        with open(script, "r", encoding="utf-8") as correct_result:
-            assert buffer.read() == correct_result.read()
 
-    dff.script.import_export.parser.dependencies["graph"] = nx_available
+    monkeypatch.setattr(sys, "argv", [
+        "dff.py2yaml",
+        str(main_file),
+        str(project_root_dir),
+        str(tmp_path / "script.yaml"),
+    ])
+
+    def _test_py2yaml():
+        py2yaml_cli()
+        assert_dirs_equal(tmp_path, output_dir)
+
+    monkeypatch.setitem(dff.script.import_export.parser.dependencies, "graph", nx_available)
     if exception:
         with pytest.raises(exception):
             _test_py2yaml()
     else:
         _test_py2yaml()
-    dff.script.import_export.parser.dependencies["graph"] = true_dependencies["graph"]
 
 
 yaml2py_params = [
@@ -122,7 +135,7 @@ yaml2py_params = [
     "script,output_dir,exception",
     yaml2py_params,
 )
-def test_yaml2py(script, output_dir, exception, tmp_path):
+def test_yaml2py(script, output_dir, exception, tmp_path, monkeypatch):
     """Test yaml2py
 
     :param script: Yaml script to convert
@@ -131,22 +144,15 @@ def test_yaml2py(script, output_dir, exception, tmp_path):
     :param tmp_path: Temporary path to convert to
     :return:
     """
+    monkeypatch.setattr(sys, "argv", [
+        "dff.yaml2py",
+        str(script),
+        str(tmp_path),
+    ])
 
     def _test_yaml2py():
-        def _assert_dir_eq(dir_cmp: dircmp):
-            """Assert two dirs are equal
-
-            :param dir_cmp:
-            :return:
-            """
-            assert dir_cmp.left_only == []
-            assert dir_cmp.right_only == []
-            assert dir_cmp.diff_files == []
-            for subdir in dir_cmp.subdirs.values():
-                _assert_dir_eq(subdir)
-
-        yaml2py(Path(script), tmp_path)
-        _assert_dir_eq(dircmp(output_dir, tmp_path))
+        yaml2py_cli()
+        assert_dirs_equal(output_dir, tmp_path)
 
     if exception:
         with pytest.raises(exception):
@@ -173,22 +179,17 @@ py2graph_params = [
     py2graph_params,
 )
 @pytest.mark.skipif(not true_dependencies["graph"], reason="`networkx` is not installed")
-def test_py2graph(project_root_dir, main_file, output, exception, tmp_path):
+def test_py2graph(project_root_dir, main_file, output, exception, tmp_path, monkeypatch):
+    monkeypatch.setattr(sys, "argv", [
+        "dff.py2graph",
+        str(main_file),
+        str(project_root_dir),
+        str(tmp_path / "graph.json"),
+    ])
+
     def _test_yaml2py():
-        def _assert_dir_eq(dir_cmp: dircmp):
-            """Assert two dirs are equal
-
-            :param dir_cmp:
-            :return:
-            """
-            assert dir_cmp.left_only == []
-            assert dir_cmp.right_only == []
-            assert dir_cmp.diff_files == []
-            for subdir in dir_cmp.subdirs.values():
-                _assert_dir_eq(subdir)
-
-        py2graph(main_file, project_root_dir, tmp_path / "graph.json")
-        _assert_dir_eq(dircmp(output, tmp_path))
+        py2graph_cli()
+        assert_dirs_equal(output, tmp_path)
 
     if exception:
         with pytest.raises(exception):
@@ -214,7 +215,7 @@ graph2py_params = [
     graph2py_params,
 )
 @pytest.mark.skipif(not true_dependencies["graph"], reason="`networkx` is not installed")
-def test_graph2py(input_file, output_dir, exception, tmp_path):
+def test_graph2py(input_file, output_dir, exception, tmp_path, monkeypatch):
     """Test graph2py
 
     :param input_file: Graph file to convert
@@ -223,25 +224,20 @@ def test_graph2py(input_file, output_dir, exception, tmp_path):
     :param tmp_path: Temporary path to convert to
     :return:
     """
+    monkeypatch.setattr(sys, "argv", [
+        "dff.graph2py",
+        str(input_file),
+        str(tmp_path)
+    ])
 
     def _test_graph2py():
-        def _assert_dir_eq(dir_cmp: dircmp):
-            """Assert two dirs are equal
-
-            :param dir_cmp:
-            :return:
-            """
-            assert dir_cmp.left_only == []
-            assert dir_cmp.right_only == []
-            assert dir_cmp.diff_files == []
-            for subdir in dir_cmp.subdirs.values():
-                _assert_dir_eq(subdir)
-
-        graph2py(Path(input_file), tmp_path)
-        _assert_dir_eq(dircmp(output_dir, tmp_path))
+        graph2py_cli()
+        assert_dirs_equal(output_dir, tmp_path)
 
     if exception:
         with pytest.raises(exception):
             _test_graph2py()
     else:
         _test_graph2py()
+
+
