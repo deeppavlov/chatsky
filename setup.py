e680c53c4393bca1814aef75b09b61a6ca8b7938
Roman Zlobin <trlkrot@gmail.com>
2022-11-01 19:26:44 +0300

    Update setup.py
    - Add sql_dependencies (a lot of dbs use sqlalchemy so I decided to move it to a separate list)
    - Add asyncpg as postgres dependency (stats used it instead of psycopg)
    - Add an entry point for dff.stats
    
    Signed-off-by: Roman Zlobin <trlkrot@gmail.com>

--- setup.py
+++ setup.py
@@ -48,9 +48,11 @@ mypy_dependencies = [
     "mypy",
 ]
 
-sqlite_dependencies = [
-    "sqlalchemy>=1.4.27",
+sql_dependencies = [
+    "sqlalchemy==1.4.27",
 ]
+
+sqlite_dependencies = sql_dependencies
 
 redis_dependencies = [
     "redis>=4.1.2",
@@ -61,20 +63,39 @@ mongodb_dependencies = [
     "bson>=0.5.10",
 ]
 
-mysql_dependencies = [
-    "sqlalchemy>=1.4.27",
-    "pymysql>=1.0.2",
-    "cryptography>=36.0.2",
-]
+mysql_dependencies = merge_req_lists([
+    sql_dependencies,
+    [
+        "pymysql>=1.0.2",
+        "cryptography>=36.0.2",
+    ]
+])
 
-postgresql_dependencies = [
-    "sqlalchemy>=1.4.27",
-    "psycopg2-binary==2.9.4",  # TODO: change to >= when psycopg2 will be stabe for windows
-]
+postgresql_dependencies = merge_req_lists([
+    sql_dependencies,
+    [
+        "psycopg2-binary==2.9.4",  # TODO: change to >= when psycopg2 will be stable for windows
+        "asyncpg>=0.26.0",
+    ]
+])
 
 ydb_dependencies = [
     "ydb>=2.5.0",
 ]
+
+clickhouse_dependencies = [
+    "aiochclient>=2.2.0",
+    "httpx<=0.23.0",
+]
+
+stats_dependencies = merge_req_lists([
+    sql_dependencies,
+    [
+        "tqdm==4.62.3",
+        "omegaconf>=2.2.2",
+        "requests>=2.28.1",
+    ]
+])
 
 test_requirements = [
     "pytest >=6.2.4,<7.0.0",
@@ -103,6 +124,8 @@ full = merge_req_lists(
         mysql_dependencies,
         postgresql_dependencies,
         ydb_dependencies,
+        clickhouse_dependencies,
+        stats_dependencies,
     ]
 )
 
@@ -135,6 +158,8 @@ EXTRA_DEPENDENCIES = {
     "mysql": mysql_dependencies,
     "postgresql": postgresql_dependencies,
     "ydb": ydb_dependencies,
+    "clickhouse": clickhouse_dependencies,
+    "stats": stats_dependencies,
 }
 
 setup(
@@ -168,4 +193,9 @@ setup(
     install_requires=core,  # Optional
     test_suite="tests",
     extras_require=EXTRA_DEPENDENCIES,
+    entry_points={
+        "console_scripts": [
+            "dff.stats=dff.stats.__main__:main"
+        ]
+    }
 )
