4d70a8a7635da99d739b5073ebb93b12592f126c
Roman Zlobin <trlkrot@gmail.com>
2022-11-09 12:58:55 +0300

    test: partially fix huggingface tests
    Rename the second `testing_model` function to `hf_matcher`

--- tests/extended_conditions/test_hf.py
+++ tests/extended_conditions/test_hf.py
@@ -1,8 +1,7 @@
 import pytest
 
 try:
-    from tokenizers import AutoTokenizer
-    from transformers import AutoModelForSequenceClassification
+    from transformers import AutoModelForSequenceClassification, AutoTokenizer
     import torch
     import numpy as np
 except ImportError:
@@ -30,19 +29,19 @@ def testing_classifier(testing_model, testing_tokenizer):
 
 
 @pytest.fixture(scope="session")
-def testing_model(testing_model, testing_tokenizer):
+def hf_matcher(testing_model, testing_tokenizer):
     yield HFMatcher(
         model=testing_model, tokenizer=testing_tokenizer, device=torch.device("cpu"), namespace_key="HFmodel"
     )
 
 
-def test_saving(save_file: str, testing_classifier: HFClassifier, testing_model: HFMatcher):
+def test_saving(save_file: str, testing_classifier: HFClassifier, hf_matcher: HFMatcher):
     testing_classifier.save(path=save_file)
     testing_classifier = HFClassifier.load(save_file, namespace_key="HFclassifier")
     assert testing_classifier
-    testing_model.save(path=save_file)
-    testing_model = HFMatcher.load(save_file, namespace_key="HFmodel")
-    assert testing_model
+    hf_matcher.save(path=save_file)
+    hf_matcher = HFMatcher.load(save_file, namespace_key="HFmodel")
+    assert hf_matcher
 
 
 def test_predict(testing_classifier: HFClassifier):
@@ -51,8 +50,8 @@ def test_predict(testing_classifier: HFClassifier):
     assert isinstance(result, dict)
 
 
-def test_transform(testing_model: HFMatcher, testing_classifier: HFClassifier):
-    result_1 = testing_model.transform()
+def test_transform(hf_matcher: HFMatcher, testing_classifier: HFClassifier):
+    result_1 = hf_matcher.transform()
     assert isinstance(result_1, np.ndarray)
     result_2 = testing_classifier.transform()
     assert isinstance(result_2, np.ndarray)
