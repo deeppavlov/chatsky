de1dbd058b40058311c3f0c74eaa80fe46b4f3bb
Roman Zlobin <trlkrot@gmail.com>
2022-11-09 14:27:10 +0300

    Remove info from __init__.py

--- dff/script/logic/slots/__init__.py
+++ dff/script/logic/slots/__init__.py
@@ -7,7 +7,3 @@ from .forms import FormPolicy, FormState
 from . import conditions
 from . import response
 from . import processing
-
-__author__ = "Denis Kuznetsov"
-__email__ = "kuznetsov.den.p@gmail.com"
-__version__ = "0.1.0"
