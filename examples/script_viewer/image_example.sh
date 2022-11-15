2515e19a6903a7b06e3e7feb42b4142f3be5b65a
Roman Zlobin <trlkrot@gmail.com>
2022-11-15 14:09:43 +0300

    Fix examples

--- examples/script_viewer/image_example.sh
+++ examples/script_viewer/image_example.sh
@@ -1,6 +1,6 @@
 # Get a DFF project project plot as a static image:
-df_script_viewer.image --root ./examples/python_files/main.py --dir ./examples/python_files/ --output_file ./plot.png
+dff.script_viewer.image -rf ./python_files/main.py -d ./python_files/ -o ./plot.png
 
 # alternative:
-# df_script_viewer.image -rf ./examples/python_files/main.py -d ./examples/python_files/ -o ./plot.png
+# dff.script_viewer.image -rf ./python_files/main.py -d ./python_files/ -o ./plot.png
 
--- examples/script_viewer/server_example.sh
+++ examples/script_viewer/server_example.sh
@@ -1,5 +1,5 @@
 # View the project plot on a local Dash server:
-df_script_viewer.server --root ./examples/python_files/main.py --dir ./examples/python_files/
+dff.script_viewer.server -rf ./python_files/main.py -d ./python_files/
 
 # alternative
-# df_script_viewer.server -rf ./examples/python_files/main.py -d ./examples/python_files/
+# dff.script_viewer.server -rf ./python_files/main.py -d ./python_files/
