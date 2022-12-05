9c566bf64e6432fbe103cf09176d709fdd69f305
Denis Kuznetsov <kuznetsov.den.p@gmail.com>
2022-12-05 18:39:30 +0300

    dev: add current structure

---
+++ connectors/messenger/generics/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# flake8: noqa: F401
+
+from .response import (
+    Session,
+    BaseModel,
+    Command,
+    Location,
+    Attachment,
+    Audio,
+    Video,
+    Image,
+    Document,
+    Attachments,
+    Link,
+    Button,
+    Keyboard,
+    Response,
+)
