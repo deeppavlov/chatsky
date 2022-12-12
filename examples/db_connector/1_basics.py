3c539b54a0ee377f5166eeb453dfef2b49eeccaf
Kuznetsov Denis <kuznetsov.den.p@gmail.com>
2022-12-12 15:37:18 +0300

    Update examples/db_connector/1_basics.py
    Co-authored-by: Roman Zlobin <trlkrot@gmail.com>

--- examples/db_connector/1_basics.py
+++ examples/db_connector/1_basics.py
@@ -41,6 +41,6 @@ if __name__ == "__main__":
     # (testing) with HAPPY_PATH
 
     # This runs example in interactive mode if not in IPython env
-    # + if `DISABLE_INTERACTIVE_MODE` is not set
+    # and if `DISABLE_INTERACTIVE_MODE` is not set
     if is_interactive_mode():
         run_interactive_mode(pipeline)  # This runs example in interactive mode
