f7b93fe433400e800791f093e7d4522cea6e6c90
pseusys <shveitsar215@gmail.com>
2022-12-15 03:24:05 +0300

    test structure changed

--- tests/script/core/utils.py
+++
@@ -1,8 +0,0 @@
-def failing_test(func, args=None, kwargs=None, checker=lambda x: True, exception=Exception):
-    kwargs = dict() if kwargs is None else kwargs
-    args = list() if args is None else args
-    try:
-        checker(func(*args, **kwargs))
-        raise Exception(f"{func=}, {args=}, {kwargs=} can not be passed")
-    except exception:
-        pass
