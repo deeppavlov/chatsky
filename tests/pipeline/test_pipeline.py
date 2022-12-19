d4660154cd51ec8224d9bd6114108e27ffb776ed
Kuznetsov Denis <kuznetsov.den.p@gmail.com>
2022-12-19 19:11:48 +0300

    Update tests/pipeline/test_pipeline.py
    Co-authored-by: Roman Zlobin <trlkrot@gmail.com>

--- tests/pipeline/test_pipeline.py
+++ tests/pipeline/test_pipeline.py
@@ -5,8 +5,6 @@ import sys
 from tests.test_utils import get_path_from_tests_to_current_dir
 
 
-# TODO: remove this as soon as turn_caching will be moved to PYPI
-sys.path.append(os.path.abspath(f"examples/{get_path_from_tests_to_current_dir(__file__)}"))
 
 dot_path_to_addon = get_path_from_tests_to_current_dir(__file__, separator=".")
 
