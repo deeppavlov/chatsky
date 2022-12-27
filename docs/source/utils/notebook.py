61b26fc0ef43a3ce7d37424e41f570a0c510db46
pseusys <shveitsar215@gmail.com>
2022-12-28 02:53:47 +0400

    attempt to remove old commits effect

--- docs/source/utils/notebook.py
+++ docs/source/utils/notebook.py
@@ -1,24 +1,16 @@
 from jupytext import jupytext
 
 
-def insert_installation_cell_into_py_example():
-    def inner(example_text: str):
-        second_cell = example_text.find("\n# %%", 5)
-        return jupytext.reads(
-            f"""{example_text[:second_cell]}
-# %% [markdown]
-\"\"\"
-__Installing dependencies__
-\"\"\"
+def add_installation_cell_into_py(s: str):
+    return jupytext.reads(
+        f"""# %% [markdown]
+### Installing dependencies
+
 # %%
 !python3 -m pip install -q dff[examples]
-# %% [markdown]
-\"\"\"
-__Running example__
-\"\"\"
-{example_text[second_cell:]}
-""",
-            "py:percent",
-        )
 
-    return inner
+
+{s}
+""",
+        "py:percent",
+    )
