aa63b9ec8483faafaa064988f7f5f826416a5456
pseusys <shveitsar215@gmail.com>
2022-12-28 02:51:26 +0400

    commit artifacts removed

--- docs/source/utils/notebook.py
+++ docs/source/utils/notebook.py
@@ -1,33 +1,22 @@
-import re
-
 from jupytext import jupytext
-
-start_pattern = re.compile(r'# %% \[markdown\]\n"""\n# (\d\. .*)\n\n[\S\s]*?"""\n')
 
 
 def insert_installation_cell_into_py_example():
-
     def inner(example_text: str):
-        match = start_pattern.match(example_text)
-        example_title = example_text[: match.span()[1]]
-        example_body = example_text[match.span()[1] :]
+        second_cell = example_text.find("\n# %%", 5)
         return jupytext.reads(
-            f"""{example_title}
-
+            f"""{example_text[:second_cell]}
 # %% [markdown]
 \"\"\"
 __Installing dependencies__
 \"\"\"
-
 # %%
 !python3 -m pip install -q dff[examples]
-
 # %% [markdown]
 \"\"\"
 __Running example__
 \"\"\"
-
-{example_body}
+{example_text[second_cell:]}
 """,
             "py:percent",
         )
