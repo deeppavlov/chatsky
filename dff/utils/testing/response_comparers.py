960baea141e3752cf2ff662d2da90282e21cc5b7
Roman Zlobin <trlkrot@gmail.com>
2023-01-12 12:45:50 +0300

    remove generics_comparer

--- dff/utils/testing/response_comparers.py
+++ dff/utils/testing/response_comparers.py
@@ -13,21 +13,3 @@ def default_comparer(candidate: Message, reference: Message, _: Context) -> Opti
     :return: None if two responses are equal or candidate response otherwise
     """
     return None if candidate == reference else candidate
-
-
-def generics_comparer(candidate: Message, reference: Message, _: Context) -> Optional[str]:
-    """
-    The generics response comparer. Assumes that true response is a :py:class:`~dff.script.responses.Message` instance
-    and received response is a :py:class:`str` instance.
-    If received response contains `ui.buttons` it compares its text representation to true response.
-    If received response contains `image`, `document`, `audio` or `video`
-    it compares its `attachment` text representation to true response.
-    If received response contains `attachments` it compares its `attachments` text representation to true response.
-    Otherwise, it compares its `text` to true response.
-
-    :param candidate: the received (candidate) response
-    :param reference: the true response
-    :param _: current Context (unused)
-    :return: None if two responses are equal or candidate response text otherwise
-    """
-    raise RuntimeError()
