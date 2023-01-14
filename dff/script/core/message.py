0228a1ed55811693674c85929ff5992eff6ad60e
pseusys <shveitsar215@gmail.com>
2023-01-14 19:57:23 +0400

    repr replaced with str

--- dff/script/core/message.py
+++ dff/script/core/message.py
@@ -101,9 +101,5 @@ class Message(DataModel):
     # state: Optional[Session] = Session.ACTIVE
     # ui: Optional[Union[Keyboard, DataModel]] = None
 
-    def __repr__(self) -> str:
-        representation = list()
-        for key, value in self.dict(exclude_none=True).items():
-            if value is not None:
-                representation += f"key={value}"
-        return " ".join(representation)
+    def __str__(self) -> str:
+        return " ".join([f"{key}='{value}'" for key, value in self.dict(exclude_none=True).items()])
