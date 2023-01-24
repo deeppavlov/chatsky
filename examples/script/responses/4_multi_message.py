b238aa7a2e2144b4188d563163d223fcc0ccddc8
Denis Kuznetsov <kuznetsov.den.p@gmail.com>
2023-01-24 21:26:12 +0300

    feat/HypotheticalMessage: fix doc&upd an example

--- examples/script/responses/4_multi_message.py
+++ examples/script/responses/4_multi_message.py
@@ -2,16 +2,14 @@
 """
 # 4. Multi Message
 
-This example shows different options for setting responses.
+This example shows Multi Message usage.
 Let's do all the necessary imports from `dff`.
 """
 
 
 # %%
-import re
-import random
 
-from dff.script import TRANSITIONS, RESPONSE, Actor, Context, Message, MultiMessage
+from dff.script import TRANSITIONS, RESPONSE, Message, MultiMessage
 import dff.script.conditions as cnd
 
 from dff.pipeline import Pipeline
@@ -21,63 +19,10 @@ from dff.utils.testing.common import (
     run_interactive_mode,
 )
 
-
-# %% [markdown]
-"""
-The response can be set by any object of python:
-
-* Callable objects. If the object is callable it must have a special signature:
-
-        func(ctx: Context, actor: Actor, *args, **kwargs) -> Any
-
-* Non-callable objects. If the object is not callable,
-    it will be returned by the agent as a response.
-
-
-The functions to be used in the `toy_script` are declared here.
-"""
-
-
-# %%
-def cannot_talk_about_topic_response(ctx: Context, actor: Actor, *args, **kwargs) -> Message:
-    request = ctx.last_request
-    if request is None or request.text is None:
-        topic = None
-    else:
-        topic_pattern = re.compile(r"(.*talk about )(.*)\.")
-        topic = topic_pattern.findall(request.text)
-        topic = topic and topic[0] and topic[0][-1]
-    if topic:
-        return Message(text=f"Sorry, I can not talk about {topic} now.")
-    else:
-        return Message(text="Sorry, I can not talk about that now.")
-
-
-def upper_case_response(response: Message):
-    # wrapper for internal response function
-    def func(ctx: Context, actor: Actor, *args, **kwargs) -> Message:
-        if response.text is not None:
-            response.text = response.text.upper()
-        return response
-
-    return func
-
-
-def fallback_trace_response(ctx: Context, actor: Actor, *args, **kwargs) -> Message:
-    return Message(
-        misc={
-            "previous_node": list(ctx.labels.values())[-2],
-            "last_request": ctx.last_request,
-        }
-    )
-
-
 # %%
 toy_script = {
     "greeting_flow": {
         "start_node": {  # This is an initial node,
-            # it doesn't need a `RESPONSE`.
-            RESPONSE: Message(),
             TRANSITIONS: {"node1": cnd.exact_match(Message(text="Hi"))},
             # If "Hi" == request of user then we make the transition
         },
@@ -95,16 +40,16 @@ toy_script = {
             TRANSITIONS: {"node3": cnd.exact_match(Message(text="Let's talk about music."))},
         },
         "node3": {
-            RESPONSE: cannot_talk_about_topic_response,
+            RESPONSE: Message(text="Sorry, I can not talk about that now."),
             TRANSITIONS: {"node4": cnd.exact_match(Message(text="Ok, goodbye."))},
         },
         "node4": {
-            RESPONSE: upper_case_response(Message(text="bye")),
+            RESPONSE: Message(text="bye"),
             TRANSITIONS: {"node1": cnd.exact_match(Message(text="Hi"))},
         },
         "fallback_node": {  # We get to this node
             # if an error occurred while the agent was running.
-            RESPONSE: fallback_trace_response,
+            RESPONSE: Message(text="Ooops"),
             TRANSITIONS: {"node1": cnd.exact_match(Message(text="Hi"))},
         },
     }
@@ -127,9 +72,9 @@ happy_path = (
     ),  # node1 -> node2
     (
         Message(text="Let's talk about music."),
-        Message(text="Sorry, I can not talk about music now."),
+        Message(text="Sorry, I can not talk about that now."),
     ),  # node2 -> node3
-    (Message(text="Ok, goodbye."), Message(text="BYE")),  # node3 -> node4
+    (Message(text="Ok, goodbye."), Message(text="bye")),  # node3 -> node4
     (
         Message(text="Hi"),
         MultiMessage(
@@ -141,37 +86,20 @@ happy_path = (
     ),  # node4 -> node1
     (
         Message(text="stop"),
-        Message(
-            misc={"previous_node": ("greeting_flow", "node1"), "last_request": Message(text="stop")}
-        ),
+        Message(text="Ooops"),
     ),
     # node1 -> fallback_node
     (
         Message(text="one"),
-        Message(
-            misc={
-                "previous_node": ("greeting_flow", "fallback_node"),
-                "last_request": Message(text="one"),
-            }
-        ),
+        Message(text="Ooops"),
     ),  # f_n->f_n
     (
         Message(text="help"),
-        Message(
-            misc={
-                "previous_node": ("greeting_flow", "fallback_node"),
-                "last_request": Message(text="help"),
-            }
-        ),
+        Message(text="Ooops"),
     ),  # f_n->f_n
     (
         Message(text="nope"),
-        Message(
-            misc={
-                "previous_node": ("greeting_flow", "fallback_node"),
-                "last_request": Message(text="nope"),
-            }
-        ),
+        Message(text="Ooops"),
     ),  # f_n->f_n
     (
         Message(text="Hi"),
@@ -188,14 +116,12 @@ happy_path = (
     ),  # node1 -> node2
     (
         Message(text="Let's talk about music."),
-        Message(text="Sorry, I can not talk about music now."),
+        Message(text="Sorry, I can not talk about that now."),
     ),  # node2 -> node3
-    (Message(text="Ok, goodbye."), Message(text="BYE")),  # node3 -> node4
+    (Message(text="Ok, goodbye."), Message(text="bye")),  # node3 -> node4
 )
 
 # %%
-random.seed(31415)  # predestination of choice
-
 
 pipeline = Pipeline.from_script(
     toy_script,
