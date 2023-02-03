f433ee7a7a00676f19a2798fe6123c1ef6f52e09
Aleksandr Sakharov <92101662+avsakharov@users.noreply.github.com>
2023-02-03 12:13:00 +0300

    Update dff/pipeline/pipeline/pipeline.py
    Co-authored-by: Kuznetsov Denis <kuznetsov.den.p@gmail.com>

--- dff/pipeline/pipeline/pipeline.py
+++ dff/pipeline/pipeline/pipeline.py
@@ -3,7 +3,7 @@ Pipeline
 --------
 The Pipeline module contains the :py:class:`.Pipeline` class,
 which is a fundamental element of the `DFF`. The Pipeline class is responsible
-for managing and executing the various components that make up a conversation flow.
+for managing and executing the various components (:py:class:`.PipelineComponent`) that make up a conversation flow.
 It provides a way to organize and structure the conversation flow.
 The Pipeline class is designed to be highly customizable and configurable,
 allowing developers to add, remove, or modify the components that make up the conversation flow.
