aeecd84d99fe70ee501e8bc3017743293102ff07
Roman Zlobin <trlkrot@gmail.com>
2023-02-07 15:59:51 +0300

    cover `send_document` with test

--- tests/messengers/telegram/test_types.py
+++ tests/messengers/telegram/test_types.py
@@ -19,13 +19,18 @@ from dff.messengers.telegram.message import (
     ParseMode,
 )
 from dff.script import Message
-from dff.script.core.message import Attachments, Keyboard, Button, Image, Location, Audio, Video, Attachment
+from dff.script.core.message import Attachments, Keyboard, Button, Image, Location, Audio, Video, Attachment, Document
 from dff.messengers.telegram.utils import open_io, close_io, batch_open_io
 from dff.utils.testing import TelegramTesting
 
 TG_BOT_TOKEN = os.getenv("TG_BOT_TOKEN")
 TG_API_ID = os.getenv("TG_API_ID")
 TG_API_HASH = os.getenv("TG_API_HASH")
+
+image = Image(source="https://folklore.linghub.ru/api/gallery/300/23.JPG")
+audio = Audio(source="https://north-folklore.ru/static/sound/IVD_No44.MP3")
+video = Video(source="https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4")
+document = Document(source=__file__)
 
 
 @pytest.mark.skipif(not TG_BOT_TOKEN, reason="`TG_BOT_TOKEN` missing")
@@ -89,37 +94,38 @@ async def test_keyboard_remove(tmp_path, pipeline_instance):
 @pytest.mark.skipif(not TG_API_ID or not TG_API_HASH, reason="TG credentials missing")
 @pytest.mark.asyncio
 @pytest.mark.parametrize(
-    ["generic_response", "prop", "filter_type"],
+    ["generic_response"],
     [
         (
             Message(
                 text="test",
-                attachments=Attachments(files=[Image(source="https://folklore.linghub.ru/api/gallery/300/23.JPG")]),
+                attachments=Attachments(files=[image]),
             ),
-            "image",
-            InputMessagesFilterPhotos,
         ),
         (
             Message(
                 text="test",
-                attachments=Attachments(files=[Audio(source="https://north-folklore.ru/static/sound/IVD_No44.MP3")]),
+                attachments=Attachments(files=[audio]),
             ),
-            "audio",
-            InputMessagesFilterMusic,
         ),
         (
             Message(
                 text="test",
                 attachments=Attachments(
-                    files=[Video(source="https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4")]
+                    files=[video]
                 ),
             ),
-            "video",
-            InputMessagesFilterVideo,
+        ),
+        (
+            Message(
+                attachments=Attachments(
+                    files=[document]
+                )
+            ),
         ),
     ],
 )
-async def test_telegram_attachment(generic_response, prop, filter_type, tmp_path, pipeline_instance):
+async def test_telegram_attachment(generic_response, tmp_path, pipeline_instance):
     telegram_response = TelegramMessage.parse_obj(generic_response)
     test_helper = TelegramTesting(pipeline_instance)
     await test_helper.send_and_check(telegram_response, tmp_path)
