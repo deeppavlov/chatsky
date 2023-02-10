0a7fc99f7516752efdaeedccc73bc97dc817dcc3
Aleksandr Sakharov <92101662+avsakharov@users.noreply.github.com>
2023-02-10 11:04:14 +0300

    Update dff/pipeline/types.py
    Co-authored-by: Kuznetsov Denis <kuznetsov.den.p@gmail.com>

--- dff/pipeline/types.py
+++ dff/pipeline/types.py
@@ -1,7 +1,7 @@
 """
 Types
 -----
-The Types module contains several classes and special types that are used throughout the `DFF`.
+The Types module contains several classes and special types that are used throughout the `DFF Pipeline`.
 The classes and special types in this module can include data models,
 data structures, and other types that are defined for type hinting.
 """
