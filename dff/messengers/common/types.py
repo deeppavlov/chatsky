60d02d186c499ab880be94bff0eb44a00ac09aca
Aleksandr Sakharov <92101662+avsakharov@users.noreply.github.com>
2023-02-10 10:32:01 +0300

    Update dff/messengers/common/types.py
    Co-authored-by: Kuznetsov Denis <kuznetsov.den.p@gmail.com>

--- dff/messengers/common/types.py
+++ dff/messengers/common/types.py
@@ -1,7 +1,7 @@
 """
 Types
 -----
- The Types module contains two special types that are used throughout the `DFF`.
+ The Types module contains two special types that are used throughout the `DFF Messengers`.
  The first type is used for the messenger interface to client interaction and the second one
  to control the polling loop.
 """
