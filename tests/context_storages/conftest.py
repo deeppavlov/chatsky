fd2d2160848138650cdf53ac9af5d1a4be961df2
ruthenian8 <ruthenian8@gmail.com>
2023-02-16 16:35:22 +0300

    change patch in test_dbs

--- tests/context_storages/conftest.py
+++ tests/context_storages/conftest.py
@@ -5,13 +5,8 @@ import nest_asyncio
 from dff.script import Context
 import pytest
 
-
-@pytest.fixture(scope="session")
-def event_loop():
-    loop = asyncio.get_event_loop_policy().new_event_loop()
-    nest_asyncio.apply(loop)
-    yield loop
-    loop.close()
+loop = asyncio.get_event_loop_policy().new_event_loop()
+nest_asyncio.apply(loop)
 
 
 @pytest.fixture(scope="function")
