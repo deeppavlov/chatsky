9b6bcf66e1d82234243d20ef29d70927ecc6bff8
ruthenian8 <ruthenian8@gmail.com>
2023-02-20 12:45:29 +0300

    avoid closing the loop manually

--- tests/context_storages/conftest.py
+++ tests/context_storages/conftest.py
@@ -6,15 +6,15 @@ from dff.script import Context
 import pytest
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="session")
 def event_loop():
     loop = asyncio.get_event_loop_policy().new_event_loop()
     asyncio.get_event_loop_policy().set_event_loop(loop)
     nest_asyncio.apply(loop)
     yield loop
-    tasks = asyncio.all_tasks(loop)
-    loop.run_until_complete(asyncio.gather(*tasks))
-    loop.close()
+    # tasks = asyncio.all_tasks(loop)
+    # loop.run_until_complete(asyncio.gather(*tasks))
+    # loop.close()
 
 
 @pytest.fixture(scope="function")
