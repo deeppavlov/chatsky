c72c42e11336a1a897486551c492d777b98c92ca
pseusys <shveitsar215@gmail.com>
2023-03-11 05:44:34 +0100

    lint applied

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,10 +5,10 @@ import re
 # -- Path setup --------------------------------------------------------------
 
 sys.path.append(os.path.abspath("."))
+from utils import patching  # noqa: E402, F401
 from utils.notebook import insert_installation_cell_into_py_example  # noqa: E402
 from utils.generate_examples import generate_example_links_for_notebook_creation  # noqa: E402
 from utils.regenerate_apiref import regenerate_apiref  # noqa: E402
-from docs.source.utils.patching import patch_autosummary_extract_summary  # noqa: E402
 
 # -- Project information -----------------------------------------------------
 
@@ -85,7 +85,6 @@ html_static_path = ["_static"]
 html_show_sourcelink = False
 
 autosummary_generate_overwrite = False
-patch_autosummary_extract_summary()
 
 # Finding examples directories
 nbsphinx_custom_formats = {".py": insert_installation_cell_into_py_example()}
--- docs/source/utils/patching.py
+++ docs/source/utils/patching.py
@@ -18,7 +18,8 @@ def _get_body(func: Callable):
 
 
 def patch_source_file(module: str, patch: str, patch_payload: Optional[str] = None):
-    patch_comment = f"# Patched with: {sha256((patch if patch_payload is None else patch_payload).encode('utf-8')).hexdigest()}"
+    patch_payload = patch if patch_payload is None else patch_payload
+    patch_comment = f"# Patched with: {sha256(patch_payload.encode('utf-8')).hexdigest()}"
     patch = f"\n\n\n{patch_comment}\n{patch}\n"
     with open(module, "r") as file:
         if any(patch_comment in line for line in file.readlines()):
@@ -31,11 +32,13 @@ def wrap_source_function(source: Callable, wrapper: Callable):
     src_file = getsourcefile(source)
     src_name = getattr(source, '__name__')
     wrap_body = _get_body(wrapper)
-    patch = f"{src_name}_old = {src_name}\ndef {src_name}_wrapper{signature(wrapper)}:\n{wrap_body}\n{src_name} = {src_name}_wrapper({src_name}_old)"
+    wrap_sign = f"def {src_name}_wrapper{signature(wrapper)}"
+    patch = f"{src_name}_old = {src_name}\n{wrap_sign}:\n{wrap_body}\n{src_name} = {src_name}_wrapper({src_name}_old)"
     patch_source_file(src_file, patch, patch_payload=f"{signature(wrapper)}:\n{wrap_body}")
 
 
-def patch_autosummary_extract_summary():
-    def extract_summary_wrapper(func):
-        return lambda doc, document: func(doc, document).split('\n\n')[-1]
-    wrap_source_function(extract_summary, extract_summary_wrapper)
+def extract_summary_wrapper(func):
+    return lambda doc, document: func(doc, document).split('\n\n')[-1]
+
+
+wrap_source_function(extract_summary, extract_summary_wrapper)
