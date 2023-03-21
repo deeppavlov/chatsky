446602f6591f8bb3c5f035232753b9a783aa40be
ruthenian8 <ruthenian8@gmail.com>
2023-03-21 17:26:53 +0300

    adapt tests to new structure; assert that tests are passing

--- tests/viewer/test_df_script_viewer.py
+++ tests/viewer/test_df_script_viewer.py
@@ -2,32 +2,28 @@ import difflib
 from io import BytesIO
 from base64 import b64encode
 from pathlib import Path
-import re
 
 import plotly.graph_objects as go
-from dff.script.utils.script_viewer import graph
-from dff.script.utils.script_viewer import plot
-from tests.utils import get_path_from_tests_to_current_dir
+from dff.utils.viewer import graph
+from dff.utils.viewer import plot
+from tests.test_utils import get_path_from_tests_to_current_dir
 
 import pytest
 
 
 @pytest.fixture(scope="session")
-def testing_graph():
+def nx_graph():
     example_dir = Path(f"examples/{get_path_from_tests_to_current_dir(__file__)}") / "python_files"
-    G = graph.get_graph(example_dir / "main.py", example_dir)
+    G = graph.get_graph(example_dir / "main.py", example_dir.absolute())
     yield G
 
 
-@pytest.mark.parametrize(
-    ["params"],
-    [
-        (dict(show_misc=False, show_response=False, show_global=False, show_local=False, show_isolates=False),),
-        (dict(show_misc=True, show_response=True, show_global=True, show_local=True, show_isolates=True),),
-    ],
-)
-def test_plotting(params, testing_graph):
-    testing_plot = plot.get_plot(testing_graph, **params)
+@pytest.mark.parametrize("show_misc", [True, False])
+@pytest.mark.parametrize("show_global", [True, False])
+@pytest.mark.parametrize("show_local", [True, False])
+@pytest.mark.parametrize("show_isolates", [True, False])
+def test_plotting(nx_graph, show_misc, show_global, show_local, show_isolates):
+    testing_plot = plot.get_plot(**locals())
     _bytes = testing_plot.pipe("png")
     assert isinstance(_bytes, bytes) and len(_bytes) > 0
     prefix = "data:image/png;base64,"
@@ -43,6 +39,7 @@ def test_plotting(params, testing_graph):
         (
             dict(
                 show_misc=False,
+                show_processing=False,
                 show_response=False,
                 show_global=False,
                 show_local=False,
@@ -53,14 +50,20 @@ def test_plotting(params, testing_graph):
         ),
         (
             dict(
-                show_misc=True, show_response=True, show_global=True, show_local=True, show_isolates=True, random_seed=1
+                show_misc=True,
+                show_processing=True,
+                show_response=True,
+                show_global=True,
+                show_local=True,
+                show_isolates=True,
+                random_seed=1,
             ),
             Path(__file__).parent / "opts_on.dot",
         ),
     ],
 )
-def test_plotting_2(params, reference_file, testing_graph, tmp_path):
-    testing_plot = plot.get_plot(testing_graph, **params)
+def test_plotting_2(params, reference_file, nx_graph, tmp_path):
+    testing_plot = plot.get_plot(nx_graph, **params)
     plot_file = tmp_path / "plot"
     testing_plot.render(filename=plot_file)
     test_lines = plot_file.open().readlines()
