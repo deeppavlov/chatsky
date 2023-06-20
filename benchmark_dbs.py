b970e83527ed06ad0738b496d733dc4600c1f1cc
Roman Zlobin <trlkrot@gmail.com>
2023-06-20 16:15:00 +0300

    add benchmark_dbs and benchmark_streamlit

---
+++ benchmark_dbs.py
@@ -0,0 +1,216 @@
+import pathlib
+from platform import system
+import typing as tp
+from uuid import uuid4
+import json
+
+from pympler import asizeof
+from pydantic import BaseModel, Field
+
+from dff.script import Context
+from dff.context_storages import context_storage_factory as partial_factory
+from dff.context_storages_old import context_storage_factory as dev_factory
+
+
+import dff.utils.benchmark.context_storage as bm
+
+
+# define benchmark classes and tools
+
+class DBFactory(BaseModel):
+    base_factory: tp.Literal["dev", "partial"]
+    uri: str
+
+    def db(self):
+        if self.base_factory == "dev":
+            return dev_factory(self.uri)
+        else:
+            return partial_factory(self.uri)
+
+
+class BenchmarkCase(BaseModel):
+    name: str
+    db_factory: DBFactory
+    uuid: str = Field(default_factory=lambda: str(uuid4()))
+    description: str = ""
+    context_num: int = 100
+    from_dialog_len: int = 300
+    to_dialog_len: int = 500
+    step_dialog_len: int = 10
+    message_lengths: tp.Tuple[int, ...] = (10, 10)
+    misc_lengths: tp.Tuple[int, ...] = (10, 10)
+
+    def get_context_updater(self):
+        def _context_updater(context: Context):
+            start_len = len(context.requests)
+            if start_len + self.step_dialog_len < self.to_dialog_len:
+                for i in range(start_len, start_len + self.step_dialog_len):
+                    context.add_label((f"flow_{i}", f"node_{i}"))
+                    context.add_request(bm.get_message(self.message_lengths))
+                    context.add_response(bm.get_message(self.message_lengths))
+                return context
+            else:
+                return None
+
+        return _context_updater
+
+    def sizes(self):
+        return {
+            "starting_context_size": asizeof.asizeof(
+                bm.get_context(self.from_dialog_len, self.message_lengths, self.misc_lengths)
+            ),
+            "final_context_size": asizeof.asizeof(
+                bm.get_context(self.to_dialog_len, self.message_lengths, self.misc_lengths)
+            ),
+            "misc_size": asizeof.asizeof(bm.get_dict(self.misc_lengths)),
+            "message_size": asizeof.asizeof(bm.get_message(self.message_lengths)),
+        }
+
+    def run(self):
+        try:
+            write_times, read_times, update_times = bm.time_context_read_write(
+                self.db_factory.db(),
+                bm.get_context(self.from_dialog_len, self.message_lengths, self.misc_lengths),
+                self.context_num,
+                context_updater=self.get_context_updater()
+            )
+            return {
+                "success": True,
+                "result": {
+                    "write_times": write_times,
+                    "read_times": read_times,
+                    "update_times": update_times,
+                }
+            }
+        except Exception as e:
+            return {
+                "success": False,
+                "result": getattr(e, "message", repr(e))
+            }
+
+
+def save_results_to_file(
+    benchmark_cases: tp.List[BenchmarkCase],
+    file: str | pathlib.Path,
+    name: str,
+    description: str,
+):
+    uuid = str(uuid4())
+    result: tp.Dict[str, tp.Any] = {
+        "name": name,
+        "description": description,
+        "uuid": uuid,
+    }
+    for case in benchmark_cases:
+        result[case.uuid] = {**case.dict(), **case.sizes(), **case.run()}
+
+    with open(file, "w", encoding="utf-8") as fd:
+        json.dump(result, fd)
+
+
+def benchmark_all(
+    file: str | pathlib.Path,
+    name: str,
+    description: str,
+    db_uris: tp.Dict[str, str],
+    case_name_postfix: str = "",
+    context_num: int = 100,
+    from_dialog_len: int = 300,
+    to_dialog_len: int = 500,
+    step_dialog_len: int = 10,
+    message_lengths: tp.Tuple[int, ...] = (10, 10),
+    misc_lengths: tp.Tuple[int, ...] = (10, 10),
+):
+    benchmark_cases = []
+    for db, uri in db_uris.items():
+        benchmark_cases.append(
+            BenchmarkCase(
+                name=db + "-dev" + case_name_postfix,
+                db_factory=DBFactory(base_factory="dev", uri=uri),
+                context_num=context_num,
+                from_dialog_len=from_dialog_len,
+                to_dialog_len=to_dialog_len,
+                step_dialog_len=step_dialog_len,
+                message_lengths=message_lengths,
+                misc_lengths=misc_lengths,
+            )
+        )
+        benchmark_cases.append(
+            BenchmarkCase(
+                name=db + "-partial" + case_name_postfix,
+                db_factory=DBFactory(base_factory="partial", uri=uri),
+                context_num=context_num,
+                from_dialog_len=from_dialog_len,
+                to_dialog_len=to_dialog_len,
+                step_dialog_len=step_dialog_len,
+                message_lengths=message_lengths,
+                misc_lengths=misc_lengths,
+            )
+        )
+    save_results_to_file(benchmark_cases, file, name, description)
+
+
+# create dir and files
+
+pathlib.Path("dbs").mkdir(exist_ok=True)
+sqlite_file = pathlib.Path("dbs/sqlite.db")
+sqlite_file.touch(exist_ok=True)
+sqlite_separator = "///" if system() == "Windows" else "////"
+
+dbs = {
+    # "JSON": "json://dbs/json.json",
+    # "Pickle": "pickle://dbs/pickle.pkl",
+    "Shelve": "shelve://dbs/shelve",
+    "PostgreSQL": "postgresql+asyncpg://postgres:pass@localhost:5432/test",
+    "MongoDB": "mongodb://admin:pass@localhost:27017/admin",
+    "Redis": "redis://:pass@localhost:6379/0",
+    "MySQL": "mysql+asyncmy://root:pass@localhost:3307/test",
+    "SQLite": f"sqlite+aiosqlite:{sqlite_separator}{sqlite_file.absolute()}",
+    # "YDB": "grpc://localhost:2136/local",
+}
+
+# benchmark
+pathlib.Path("benchmarks").mkdir(exist_ok=True)
+
+benchmark_all(
+    "benchmarks/alexaprize.json",
+    "Alexaprize-like dialogue benchmarks",
+    "This benchmark set tests against parameters that mimic dialogues from alexaprize.",
+    db_uris=dbs,
+    from_dialog_len=1,
+    to_dialog_len=50,
+    message_lengths=(3, 5, 6, 5, 3),
+    misc_lengths=(2, 4, 3, 8, 100),
+)
+
+benchmark_all(
+    "benchmarks/alexaprize_longer.json",
+    "Alexaprize-like dialogue benchmarks (longer)",
+    "This benchmark set tests against parameters that mimic dialogues from alexaprize,"
+    "but dialog len is increased.",
+    db_uris=dbs,
+    from_dialog_len=100,
+    to_dialog_len=1001,
+    step_dialog_len=100,
+    message_lengths=(3, 5, 6, 5, 3),
+    misc_lengths=(2, 4, 3, 8, 100),
+)
+
+benchmark_all(
+    "benchmarks/short_messages.json",
+    "Short messages",
+    "This benchmark set tests with short messages, long dialog len.",
+    db_uris=dbs,
+    from_dialog_len=100,
+    to_dialog_len=1001,
+    step_dialog_len=100,
+    message_lengths=(2, 30),
+    misc_lengths=(0, 0),
+)
+
+benchmark_all(
+    "benchmarks/default.json",
+    "Default",
+    "This benchmark set tests using default parameters.",
+    db_uris=dbs,
+)
---
+++ benchmark_streamlit.py
@@ -0,0 +1,314 @@
+import streamlit as st
+import json
+from pathlib import Path
+from statistics import mean
+import pandas as pd
+import numpy as np
+from pympler import asizeof
+from humanize import naturalsize
+import altair as alt
+
+
+st.set_page_config(
+    page_title="DB benchmark",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+
+benchmark_results_files = Path("benchmark_results_files.json")
+
+if not benchmark_results_files.exists():
+    with open(benchmark_results_files, "w", encoding="utf-8") as fd:
+        json.dump([], fd)
+
+if "benchmark_files" not in st.session_state:
+    with open(benchmark_results_files, "r", encoding="utf-8") as fd:
+        st.session_state["benchmark_files"] = json.load(fd)
+
+if "benchmarks" not in st.session_state:
+    st.session_state["benchmarks"] = {}
+
+    for file in st.session_state["benchmark_files"]:
+        with open(file, "r", encoding="utf-8") as fd:
+            st.session_state["benchmarks"][file] = json.load(fd)
+
+if "compare" not in st.session_state:
+    st.session_state["compare"] = []
+
+
+def set_average_results(benchmark):
+    if not benchmark["success"] or isinstance(benchmark["result"], str):
+        return
+
+    def get_complex_stats(results):
+        average_grouped_by_context_num = [mean(times.values()) for times in results]
+        average_grouped_by_dialog_len = {key: mean([times[key] for times in results]) for key in results[0].keys()}
+        average = mean(average_grouped_by_context_num)
+        return average_grouped_by_context_num, average_grouped_by_dialog_len, average
+
+    read_stats = get_complex_stats(benchmark["result"]["read_times"])
+    update_stats = get_complex_stats(benchmark["result"]["update_times"])
+
+    result = {
+        "average_write_time": mean(benchmark["result"]["write_times"]),
+        "average_read_time": read_stats[2],
+        "average_update_time": update_stats[2],
+        "write_times": benchmark["result"]["write_times"],
+        "read_times_grouped_by_context_num": read_stats[0],
+        "read_times_grouped_by_dialog_len": read_stats[1],
+        "update_times_grouped_by_context_num": update_stats[0],
+        "update_times_grouped_by_dialog_len": update_stats[1],
+    }
+    result["pretty_write"] = float(f'{result["average_write_time"]:.3}')
+    result["pretty_read"] = float(f'{result["average_read_time"]:.3}')
+    result["pretty_update"] = float(f'{result["average_update_time"]:.3}')
+
+    benchmark["average_results"] = result
+
+
+st.sidebar.text(f"Benchmarks take {naturalsize(asizeof.asizeof(st.session_state['benchmarks']))} RAM")
+
+add_tab, view_tab, compare_tab = st.tabs(["Benchmark sets", "View", "Compare"])
+
+
+###############################################################################
+# Benchmark file manipulation tab
+# Allows adding and deleting benchmark files
+###############################################################################
+
+with add_tab:
+    benchmark_list = []
+
+    for file, benchmark in st.session_state["benchmarks"].items():
+        benchmark_list.append(
+            {
+                "file": file,
+                "name": benchmark["name"],
+                "description": benchmark["description"],
+                "uuid": benchmark["uuid"],
+                "delete": False,
+            }
+        )
+
+    df = pd.DataFrame(data=benchmark_list)
+    edited_df = st.data_editor(df, disabled=("file", "name", "description", "uuid"))
+
+    delist_container = st.container()
+    delist_container.divider()
+
+    def delist_benchmarks():
+        delisted_sets = [f"{name} ({uuid})"
+                         for name, uuid in edited_df.loc[edited_df["delete"]][["name", "uuid"]].values
+                         ]
+
+        st.session_state["compare"] = [
+            item for item in st.session_state["compare"] if item["benchmark_set"] not in delisted_sets
+        ]
+
+        files_to_delist = edited_df.loc[edited_df["delete"]]["file"]
+        st.session_state["benchmark_files"] = list(set(st.session_state["benchmark_files"]) - set(files_to_delist))
+        for file in files_to_delist:
+            del st.session_state["benchmarks"][file]
+            delist_container.text(f"Delisted {file}")
+
+
+    delist_container.button(label="Delist selected benchmark sets", on_click=delist_benchmarks)
+
+    add_container = st.container()
+    add_container.divider()
+
+    add_container.text_input(label="Benchmark set file", key="add_benchmark_file")
+
+    def add_benchmark():
+        benchmark_file = st.session_state["add_benchmark_file"]
+        if benchmark_file == "":
+            return
+
+        if benchmark_file in st.session_state["benchmark_files"]:
+            add_container.warning("Benchmark file already added")
+            return
+
+        if not Path(benchmark_file).exists():
+            add_container.warning("File does not exists")
+            return
+
+        with open(benchmark_file, "r", encoding="utf-8") as fd:
+            file_contents = json.load(fd)
+
+        for benchmark in st.session_state["benchmarks"].values():
+            if file_contents["uuid"] == benchmark["uuid"]:
+                add_container.warning("Benchmark with the same uuid already exists")
+                return
+
+        st.session_state["benchmark_files"].append(benchmark_file)
+        with open(benchmark_results_files, "w", encoding="utf-8") as fd:
+            json.dump(list(st.session_state["benchmark_files"]), fd)
+        st.session_state["benchmarks"][benchmark_file] = file_contents
+
+        add_container.text(f"Added {benchmark_file} set")
+
+    add_container.button("Add benchmark set from file", on_click=add_benchmark)
+
+###############################################################################
+# View tab
+# Allows viewing existing benchmarks
+###############################################################################
+
+with view_tab:
+    set_choice, benchmark_choice, compare = st.columns([3, 3, 1])
+
+    sets = {
+        f"{benchmark['name']} ({benchmark['uuid']})": benchmark
+        for benchmark in st.session_state["benchmarks"].values()
+    }
+    benchmark_set = set_choice.selectbox("Benchmark set", sets.keys())
+
+    if benchmark_set is None:
+        set_choice.warning("No benchmark sets available")
+        st.stop()
+
+    selected_set = sets[benchmark_set]
+
+    set_choice.text("Set description:")
+    set_choice.markdown(selected_set["description"])
+
+    benchmarks = {
+        f"{benchmark['name']} ({benchmark['uuid']})": benchmark
+        for field, benchmark in selected_set.items()
+        if field not in ("name", "description", "uuid")
+    }
+
+    benchmark = benchmark_choice.selectbox("Benchmark", benchmarks.keys())
+
+    if benchmark is None:
+        benchmark_choice.warning("No benchmarks in the set")
+        st.stop()
+
+    selected_benchmark = benchmarks[benchmark]
+
+    benchmark_choice.text("Benchmark description:")
+    benchmark_choice.markdown(selected_benchmark["description"])
+
+    with st.expander("Benchmark stats"):
+        reproducible_stats = {
+            stat: selected_benchmark[stat]
+            for stat in (
+                "db_factory",
+                "context_num",
+                "from_dialog_len",
+                "to_dialog_len",
+                "step_dialog_len",
+                "message_lengths",
+                "misc_lengths",
+            )
+        }
+
+        size_stats = {
+            stat: naturalsize(selected_benchmark[stat], gnu=True)
+            for stat in (
+                "starting_context_size",
+                "final_context_size",
+                "misc_size",
+                "message_size",
+            )
+        }
+
+        st.json(reproducible_stats)
+        st.json(size_stats)
+
+    if not selected_benchmark["success"]:
+        st.warning(selected_benchmark["result"])
+    else:
+        set_average_results(selected_benchmark)
+
+        write, read, update = st.columns(3)
+
+        write.metric("Write", selected_benchmark["average_results"]["pretty_write"])
+        read.metric("Read", selected_benchmark["average_results"]["pretty_read"])
+        update.metric("Update", selected_benchmark["average_results"]["pretty_update"])
+
+        compare_item = {
+            "benchmark_set": benchmark_set,
+            "benchmark": benchmark,
+            "write": selected_benchmark["average_results"]["average_write_time"],
+            "read": selected_benchmark["average_results"]["average_read_time"],
+            "update": selected_benchmark["average_results"]["average_update_time"],
+        }
+
+        def add_results_to_compare_tab():
+            if compare_item not in st.session_state["compare"]:
+                st.session_state["compare"].append(compare_item)
+            else:
+                st.session_state["compare"].remove(compare_item)
+
+        compare.button(
+            "Add to Compare" if compare_item not in st.session_state["compare"] else "Remove from Compare",
+            on_click=add_results_to_compare_tab
+        )
+
+        select_graph, graph = st.columns([1, 3])
+
+        graphs = {
+            "Write": selected_benchmark["average_results"]["write_times"],
+            "Read (grouped by contex_num)": selected_benchmark["average_results"]["read_times_grouped_by_context_num"],
+            "Read (grouped by dialog_len)": selected_benchmark["average_results"]["read_times_grouped_by_dialog_len"],
+            "Update (grouped by contex_num)": selected_benchmark["average_results"]["update_times_grouped_by_context_num"],
+            "Update (grouped by dialog_len)": selected_benchmark["average_results"]["update_times_grouped_by_dialog_len"],
+        }
+
+        selected_graph = select_graph.selectbox("Select graph to display", graphs.keys())
+
+        graph_data = graphs[selected_graph]
+
+        if isinstance(graph_data, dict):
+            data = pd.DataFrame({"dialog_len": graph_data.keys(), "time": graph_data.values()})
+        else:
+            data = pd.DataFrame({"context_num": range(len(graph_data)), "time": graph_data})
+
+        chart = alt.Chart(data).mark_circle().encode(
+            x="dialog_len:Q" if isinstance(graph_data, dict) else "context_num:Q",
+            y="time:Q",
+        ).interactive()
+
+        graph.altair_chart(chart, use_container_width=True)
+
+
+###############################################################################
+# Compare tab
+# Allows viewing existing benchmarks
+###############################################################################
+
+with compare_tab:
+    df = pd.DataFrame(st.session_state["compare"])
+
+    if not df.empty:
+        st.dataframe(
+            df.style.highlight_min(
+                axis=0, subset=["write", "read", "update"], props='background-color:green;'
+            ).highlight_max(
+                axis=0, subset=["write", "read", "update"], props='background-color:red;'
+            )
+        )
+
+        if len(st.session_state["compare"]) == 2:
+            def get_diff(last_metric, first_metric):
+                return (last_metric / first_metric - 1) * 100
+
+            write, read, update = st.columns(3)
+
+            first_dict, second_dict = st.session_state["compare"]
+
+            columns = {
+                "write": write,
+                "read": read,
+                "update": update,
+            }
+
+            for column_name, column in columns.items():
+                column.metric(
+                    label=column_name.title(),
+                    value=second_dict[column_name],
+                    delta=f"{get_diff(second_dict[column_name], first_dict[column_name])} %",
+                    delta_color="inverse"
+                )
