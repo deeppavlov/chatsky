8480958571a57acd169e377e24b34fd610d4f788
ruthenian8 <ruthenian8@gmail.com>
2023-06-22 15:36:43 +0300

    Revert "replace 'today' variable with 'build_date' variable (#145)" (#151)
    This reverts commit c0097ff0b3761bf54bb34848b5bd75933d9c6163.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,6 @@
 import os
 import sys
 import re
-import datetime
 
 # -- Path setup --------------------------------------------------------------
 
@@ -90,9 +89,8 @@ autosummary_generate_overwrite = False
 
 # Finding tutorials directories
 nbsphinx_custom_formats = {".py": insert_installation_cell_into_py_tutorial()}
-nbsphinx_prolog = f"""
+nbsphinx_prolog = """
 :tutorial_name: {{ env.docname }}
-:build_time: {datetime.datetime.now().strftime("%b %d, %Y")}
 """
 
 html_logo = "_static/images/logo-simple.svg"
