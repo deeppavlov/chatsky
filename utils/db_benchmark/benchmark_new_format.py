266e28043b16b20975fd3a36826ddeb9b0ef88c7
Roman Zlobin <trlkrot@gmail.com>
2023-07-06 23:09:42 +0300

    remove old format support from format updater

--- utils/db_benchmark/benchmark_new_format.py
+++ utils/db_benchmark/benchmark_new_format.py
@@ -7,8 +7,6 @@ import json
 import pathlib
 import typing as tp
 
-from dff.utils.benchmark.context_storage import BenchmarkCase
-
 benchmark_path = pathlib.Path("benchmarks")
 
 
@@ -16,58 +14,13 @@ def update_benchmark_file(benchmark_set_file: tp.Union[pathlib.Path, str]):
     with open(benchmark_set_file, "r") as fd:
         benchmark_set = json.load(fd)
 
-    non_benchmark_fields = ("name", "description", "uuid", "benchmarks")
-
-    new_benchmark_set = {k: v for k, v in benchmark_set.items() if k in non_benchmark_fields}
-
-    if "benchmarks" not in benchmark_set:
-        new_benchmark_set["benchmarks"] = {k: v for k, v in benchmark_set.items() if k not in non_benchmark_fields}
-
-    for key, benchmark in new_benchmark_set["benchmarks"].items():
-        # update old factory specification
-        if benchmark["db_factory"].get("base_factory") == "dev":
-            postfix = "_old"
-        elif benchmark["db_factory"].get("base_factory") == "partial":
-            postfix = ""
-        else:
-            postfix = None
-
-        if postfix is not None:
-            benchmark["db_factory"].pop("base_factory", None)
-            benchmark["db_factory"].update(
-                {
-                    "factory_module": "dff.context_storages" + postfix,
-                    "factory": "context_storage_factory",
-                }
-            )
-
-    # update average calculation
-    for benchmark in new_benchmark_set["benchmarks"].values():
-        BenchmarkCase.set_average_results(benchmark)
-
-    # update lengths -> dimensions renaming
-    for benchmark in new_benchmark_set["benchmarks"].values():
-        for param in ("message", "misc"):
-            dimensions = benchmark.pop(f"{param}_lengths", None)
-            if dimensions is not None:
-                benchmark[f"{param}_dimensions"] = dimensions
-
-    # update benchmark_config
-    for benchmark in new_benchmark_set["benchmarks"].values():
-        if "benchmark_config" not in benchmark:
-            benchmark_config = {
-                "context_num": benchmark.pop("context_num"),
-                "from_dialog_len": benchmark.pop("from_dialog_len"),
-                "to_dialog_len": benchmark.pop("to_dialog_len"),
-                "step_dialog_len": benchmark.pop("step_dialog_len"),
-                "message_dimensions": benchmark.pop("message_dimensions"),
-                "misc_dimensions": benchmark.pop("misc_dimensions"),
-            }
-
-            benchmark["benchmark_config"] = benchmark_config
+    # move benchmarks from dict to list
+    if isinstance(benchmark_set.get("benchmarks"), dict):
+        benchmarks = benchmark_set.pop("benchmarks")
+        benchmark_set["benchmarks"] = list(benchmarks.values())
 
     # update sizes
-    for benchmark in new_benchmark_set["benchmarks"].values():
+    for benchmark in benchmark_set["benchmarks"]:
         if "sizes" not in benchmark:
             sizes = {
                 key: benchmark.pop(key) for key in (
@@ -77,11 +30,8 @@ def update_benchmark_file(benchmark_set_file: tp.Union[pathlib.Path, str]):
 
             benchmark["sizes"] = sizes
 
-    benchmarks = new_benchmark_set.pop("benchmarks")
-    new_benchmark_set["benchmarks"] = list(benchmarks.values())
-
     with open(benchmark_set_file, "w") as fd:
-        json.dump(new_benchmark_set, fd)
+        json.dump(benchmark_set, fd)
 
 
 if __name__ == "__main__":
