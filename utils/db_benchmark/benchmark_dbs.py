63efc4ddd3fb8cdf0cdabd77297b0de69decee8b
Roman Zlobin <trlkrot@gmail.com>
2023-07-07 00:38:23 +0300

    Revert "remove partial from benchmark_dbs.py"
    This reverts commit f52931a4

--- utils/db_benchmark/benchmark_dbs.py
+++ utils/db_benchmark/benchmark_dbs.py
@@ -4,15 +4,61 @@ Benchmark DBs
 This module contains config presets for benchmarks.
 """
 import pathlib
+import typing as tp
 from platform import system
 
-from dff.utils.benchmark.context_storage import (
-    save_results_to_file,
-    BenchmarkConfig,
-    benchmark_all,
-    BenchmarkCase,
-    DBFactory,
-)
+from dff.utils.benchmark.context_storage import save_results_to_file, BenchmarkCase, DBFactory, BenchmarkConfig
+
+
+# partial-specific logic
+def get_cases(
+    db_uris: tp.Dict[str, str],
+    case_name_postfix: str = "",
+    benchmark_config: BenchmarkConfig = BenchmarkConfig(),
+    description: str = "",
+):
+    benchmark_cases = []
+    for db, uri in db_uris.items():
+        benchmark_cases.append(
+            BenchmarkCase(
+                name=db + "-dev" + case_name_postfix,
+                db_factory=DBFactory(uri=uri, factory_module="dff.context_storages_old"),
+                benchmark_config=benchmark_config,
+                description=description,
+            )
+        )
+        benchmark_cases.append(
+            BenchmarkCase(
+                name=db + "-partial" + case_name_postfix,
+                db_factory=DBFactory(uri=uri),
+                benchmark_config=benchmark_config,
+                description=description,
+            )
+        )
+    return benchmark_cases
+
+
+def benchmark_all(
+    file: tp.Union[str, pathlib.Path],
+    name: str,
+    description: str,
+    db_uris: tp.Dict[str, str],
+    case_name_postfix: str = "",
+    benchmark_config: BenchmarkConfig = BenchmarkConfig(),
+    exist_ok: bool = False,
+):
+    save_results_to_file(
+        get_cases(
+            db_uris,
+            case_name_postfix,
+            benchmark_config=benchmark_config,
+            description=description,
+        ),
+        file,
+        name,
+        description,
+        exist_ok=exist_ok
+    )
 
 
 # create dir and files
@@ -24,13 +70,13 @@ sqlite_separator = "///" if system() == "Windows" else "////"
 dbs = {
     # "JSON": "json://dbs/json.json",
     # "Pickle": "pickle://dbs/pickle.pkl",
-    "Shelve": "shelve://dbs/shelve",
+    # "Shelve": "shelve://dbs/shelve",
     "PostgreSQL": "postgresql+asyncpg://postgres:pass@localhost:5432/test",
     "MongoDB": "mongodb://admin:pass@localhost:27017/admin",
-    "Redis": "redis://:pass@localhost:6379/0",
+    # "Redis": "redis://:pass@localhost:6379/0",
     "MySQL": "mysql+asyncmy://root:pass@localhost:3307/test",
     "SQLite": f"sqlite+aiosqlite:{sqlite_separator}{sqlite_file.absolute()}",
-    # "YDB": "grpc://localhost:2136/local",
+    "YDB": "grpc://localhost:2136/local",
 }
 
 # benchmark
@@ -86,47 +132,38 @@ benchmark_all(
 
 save_results_to_file(
     [
-        *[
-            BenchmarkCase(
-                db_factory=DBFactory(uri=uri),
-                name=name + "-long-dialog-len",
-                benchmark_config=BenchmarkConfig(
-                    context_num=10,
-                    from_dialog_len=10000,
-                    to_dialog_len=10050,
-                ),
-                description="Benchmark with very long dialog len.",
-            )
-            for name, uri in dbs.items()
-        ],
-        *[
-            BenchmarkCase(
-                db_factory=DBFactory(uri=uri),
-                name=name + "-long-message-len",
-                benchmark_config=BenchmarkConfig(
-                    context_num=10,
-                    from_dialog_len=1,
-                    to_dialog_len=3,
-                    message_dimensions=(10000, 1),
-                ),
-                description="Benchmark with messages containing many keys.",
-            )
-            for name, uri in dbs.items()
-        ],
-        *[
-            BenchmarkCase(
-                db_factory=DBFactory(uri=uri),
-                name=name + "-long-misc-len",
-                benchmark_config=BenchmarkConfig(
-                    context_num=10,
-                    from_dialog_len=1,
-                    to_dialog_len=3,
-                    misc_dimensions=(10000, 1),
-                ),
-                description="Benchmark with misc containing many keys.",
-            )
-            for name, uri in dbs.items()
-        ],
+        *get_cases(
+            db_uris=dbs,
+            case_name_postfix="-long-dialog-len",
+            benchmark_config=BenchmarkConfig(
+                context_num=10,
+                from_dialog_len=10000,
+                to_dialog_len=10050,
+            ),
+            description="Benchmark with very long dialog len."
+        ),
+        *get_cases(
+            db_uris=dbs,
+            case_name_postfix="-long-message-len",
+            benchmark_config=BenchmarkConfig(
+                context_num=10,
+                from_dialog_len=1,
+                to_dialog_len=3,
+                message_dimensions=(10000, 1),
+            ),
+            description="Benchmark with messages containing many keys."
+        ),
+        *get_cases(
+            db_uris=dbs,
+            case_name_postfix="-long-misc-len",
+            benchmark_config=BenchmarkConfig(
+                context_num=10,
+                from_dialog_len=1,
+                to_dialog_len=3,
+                misc_dimensions=(10000, 1),
+            ),
+            description="Benchmark with misc containing many keys."
+        ),
     ],
     file=benchmark_dir / "extremes.json",
     name="Extreme",
