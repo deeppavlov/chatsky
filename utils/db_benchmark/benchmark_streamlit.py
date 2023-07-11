4e2738156779f45da87d04321af71d7a1ca2e5c4
Roman Zlobin <trlkrot@gmail.com>
2023-07-12 02:20:02 +0300

    add password protection

--- utils/db_benchmark/benchmark_streamlit.py
+++ utils/db_benchmark/benchmark_streamlit.py
@@ -50,6 +50,39 @@ UPLOAD_FILES_DIR = Path("uploaded_benchmarks")
 UPLOAD_FILES_DIR.mkdir(exist_ok=True)
 
 
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["password"] == st.secrets["password"]:
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # don't store password
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        # First run, show input for password.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        return False
+    elif not st.session_state["password_correct"]:
+        # Password not correct, show input + error.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        st.error("😕 Password incorrect")
+        return False
+    else:
+        # Password correct.
+        return True
+
+
+if not check_password():
+    st.stop()
+
+
 if not BENCHMARK_RESULTS_FILES.exists():
     with open(BENCHMARK_RESULTS_FILES, "w", encoding="utf-8") as fd:
         json.dump([], fd)
