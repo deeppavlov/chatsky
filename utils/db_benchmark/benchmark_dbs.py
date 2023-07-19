8c07fa97e6c6a6008185cb6ee5364d37257f381e
Roman Zlobin <trlkrot@gmail.com>
2023-07-19 12:24:12 +0300

    add delays to benchmarks

--- utils/db_benchmark/benchmark_dbs.py
+++ utils/db_benchmark/benchmark_dbs.py
@@ -5,6 +5,7 @@ This module contains config presets for benchmarks.
 """
 import pathlib
 import typing as tp
+import json
 from platform import system
 
 from dff.utils.benchmark.context_storage import save_results_to_file, BenchmarkCase, DBFactory, BenchmarkConfig
@@ -18,14 +19,14 @@ def get_cases(
 ):
     benchmark_cases = []
     for config_name, config in benchmark_configs.items():
-        benchmark_cases.append(
-            BenchmarkCase(
-                name=config_name + "-dev",
-                db_factory=DBFactory(uri=db_uri, factory_module="dff.context_storages_old"),
-                benchmark_config=config,
-                description=description,
-            )
-        )
+        # benchmark_cases.append(
+        #     BenchmarkCase(
+        #         name=config_name + "-dev",
+        #         db_factory=DBFactory(uri=db_uri, factory_module="dff.context_storages_old"),
+        #         benchmark_config=config,
+        #         description=description,
+        #     )
+        # )
         benchmark_cases.append(
             BenchmarkCase(
                 name=config_name + "-partial",
@@ -65,15 +66,15 @@ sqlite_file.touch(exist_ok=True)
 sqlite_separator = "///" if system() == "Windows" else "////"
 
 dbs = {
-    "JSON": "json://dbs/json.json",
-    "Pickle": "pickle://dbs/pickle.pkl",
+    # "JSON": "json://dbs/json.json",
+    # "Pickle": "pickle://dbs/pickle.pkl",
     "Shelve": "shelve://dbs/shelve",
-    "PostgreSQL": "postgresql+asyncpg://postgres:pass@localhost:5432/test",
-    "MongoDB": "mongodb://admin:pass@localhost:27017/admin",
-    "Redis": "redis://:pass@localhost:6379/0",
-    "MySQL": "mysql+asyncmy://root:pass@localhost:3307/test",
-    "SQLite": f"sqlite+aiosqlite:{sqlite_separator}{sqlite_file.absolute()}",
-    "YDB": "grpc://localhost:2136/local",
+    # "PostgreSQL": "postgresql+asyncpg://postgres:pass@localhost:5432/test",
+    # "MongoDB": "mongodb://admin:pass@localhost:27017/admin",
+    # "Redis": "redis://:pass@localhost:6379/0",
+    # "MySQL": "mysql+asyncmy://root:pass@localhost:3307/test",
+    # "SQLite": f"sqlite+aiosqlite:{sqlite_separator}{sqlite_file.absolute()}",
+    # "YDB": "grpc://localhost:2136/local",
 }
 
 # benchmark
@@ -82,48 +83,71 @@ benchmark_dir = pathlib.Path("benchmarks")
 benchmark_dir.mkdir(exist_ok=True)
 
 
+configs = {
+    "large-misc": BenchmarkConfig(
+        from_dialog_len=1,
+        to_dialog_len=50,
+        message_dimensions=(3, 5, 6, 5, 3),
+        misc_dimensions=(2, 4, 3, 8, 100),
+    ),
+    "short-messages": BenchmarkConfig(
+        from_dialog_len=500,
+        to_dialog_len=550,
+        message_dimensions=(2, 30),
+        misc_dimensions=(0, 0),
+    ),
+    "default": BenchmarkConfig(),
+    "large-misc-long-dialog": BenchmarkConfig(
+        from_dialog_len=500,
+        to_dialog_len=550,
+        message_dimensions=(3, 5, 6, 5, 3),
+        misc_dimensions=(2, 4, 3, 8, 100),
+    ),
+    "very-long-dialog-len": BenchmarkConfig(
+        context_num=10,
+        from_dialog_len=10000,
+        to_dialog_len=10050,
+    ),
+    "very-long-message-len": BenchmarkConfig(
+        context_num=10,
+        from_dialog_len=1,
+        to_dialog_len=3,
+        message_dimensions=(10000, 1),
+    ),
+    "very-long-misc-len": BenchmarkConfig(
+        context_num=10,
+        from_dialog_len=1,
+        to_dialog_len=3,
+        misc_dimensions=(10000, 1),
+    ),
+}
+
+
+delays = {
+    "-0-delay": 0,
+    "-0.1-delay": 0.1,
+    "-0.5-delay": 0.5,
+}
+
+
+def delay_config(conf: BenchmarkConfig, delay):
+    old_conf = json.loads(conf.json())
+    old_conf.pop("delay", None)
+    new_conf = BenchmarkConfig(**old_conf, delay=delay)
+    return new_conf
+
+
+delayed_configs = {
+    key1 + key2: delay_config(configs[key1], delays[key2])
+    for key1 in configs
+    for key2 in delays
+}
+
 for db_name, db_uri in dbs.items():
     benchmark_all(
-        benchmark_dir / f"{db_name}.json",
-        db_name,
-        description="Basic configs",
+        benchmark_dir / f"delay-{db_name}.json",
+        "Delay comparison",
+        description="Compare different delays between db operations",
         db_uri=db_uri,
-        benchmark_configs={
-            "large-misc": BenchmarkConfig(
-                from_dialog_len=1,
-                to_dialog_len=50,
-                message_dimensions=(3, 5, 6, 5, 3),
-                misc_dimensions=(2, 4, 3, 8, 100),
-            ),
-            "short-messages": BenchmarkConfig(
-                from_dialog_len=500,
-                to_dialog_len=550,
-                message_dimensions=(2, 30),
-                misc_dimensions=(0, 0),
-            ),
-            "default": BenchmarkConfig(),
-            "large-misc-long-dialog": BenchmarkConfig(
-                from_dialog_len=500,
-                to_dialog_len=550,
-                message_dimensions=(3, 5, 6, 5, 3),
-                misc_dimensions=(2, 4, 3, 8, 100),
-            ),
-            "very-long-dialog-len": BenchmarkConfig(
-                context_num=10,
-                from_dialog_len=10000,
-                to_dialog_len=10050,
-            ),
-            "very-long-message-len": BenchmarkConfig(
-                context_num=10,
-                from_dialog_len=1,
-                to_dialog_len=3,
-                message_dimensions=(10000, 1),
-            ),
-            "very-long-misc-len": BenchmarkConfig(
-                context_num=10,
-                from_dialog_len=1,
-                to_dialog_len=3,
-                misc_dimensions=(10000, 1),
-            ),
-        },
+        benchmark_configs=delayed_configs,
     )
