04a1700274a5a2e513ee2b6019f7859e2f7d147e
Roman Zlobin <trlkrot@gmail.com>
2023-08-09 11:29:49 +0300

    Fail tests on skip (#192)
    * add test markers
    
    * fail skipped tests
    
    * lowercase none
    
    * allow skipping all for no_deps test
    
    * add comment in makefile
    
    * add info about special keywords

--- tests/context_storages/test_dbs.py
+++ tests/context_storages/test_dbs.py
@@ -100,6 +100,7 @@ def test_pickle(testing_file, testing_context, context_id):
 
 @pytest.mark.skipif(not MONGO_ACTIVE, reason="Mongodb server is not running")
 @pytest.mark.skipif(not mongo_available, reason="Mongodb dependencies missing")
+@pytest.mark.docker
 def test_mongo(testing_context, context_id):
     if system() == "Windows":
         pytest.skip()
@@ -117,6 +118,7 @@ def test_mongo(testing_context, context_id):
 
 @pytest.mark.skipif(not REDIS_ACTIVE, reason="Redis server is not running")
 @pytest.mark.skipif(not redis_available, reason="Redis dependencies missing")
+@pytest.mark.docker
 def test_redis(testing_context, context_id):
     db = context_storage_factory("redis://{}:{}@localhost:6379/{}".format("", os.getenv("REDIS_PASSWORD"), "0"))
     generic_test(db, testing_context, context_id)
@@ -125,6 +127,7 @@ def test_redis(testing_context, context_id):
 
 @pytest.mark.skipif(not POSTGRES_ACTIVE, reason="Postgres server is not running")
 @pytest.mark.skipif(not postgres_available, reason="Postgres dependencies missing")
+@pytest.mark.docker
 def test_postgres(testing_context, context_id):
     db = context_storage_factory(
         "postgresql+asyncpg://{}:{}@localhost:5432/{}".format(
@@ -147,6 +150,7 @@ def test_sqlite(testing_file, testing_context, context_id):
 
 @pytest.mark.skipif(not MYSQL_ACTIVE, reason="Mysql server is not running")
 @pytest.mark.skipif(not mysql_available, reason="Mysql dependencies missing")
+@pytest.mark.docker
 def test_mysql(testing_context, context_id):
     db = context_storage_factory(
         "mysql+asyncmy://{}:{}@localhost:3307/{}".format(
@@ -161,6 +165,7 @@ def test_mysql(testing_context, context_id):
 
 @pytest.mark.skipif(not YDB_ACTIVE, reason="YQL server not running")
 @pytest.mark.skipif(not ydb_available, reason="YDB dependencies missing")
+@pytest.mark.docker
 def test_ydb(testing_context, context_id):
     db = context_storage_factory(
         "{}{}".format(
