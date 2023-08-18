4263b7e21c3e8c9123b5ac3a9b738274b93bb405
Roman Zlobin <trlkrot@gmail.com>
2023-08-18 03:37:06 +0300

    move databases inside the benchmark_dir

--- utils/db_benchmark/benchmark_dbs.py
+++ utils/db_benchmark/benchmark_dbs.py
@@ -9,16 +9,23 @@ from platform import system
 from dff.utils.db_benchmark import benchmark_all, basic_configurations
 
 
+# benchmarks will be saved to this directory
+benchmark_dir = Path("benchmarks")
+
+benchmark_dir.mkdir(exist_ok=True)
+
+
 # these files are required for file-based dbs
-Path("dbs").mkdir(exist_ok=True)
-sqlite_file = Path("dbs/sqlite.db")
+db_path = benchmark_dir / "dbs"
+db_path.mkdir(exist_ok=True)
+sqlite_file = db_path / "sqlite.db"
 sqlite_file.touch(exist_ok=True)
 sqlite_separator = "///" if system() == "Windows" else "////"
 
 dbs = {
-    "JSON": "json://dbs/json.json",
-    "Pickle": "pickle://dbs/pickle.pkl",
-    "Shelve": "shelve://dbs/shelve",
+    "JSON": f"json://{db_path}/json.json",
+    "Pickle": f"pickle://{db_path}/pickle.pkl",
+    "Shelve": f"shelve://{db_path}/shelve",
     "PostgreSQL": "postgresql+asyncpg://postgres:pass@localhost:5432/test",
     "MongoDB": "mongodb://admin:pass@localhost:27017/admin",
     "Redis": "redis://:pass@localhost:6379/0",
@@ -26,11 +33,6 @@ dbs = {
     "SQLite": f"sqlite+aiosqlite:{sqlite_separator}{sqlite_file.absolute()}",
     "YDB": "grpc://localhost:2136/local",
 }
-
-# benchmarks will be saved to this directory
-benchmark_dir = Path("benchmarks")
-
-benchmark_dir.mkdir(exist_ok=True)
 
 
 for db_name, db_uri in dbs.items():
