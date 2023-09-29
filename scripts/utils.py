4d3f23ed1b401f343088884b62d4bd6542a8c804
pseusys <shveitsar215@gmail.com>
2023-09-29 11:28:24 +0200

    docker quiet + containers always down

--- scripts/utils.py
+++ scripts/utils.py
@@ -9,9 +9,13 @@ from wrapt import decorator
 def docker_client(wrapped: Callable[[Optional[DockerClient]], None], _, __, ___):
     if "linux" in sys.platform:
         docker = DockerClient(compose_files=["docker-compose.yml"])
-        docker.compose.up(detach=True, wait=True)
-        result = wrapped(docker)
-        docker.compose.down(remove_orphans=False)
+        docker.compose.up(detach=True, wait=True, quiet=True)
+        try:
+            result = wrapped(docker)
+        except:
+            result = 1
+        finally:
+            docker.compose.down(remove_orphans=False, quiet=True)
     else:
         result = wrapped(None)
     return result
