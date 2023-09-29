0fbb421045db738733911a4108ba55c3b384a1d0
pseusys <shveitsar215@gmail.com>
2023-09-29 11:39:37 +0200

    specified exception

--- scripts/utils.py
+++ scripts/utils.py
@@ -10,12 +10,16 @@ def docker_client(wrapped: Callable[[Optional[DockerClient]], None], _, __, ___)
     if "linux" in sys.platform:
         docker = DockerClient(compose_files=["docker-compose.yml"])
         docker.compose.up(detach=True, wait=True, quiet=True)
+        error = None
         try:
             result = wrapped(docker)
-        except:
+        except Exception as e:
             result = 1
+            error = e
         finally:
             docker.compose.down(remove_orphans=False, quiet=True)
+        if error is not None:
+            raise error
     else:
         result = wrapped(None)
     return result
