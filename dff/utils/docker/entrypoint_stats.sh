9b2d45fedc4377bb8af70e1fe1575c77cf3e2c39
ruthenian8 <ruthenian8@gmail.com>
2023-10-27 10:15:16 +0300

    Fix/dashboard (#227)
    * update dashboard workflow
    
    * run on workflow_dispatch
    
    * trigger on PR to dev for debugging
    
    * update build push action
    
    * update context && file
    
    * remove trigger on PR
    
    * add sample data provider
    
    * Update dashboard, finalize data provider
    
    * Add extractors for requests and responses as default instrumentors
    
    * add 'get_last_request', 'get_last_response'; add charts for requests and responses; update test
    
    * Update sections in superset_guide; update extractor functions tutor
    
    * doc build fix: Change functions to func names in __all__
    
    * Apply suggestions by RLKRO from code review
    
    Co-authored-by: Roman Zlobin <trlkrot@gmail.com>
    
    * intermediary update; update data provider tutor; update charts;
    
    * Apply suggestions from code review by RLKRO
    
    Co-authored-by: Roman Zlobin <trlkrot@gmail.com>
    
    * Upload annotations.png via GUI
    
    * Update requests & responses charts; update superset_guide
    
    * expand the Superset guide with chart creation instructions;Add links to Superset documentation
    
    * update graph plot; update dataset to get flow lag; update data provider
    
    * update guide; move sample data collector to /utils
    
    * update links to avoid sphinx warnings
    
    * update dashboard description; update tutorials
    
    * update superset guide
    
    * update label extractor; add infile parameter to main; remove link to sample_data_provider
    
    * update tests
    
    * update format
    
    * minor doc improvements
    
    * update dashboard datasource config
    
    * add dff_stats dataset; link charts to dff_stats dataset
    
    * update tests and documentation to reflect config changes
    
    * update formatting
    
    * add the test configuration file
    
    * move get_current_label to after_handler in sample_data_provider.py
    
    * update sql with if statements
    
    * specify healthcheck for clickhouse and wait for healthcheck with otlp; make a variable for superset metadata db; adjust dockerfile to use configuration overrides
    
    * add dashboard-metadata docker image; adjust to run on port 5433
    
    * Update .github/workflows/update_dashboard.yml
    
    Apply suggestions from @RLKRo
    
    Co-authored-by: Roman Zlobin <trlkrot@gmail.com>
    
    * Update dff/stats/cli.py
    
    Apply suggestions to label_lag/flow_lag
    
    Co-authored-by: Roman Zlobin <trlkrot@gmail.com>
    
    * remove query statements from dashboard files; remove filter values from charts; update docker-compose healthcheck; apply formatting
    
    * wait for pg database; sleep to avoid process conflict
    
    * Apply suggestions by @RLKRo
    
    Apply suggestions by @RLKRo
    
    Co-authored-by: Roman Zlobin <trlkrot@gmail.com>
    
    * Update config files; Update sql expressions in CLI; update healthcheck expression; create volume for dashboard metadata
    
    * convert request_id to int in final_nodes
    
    * remove sql in final_nodes
    
    * update history filter to numerical range type & change datasource to node_stats
    
    * remove utils include from manifest
    
    ---------
    
    Co-authored-by: Roman Zlobin <trlkrot@gmail.com>

--- dff/utils/docker/entrypoint_stats.sh
+++ dff/utils/docker/entrypoint_stats.sh
@@ -2,6 +2,7 @@
 export SERVER_THREADS_AMOUNT=8
 set -m
 nohup /bin/bash /usr/bin/run-server.sh &
+sleep 5
 superset fab create-admin --firstname superset --lastname admin --username $SUPERSET_USERNAME --email admin@admin.com --password $SUPERSET_PASSWORD
 superset db upgrade
 superset init
