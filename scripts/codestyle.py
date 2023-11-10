da5fe0001c156f7941eb4f14a448a78f7292fa94
pseusys <shveitsar215@gmail.com>
2023-11-10 08:57:46 +0100

    codestyle comments added

--- scripts/codestyle.py
+++ scripts/codestyle.py
@@ -15,22 +15,19 @@ def _get_paths(paths: List[str]) -> List[pathlib.Path]:
     return [path for dir in paths for path in pathlib.Path(dir).glob("**/*.py")]
 
 
-def lint() -> int:
+def _run_lint_and_mypy() -> int:
     lint_result = 0
+    # E24 prohibits from having "," at the end of a line, W503 prohibits from starting a line with binary operator
     flake8_configs = ["--select=E,W,F", "--ignore=E24,W503"]
     lint_result += flake_main([f"--max-line-length={_STANDARD_PATHS_LEN}"] + flake8_configs + _STANDARD_PATHS)
     lint_result += flake_main([f"--max-line-length={_SHORT_PATHS_LEN}"] + flake8_configs + _SHORT_PATHS)
 
-    would_format = format(False)
-    if would_format == 1:
-        print(("=" * 38) + "\nBad formatting? Run: poetry run format\n" + ("=" * 38))
-
     # TODO: Add mypy testing
     # @mypy . --exclude venv*,build
-    return lint_result or would_format
+    return lint_result
 
 
-def format(modify: bool = True) -> int:
+def _run_black(modify: bool) -> int:
     report = black.Report(check=not modify, quiet=False)
     write = black.WriteBack.YES if modify else black.WriteBack.CHECK
     for path in _get_paths(_STANDARD_PATHS):
@@ -40,3 +37,15 @@ def format(modify: bool = True) -> int:
         mode = black.Mode(line_length=_SHORT_PATHS_LEN)
         black.reformat_one(path, False, write, mode, report)
     return report.return_code
+
+
+def lint() -> int:
+    lint_result = _run_lint_and_mypy()
+    would_format = _run_black(False)
+    if would_format == 1:
+        print(("=" * 38) + "\nBad formatting? Run: poetry run format\n" + ("=" * 38))
+    return lint_result or would_format
+
+
+def format() -> int:
+    return _run_black(True)
