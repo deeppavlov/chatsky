6e0307fca4c18c65d15ddc9983385d71ff86f322
Roman Zlobin <trlkrot@gmail.com>
2023-12-11 00:32:05 +0300

    fix tutorial function signatures

--- tutorials/script/core/3_responses.py
+++ tutorials/script/core/3_responses.py
@@ -34,7 +34,7 @@ The response can be set by Callable or *Message:
 
 * Callable objects. If the object is callable it must have a special signature:
 
-        func(ctx: Context, pipeline: Pipeline) -> Any
+        func(ctx: Context, pipeline: Pipeline) -> Message
 
 * *Message objects. If the object is *Message
     it will be returned by the agent as a response.
