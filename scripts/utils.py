9dce7b5a2956e534874f2b0c28ae22a9e330a9d8
Roman Zlobin <trlkrot@gmail.com>
2023-12-14 17:17:17 +0300

    remove docker-compose usage

--- scripts/utils.py
+++ scripts/utils.py
@@ -9,7 +9,7 @@ from wrapt import decorator
 def docker_client(wrapped: Callable[[Optional[DockerClient]], int], _, __, ___) -> int:
     if "linux" in sys.platform:
         docker = DockerClient(
-            compose_files=["docker-compose.yml"],
+            compose_files=["compose.yml"],
             compose_profiles=["context_storage", "stats"],
         )
         docker.compose.up(detach=True, wait=True, quiet=True)
