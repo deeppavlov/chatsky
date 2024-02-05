bf1a81a1cb45ec957101e0acefebfce1cef09041
NotBioWaste905 <andruhabobr@gmail.com>
2024-02-06 00:53:09 +0300

    Added attachment management, started implementing keyboard

--- dff/messengers/vk.py
+++ dff/messengers/vk.py
@@ -6,10 +6,9 @@ that can be used to interact with the Telegram API.
 """
 from typing import Callable, Optional, Sequence, cast
 from pydantic import HttpUrl
-import asyncio
+# import asyncio
 
-# from vkbottle import API
-from vkwave.bots import SimpleLongPollBot, SimpleBotEvent
+from pyvkbot import Bot
 import requests
 
 from dff.messengers.common import MessengerInterface
@@ -19,23 +18,25 @@ from dff.script.core.context import Context
 from dff.script.core.message import  Audio, Button, Document, Image, Keyboard, Location, Message, Video
 
 
-def _create_keyboard(buttons: Sequence[Sequence[Button]]):
-    pass
+
 
 class _AbstractVKInterface(MessengerInterface):
     def __init__(self, token: str, group_id: str) -> None:
-        self.bot = SimpleLongPollBot(tokens=token, group_id=group_id)
-        self.bot.message_handler()(self.on_message)
+        self.bot = Bot(token=token, group_id=group_id)
+        self.bot.on('message', self.on_message)
     
+    def _create_keyboard(self, buttons: Sequence[Sequence[Button]]):
+        keyboard = Keyboard(inline=False)
+        keyboard.add_button(label="123")
     
-    def extract_message_from_vk(self, message_vk) -> Message:
+    def extract_message_from_vk(self, message_vk: dict[str, str]) -> Message:
         message = Message()
         message.attachments = list()
 
-        if message_vk.object.object.message.text is not None:
-            message.text = message_vk.object.object.message.text
-        
-        if message_vk.attachments is not []:
+        if message_vk["text"] is not None:
+            message.text = message_vk["text"]
+
+        if message_vk["attachments"] is not []:
             for element in message_vk["attachments"]:
                 match element["type"]:
                     case "photo":
@@ -43,39 +44,40 @@ class _AbstractVKInterface(MessengerInterface):
                     case "video":
                         pass
                     case "audio":
-                        pass
+                        message.attachments += [Audio(source=HttpUrl(element[element["type"]]['url']))]
                     case "doc":
-                        pass
+                        message.attachments += [Document(source=HttpUrl(element[element["type"]]['url']))]
                     case "link":
-                        pass
+                        message.text = element[element["type"]]['url']
                     case _:
                         pass
 
         return message
 
 
-    async def cast_message_to_vk_and_send(self, bot: SimpleLongPollBot, orig_message, message: Message) -> None:
+    def cast_message_to_vk_and_send(self, bot, orig_message, message: Message) -> None:
         if message.attachments is not None:
             pass
         if message.text is not None:
-            await orig_message.answer(message=message.text)
+            bot.send_message(peer_id=orig_message["peer_id"], text=message.text)
 
 
-    async def on_message(self, event: SimpleBotEvent) -> None:
-        message = await self.extract_message_from_vk(event)
+    def on_message(self, _, event) -> None:
+        message = self.extract_message_from_vk(event)
         message.original_message = event
-        resp = await self.callback(message, event.object.object.message.peer_id)
+        resp = self.callback(message, event["peer_id"])
         if resp.last_response is not None:
-            await self.cast_message_to_vk_and_send(self.bot, event, resp.last_response)
+            self.cast_message_to_vk_and_send(self.bot, event, resp.last_response)
 
 
-    async def connect(self, callback, *args, **kwargs):
+    def connect(self, callback, *args, **kwargs):
         self.callback = callback
 
-class PollingVKInterface(_AbstractVKInterface):
-    def __init__(self, token: str) -> None:
-        super().__init__(token)
 
-    async def connect(self, callback, *args, **kwargs):
-        await super().connect(callback, *args, **kwargs)
-        self.bot.run_forever()
+class PollingVKInterface(_AbstractVKInterface):
+    def __init__(self, token: str, group_id: str) -> None:
+        super().__init__(token, group_id)
+
+    def connect(self, callback, *args, **kwargs):
+        super().connect(callback, *args, **kwargs)
+        self.bot.start_polling(lambda: print("Bot started"))
