d25fd3086f2a12646dd41e5f9e9356114203750c
Roman Zlobin <RLKRo@proton.me>
2024-02-12 02:08:39 +0300

    qol: import script objects inside `dff/__init__.py`
    This allows importing `Message` directly from `dff`, among other import improvements.

--- dff/__init__.py
+++ dff/__init__.py
@@ -13,4 +13,22 @@ nest_asyncio.apply()
 from dff.pipeline import Pipeline
 from dff.script import Context, Script
 
+from dff.script.core.message import (
+    Location,
+    Attachment,
+    Audio,
+    Video,
+    Image,
+    Document,
+    Attachments,
+    Link,
+    Button,
+    Keyboard,
+    Message,
+    MultiMessage
+)
+import dff.script.responses as rsp
+import dff.script.labels as lbl
+import dff.script.conditions as cnd
+
 Script.model_rebuild()
