7115df95f8699f47d56ee824827edf59e7596002
Roman Zlobin <RLKRo@proton.me>
2024-04-08 18:32:00 +0300

    rewrite responses tests

--- tests/script/test_responses.py
+++ tests/script/test_responses.py
@@ -1,11 +1,30 @@
-# %%
+import random
+
+import pytest
+
 from dff.pipeline import Pipeline
-from dff.script import Context
+from dff.script import Message, Context
 from dff.script.responses import random_choice
 
 
-def test_response():
-    ctx = Context()
+class TestResponses:
     pipeline = Pipeline.from_script(script={"flow": {"node": {}}}, start_label=("flow", "node"))
-    for _ in range(10):
-        assert random_choice(["text1", "text2"])(ctx, pipeline) in ["text1", "text2"]
+
+    @pytest.fixture
+    def ctx(self):
+        yield Context()
+
+    def test_random_choice(self, ctx):
+        random.seed(0)
+
+        rsp = random_choice(
+            [
+                Message(text="1"),
+                Message(text="2"),
+                Message(text="3"),
+            ]
+        )
+
+        assert rsp(ctx, self.pipeline).text == "2"
+        assert rsp(ctx, self.pipeline).text == "2"
+        assert rsp(ctx, self.pipeline).text == "1"
