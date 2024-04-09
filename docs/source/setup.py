30c30c407a164fc42e6dcc1488f2c0593a425205
ZergLev <vasilev.sv@phystech.edu>
2024-04-09 23:01:36 +0500

    mistake patched

--- docs/source/setup.py
+++ docs/source/setup.py
@@ -53,5 +53,5 @@ def setup(root_dir: str, output_dir: str):
             ("dff.utils.turn_caching", "Caching"),
             ("dff.utils.db_benchmark", "DB Benchmark"),
         ],
-        root_dir=root_dir,
+        source_dir=(root_dir + "/docs/source"),
     )
