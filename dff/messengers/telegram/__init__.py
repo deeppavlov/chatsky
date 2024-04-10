1bfe52493224645d09195b6df9d20f85d94a7eac
pseusys <shveitsar215@gmail.com>
2024-04-11 01:05:38 +0200

    extras added

--- dff/messengers/telegram/__init__.py
+++ dff/messengers/telegram/__init__.py
@@ -1,12 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from .attachments import (
-    TelegramContact,
-    TelegramPoll,
-    TelegramAudio,
-    TelegramVideo,
-    TelegramAnimation,
-    TelegramImage,
-    TelegramDocument,
-)
 from .interface import PollingTelegramInterface, CallbackTelegramInterface
