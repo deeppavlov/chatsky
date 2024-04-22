68a6c6673bf636daac60bf514ad3c5a80d9bfdcb
Roman Zlobin <RLKRo@proton.me>
2024-04-22 20:31:12 +0300

    rewrite script tests

--- tests/script/test_script.py
+++ tests/script/test_script.py
@@ -1,140 +1,88 @@
-# %%
-import itertools
+import pytest
+from pydantic import ValidationError
 
 from dff.script import (
     GLOBAL,
-    TRANSITIONS,
-    RESPONSE,
-    MISC,
-    PRE_RESPONSE_PROCESSING,
-    PRE_TRANSITIONS_PROCESSING,
     Script,
     Node,
     Message,
 )
 
 
-def positive_test(samples, custom_class):
-    results = []
-    for sample in samples:
-        try:
-            res = custom_class(**sample)
-            results += [res]
-        except Exception as exception:
-            raise Exception(f"sample={sample} gets exception={exception}")
-    return results
+def _create_node(transition, condition, response, pre_response_processing, pre_transitions_processing, misc):
+    return Node(
+        transitions={transition: condition},
+        response=response,
+        pre_response_processing=pre_response_processing,
+        pre_transitions_processing=pre_transitions_processing,
+        misc=misc,
+    )
 
 
-def negative_test(samples, custom_class):
-    for sample in samples:
-        try:
-            custom_class(**sample)
-        except Exception:  # TODO: special type of exceptions
-            continue
-        raise Exception(f"sample={sample} can not be passed")
-
-
-def std_func(ctx, pipeline):
+def custom_user_function(ctx, pipeline):
     pass
 
 
-def test_node_creation():
-    node_creation(PRE_RESPONSE_PROCESSING)
+@pytest.mark.parametrize(
+    "transition", [custom_user_function, "node", ("flow", "node"), ("node", 2.5), ("flow", "node", 2)]
+)
+@pytest.mark.parametrize("condition", [custom_user_function])
+@pytest.mark.parametrize("response", [Message("text"), custom_user_function, None])
+@pytest.mark.parametrize("pre_response_processing", [{}, {"name": custom_user_function}])
+@pytest.mark.parametrize("pre_transitions_processing", [{}, {"name": custom_user_function}])
+@pytest.mark.parametrize("misc", [{}, {1: "var"}])
+def test_node_initialization(
+    transition, condition, response, pre_response_processing, pre_transitions_processing, misc
+):
+    _create_node(transition, condition, response, pre_response_processing, pre_transitions_processing, misc)
 
 
-def node_creation(pre_response_proc):
-    samples = {
-        "transition": [std_func, "node", ("flow", "node"), ("node", 2.0), ("flow", "node", 2.0)],
-        "condition": [std_func],
-        RESPONSE.name.lower(): [Message("text"), std_func, None],
-        pre_response_proc.name.lower(): [{}, {1: std_func}, None],
-        PRE_TRANSITIONS_PROCESSING.name.lower(): [{}, {1: std_func}, None],
-        MISC.name.lower(): [{}, {1: "var"}, None],
+@pytest.mark.parametrize(
+    "field,value",
+    [
+        ("transition", None),
+        ("transition", 0),
+        ("condition", None),
+        ("transition", 0),
+        ("response", 0),
+        ("pre_response_processing", 0),
+        ("pre_transitions_processing", 0),
+        ("misc", 0),
+    ],
+)
+def test_node_initialization_raises_validation_error(field, value):
+    erroneous_dict = {
+        "transition": custom_user_function,
+        "condition": custom_user_function,
+        "response": custom_user_function,
+        "pre_response_processing": custom_user_function,
+        "pre_transitions_processing": custom_user_function,
+        "misc": {},
+        field: value,
     }
-    samples = [
-        {
-            TRANSITIONS.name.lower(): {transition: condition},
-            RESPONSE.name.lower(): response,
-            pre_response_proc.name.lower(): pre_response,
-            PRE_TRANSITIONS_PROCESSING.name.lower(): pre_transitions,
-            MISC.name.lower(): misc,
-        }
-        for transition, condition, response, pre_response, pre_transitions, misc in itertools.product(
-            *list(samples.values())
-        )
-    ]
-    samples = [{k: v for k, v in sample.items() if v is not None} for sample in samples]
-    positive_test(samples, Node)
 
-    samples = {
-        "transition": [None],
-        "condition": [None, 123, "asdasd", 2.0, [], {}],
-        pre_response_proc.name.lower(): [123, "asdasd", 2.0, {1: None}, {1: 123}, {1: 2.0}, {1: []}, {1: {}}],
-        PRE_TRANSITIONS_PROCESSING.name.lower(): [123, "asdasd", 2.0, {1: None}, {1: 123}, {1: 2.0}, {1: []}, {1: {}}],
-        MISC.name.lower(): [123, "asdasd", 2.0],
-    }
-    samples = [
-        {
-            TRANSITIONS.name.lower(): {val if key == "transition" else "node": val if key == "condition" else std_func},
-            RESPONSE.name.lower(): val if key == RESPONSE.name.lower() else None,
-            pre_response_proc.name.lower(): val if key == pre_response_proc.name.lower() else None,
-            PRE_TRANSITIONS_PROCESSING.name.lower(): val if key == PRE_TRANSITIONS_PROCESSING.name.lower() else None,
-            MISC.name.lower(): val if key == MISC.name.lower() else None,
-        }
-        for key, values in samples.items()
-        for val in values
-    ]
-    samples = [{k: v for k, v in sample.items() if v is not None} for sample in samples]
-    negative_test(samples, Node)
+    with pytest.raises(ValidationError):
+        _create_node(**erroneous_dict)
 
 
-def node_test(node: Node):
-    assert list(node.transitions)[0] == ("", "node", float("-inf"))
-    assert callable(list(node.transitions.values())[0])
-    assert isinstance(node.pre_response_processing, dict)
-    assert isinstance(node.pre_transitions_processing, dict)
-    assert node.misc == {"key": "val"}
+class TestScript:
+    node = Node(misc={"key": "val"})
 
+    script = Script(script={GLOBAL: node})
 
-def test_node_exec():
-    # node = Node(
-    #     **{
-    #         TRANSITIONS.name.lower(): {"node": std_func},
-    #         RESPONSE.name.lower(): "text",
-    #         PROCESSING.name.lower(): {1: std_func},
-    #         PRE_TRANSITIONS_PROCESSING.name.lower(): {1: std_func},
-    #         MISC.name.lower(): {"key": "val"},
-    #     }
-    # )
-    # node_test(node)
-    node = Node(
-        **{
-            TRANSITIONS.name.lower(): {"node": std_func},
-            RESPONSE.name.lower(): Message("text"),
-            PRE_RESPONSE_PROCESSING.name.lower(): {1: std_func},
-            PRE_TRANSITIONS_PROCESSING.name.lower(): {1: std_func},
-            MISC.name.lower(): {"key": "val"},
-        }
-    )
-    node_test(node)
+    def test_global_creation(self):
+        assert self.script[GLOBAL][GLOBAL] == self.node
 
+    def test_methods(self):
+        assert self.script[GLOBAL] == {GLOBAL: self.node}
 
-def test_script():
-    script_test(PRE_RESPONSE_PROCESSING)
+        assert self.script.get(GLOBAL) == {GLOBAL: self.node}
+        assert self.script.get("", 1) == 1
 
+        assert tuple(self.script.keys()) == (GLOBAL,)
 
-def script_test(pre_response_proc):
-    node_template = {
-        TRANSITIONS: {"node": std_func},
-        RESPONSE: Message("text"),
-        pre_response_proc: {1: std_func},
-        PRE_TRANSITIONS_PROCESSING: {1: std_func},
-        MISC: {"key": "val"},
-    }
-    script = Script(script={GLOBAL: node_template.copy(), "flow": {"node": node_template.copy()}})
-    node_test(script[GLOBAL][GLOBAL])
-    node_test(script["flow"]["node"])
-    assert list(script.keys()) == [GLOBAL, "flow"]
-    assert len(script.values()) == 2
-    assert list(script) == [GLOBAL, "flow"]
-    assert len(list(script.items())) == 2
+        assert tuple(self.script.values()) == ({GLOBAL: self.node},)
+
+        assert tuple(self.script.items()) == ((GLOBAL, {GLOBAL: self.node}),)
+
+        assert tuple(iter(self.script)) == (GLOBAL,)
