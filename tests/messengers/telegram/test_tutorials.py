d30358b332bbb84b11e1d1667c9f135d9db14fa4
pseusys <shveitsar215@gmail.com>
2024-05-08 21:12:48 +0200

    dot path used instead of slash

--- tests/messengers/telegram/test_tutorials.py
+++ tests/messengers/telegram/test_tutorials.py
@@ -4,7 +4,7 @@ import pytest
 
 from tests.test_utils import get_path_from_tests_to_current_dir
 
-dot_path_to_addon = get_path_from_tests_to_current_dir(__file__)
+dot_path_to_addon = get_path_from_tests_to_current_dir(__file__, separator=".")
 happy_paths_file = Path(__file__).parent / "test_happy_paths.json"
 
 
