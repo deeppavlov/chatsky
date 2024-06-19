7d8c68c689697b7455605ceff4f78b873123f0e9
ZergLev <vasilev.sv@phystech.edu>
2024-06-19 15:42:25 +0500

    ContextLock() test added

--- tests/pipeline/test_messenger_interface.py
+++ tests/pipeline/test_messenger_interface.py
@@ -83,7 +83,7 @@ def test_echo_responses():
         def __init__(self):
             self.ctx_id = uuid.uuid4()
             self.requests = ["some request", "another request", "gkjln;s!", "foobarraboof"]
-            self.requests_copy = self.requests.copy()
+            self.expected_updates = self.requests.copy()
             self.received_updates = []
             # self.received_updates = self.requests.copy()
             self.obtained_updates = False
@@ -91,7 +91,7 @@ def test_echo_responses():
             super().__init__()
 
         def not_obtained_updates(self):
-            if len(self.received_updates) == len(self.requests_copy):
+            if len(self.received_updates) == len(self.expected_updates):
                 self.obtained_updates = True
             return not self.obtained_updates
 
@@ -125,7 +125,7 @@ def test_echo_responses():
     asyncio.run(new_pipeline.messenger_interface.run_in_foreground(new_pipeline, loop=interface.not_obtained_updates))
     print("run_in_foreground passed!")
     for i in range(4):
-        assert interface.requests_copy[i] == interface.received_updates[i]
+        assert interface.expected_updates[i] == interface.received_updates[i]
     assert interface.not_obtained_updates() == False
     """
     get_updates -> if not obtained_updates: [ctx_id, request]
@@ -135,6 +135,69 @@ def test_echo_responses():
     
     assert received_updates == expected
     """
+
+
+def test_context_lock():
+    def repeat_message_back(ctx: Context, _: Pipeline, *args, **kwargs):
+        return ctx.last_request
+    ECHO_SCRIPT = {
+        "echo_flow": {
+            "start_node": {
+                RESPONSE: repeat_message_back,
+                TRANSITIONS: {"start_node": cnd.true()},
+            },
+        }
+    }
+    # (respond=request)
+
+    """
+    requests_queue = asyncio.Queue()
+    for item in requests:
+        requests_queue.put(item)
+    """
+
+    class TestCLIInterface(PollingMessengerInterface):
+        def __init__(self):
+            self.obtained_updates = False
+            self.requests = [(0, "id: 1"), (0, "id: 2"), (1, "id: 3")]
+            self.expected_updates = [(0, "id: 1"), (1, "id: 3"), (0, "id: 2")]
+            self.received_updates = []
+            super().__init__()
+
+        def not_obtained_updates(self):
+            if len(self.received_updates) == len(self.expected_updates):
+                self.obtained_updates = True
+            return not self.obtained_updates
+
+        def _get_updates(self):
+            if len(self.requests) > 0:
+                request = self.requests.pop(0)
+                return [(request[0], Message(str(request[1])))]
+        
+        async def _respond(self, ctx_id, last_response):
+            print("response received!")
+            self.received_updates.append(str(last_response.text))
+            print("requests=", self.requests, ", received_updates=", self.received_updates, ", response=", last_response)
+            
+    async def _process_request(self, ctx_id, update: Message, pipeline: Pipeline):
+        # First worker hands over control to (0, id: 2) first, which immediately gives it back because of the ContextLock(). Then, id: 3 gets completed immediately, due to there not being any more 'await' statements there. What's important here is that worker for the "id: 2" couldn't start working on it's task immediately, because of ContextLock(), proving it's function.
+        context = await pipeline._run_pipeline(update, ctx_id)
+        if context.last_response.text == "id: 1":
+            await asyncio.sleep(0)
+        await self._respond(ctx_id, context.last_response)
+
+
+    new_pipeline = Pipeline.from_script(
+        ECHO_SCRIPT,
+        start_label=("echo_flow", "start_node"),
+        fallback_label=("echo_flow", "start_node"),
+        messenger_interface=TestCLIInterface()
+    )
+    interface = new_pipeline.messenger_interface
+    asyncio.run(new_pipeline.messenger_interface.run_in_foreground(new_pipeline, loop=interface.not_obtained_updates))
+    for i in range(3):
+        assert interface.expected_updates[i][1] == interface.received_updates[i]
+    assert interface.not_obtained_updates() == False
 
 
 def test_callback_messenger_interface(monkeypatch):
