fc98008c148ecfe506abe46f4ce7d8dbaff1db3d
Roman Zlobin <RLKRo@proton.me>
2024-06-25 14:31:01 +0300

    patch TG_BOT_TOKEN for tutorial tests

--- tests/messengers/telegram/test_tutorials.py
+++ tests/messengers/telegram/test_tutorials.py
@@ -21,7 +21,8 @@ happy_paths_file = Path(__file__).parent / "test_happy_paths.json"
     "tutorial_module_name",
     ["1_basic", "2_attachments", "3_advanced"],
 )
-def test_tutorials_memory(tutorial_module_name: str):
+def test_tutorials_memory(tutorial_module_name: str, monkeypatch):
+    monkeypatch.setenv("TG_BOT_TOKEN", "token")
     happy_path_data = loads(happy_paths_file.read_text())[tutorial_module_name]
     happy_path_steps = cast_dict_to_happy_step(happy_path_data)
     module = import_module(f"tutorials.{dot_path_to_addon}.{tutorial_module_name}")
