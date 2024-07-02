365a301b887de32313d236e4d0ed0a1654d78e3b
Roman Zlobin <RLKRo@proton.me>
2024-07-02 13:52:19 +0300

    patch attachment comparison to support different OSes

--- tests/messengers/telegram/test_tutorials.py
+++ tests/messengers/telegram/test_tutorials.py
@@ -5,6 +5,7 @@ from pathlib import Path
 import pytest
 
 from dff.messengers.telegram import telegram_available
+from dff.script.core.message import DataAttachment
 from tests.test_utils import get_path_from_tests_to_current_dir
 
 if telegram_available:
@@ -20,6 +21,17 @@ happy_paths_file = Path(__file__).parent / "test_happy_paths.json"
     ["1_basic", "2_attachments", "3_advanced"],
 )
 def test_tutorials(tutorial_module_name: str, monkeypatch):
+    def patched_data_attachment_eq(self: DataAttachment, other: DataAttachment):
+        first_copy = self.model_copy()
+        if first_copy.cached_filename is not None:
+            first_copy.cached_filename = first_copy.cached_filename.name
+        second_copy = other.model_copy()
+        if second_copy.cached_filename is not None:
+            second_copy.cached_filename = second_copy.cached_filename.name
+        return super(DataAttachment, first_copy).__eq__(second_copy)
+
+    monkeypatch.setattr(DataAttachment, "__eq__", patched_data_attachment_eq)
+
     monkeypatch.setenv("TG_BOT_TOKEN", "token")
     happy_path_data = loads(happy_paths_file.read_text())[tutorial_module_name]
     happy_path_steps = cast_dict_to_happy_step(happy_path_data)
