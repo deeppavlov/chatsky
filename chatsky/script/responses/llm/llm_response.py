6eb910d0a0712e7098e805e770ed2a41f65f1e9e
askatasuna <andruha.chirkin@gmail.com>
2024-07-22 12:21:48 +0300

    Added image processing, updated tutorial

--- chatsky/script/responses/llm/llm_response.py
+++ chatsky/script/responses/llm/llm_response.py
@@ -15,6 +15,9 @@ from langchain_mistralai import ChatMistralAI
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_core.output_parsers import StrOutputParser
 
+import base64
+import httpx
+
 from chatsky.script import Message, Context
 from chatsky.pipeline import Pipeline
 
@@ -27,7 +30,14 @@ class LLMResponse(BaseModel):
     This class acts as a wrapper over OpenaAI models.
     and handles message exchange between remote model and chatsky classes.
     """
-    def __init__(self, model: Union[ChatOpenAI, ChatAnthropic, ChatVertexAI, ChatCohere, ChatMistralAI], system_prompt: str="") -> None:
+
+    def __init__(
+        self,
+        model: Union[
+            ChatOpenAI, ChatAnthropic, ChatVertexAI, ChatCohere, ChatMistralAI
+        ],
+        system_prompt: str = "",
+    ) -> None:
         """
         :param model: Model object.
         :param system_prompt: System prompt for the model.
@@ -38,11 +48,30 @@ class LLMResponse(BaseModel):
         if system_prompt != "":
             self.messages.append(SystemMessage(content=system_prompt))
 
-
-    def respond(self, prompt: str=""):
+    def respond(self, prompt: str = ""):
         def inner_response(ctx: Context, _: Pipeline) -> Message:
-            self.messages.append(HumanMessage(prompt + '\n' + ctx.last_request.text))
+            if ctx.last_request.attachments != []:
+                content = [{"type": "text", "text": prompt + "\n" + ctx.last_request.text}]
+                for image in ctx.last_request.attachments:
+                    image_source = image.source
+                    if "http" in image_source:
+                        image_data = httpx.get(image_source).content
+                    else:
+                        with open(image_source, "rb") as image_file:
+                            image_data = image_file.read()
+                    image_b64 = base64.b64encode(image_data).decode("utf-8")
+                    extension = image_source.split(".")[-1]
+                    image_b64 = f"data:image/{extension};base64,{image_b64}"
+                    content.append(
+                        {"type": "image_url", "image_url": {"url": image_b64}}
+                    )
+
+                message = HumanMessage(content=content)
+            else:
+                message = HumanMessage(prompt + "\n" + ctx.last_request.text)
+            self.messages.append(message)
             result = self.parser.invoke(self.model.invoke(self.messages))
             self.messages.append(SystemMessage(result))
             return Message(text=result)
+
         return inner_response
--- tutorials/script/responses/llm_responses/1_basics.py
+++ tutorials/script/responses/llm_responses/1_basics.py
@@ -2,7 +2,7 @@
 """
 # LLM Responses: 1. Basics
 
-Using Chatky you can easily add LLM invocations to your script.
+Using Chatsky you can easily add LLM invocations to your script.
 In this tutorial we will see how to use LLM responses.
 Chatsky uses langchain under the hood to connect to the remote models.
 """
@@ -14,6 +14,7 @@ Chatsky uses langchain under the hood to connect to the remote models.
 # %%
 from chatsky.script import Message
 from chatsky.script.conditions import exact_match
+from chatsky.script.conditions import std_conditions as cnd
 from chatsky.script import RESPONSE, TRANSITIONS
 from chatsky.pipeline import Pipeline
 from chatsky.utils.testing import (
@@ -31,37 +32,49 @@ from langchain_openai import ChatOpenAI
 # %% [markdown]
 """
 Now we need to create a model object.
+Keep in mind, that if you instantiate model object outside of the script,
+it will be reused across all the nodes and therefore it will store all dialogue history.
+This is not advised if you are short on tokens or if you do not need to store all dialogue history.
+Alternatively you can instantiate model object inside of RESPONSE field in the nodes you need.
 """
 
 # %%
 model = LLMResponse(ChatOpenAI(model="gpt-3.5-turbo"), system_prompt="You are an experienced barista in a local coffeshop. Answer your customers questions about coffee and barista work.")
+
+# %% [markdown]
+"""
+Also you can pass images to the LLM, just pass them as attachments to your message.
+"""
 
 # %%
 toy_script = {
     "greeting_flow": {
         "start_node": {
             RESPONSE: Message(""),
-            TRANSITIONS: {"node1": exact_match("Hi")},
+            TRANSITIONS: {"greeting_node": exact_match("Hi")},
         },
-        "node1": {
-            RESPONSE: model.respond,
-            TRANSITIONS: {"node2": exact_match("i'm fine, how are you?")},
+        "greeting_node": {
+            RESPONSE: model.respond(),
+            TRANSITIONS: {"main_node": exact_match("i'm fine, how are you?")},
         },
-        "node2": {
-            RESPONSE: model.respond,
-            TRANSITIONS: {"node3": exact_match("Tell me about latte art.")},
+        "main_node": {
+            RESPONSE: model.respond(),
+            TRANSITIONS: {
+                "latte_art_node": exact_match("Tell me about latte art."),
+                "image_desc_node": exact_match("Tell me what coffee is it?")},
         },
-        "node3": {
-            RESPONSE: model.respond,
-            TRANSITIONS: {"node4": exact_match("Ok, goodbye.")},
+        "latte_art_node": {
+            RESPONSE: model.respond(prompt="PROMPT: pretend that you have never heard about latte art before."),
+            TRANSITIONS: {"image_desc_node": exact_match("Ok, goodbye.")},
         },
-        "node4": {
-            RESPONSE: model.respond,
-            TRANSITIONS: {"node1": exact_match("Hi")},
+        "image_desc_node": {
+            # we expect user to send some images of coffee.
+            RESPONSE: model.respond(prompt="PROMPT: user will give you some images of coffee. Describe them."),
+            TRANSITIONS: {"main_node": cnd.true()},
         },
         "fallback_node": {
             RESPONSE: Message("I didn't quite understand you..."),
-            TRANSITIONS: {"node1": exact_match("Hi")},
+            TRANSITIONS: {"main_node": cnd.true()},
         },
     }
 }
