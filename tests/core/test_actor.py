aca9a325849cdb6886e83560dc0f0e10001f3781
Roman Zlobin <RLKRo@proton.me>
2024-08-08 21:03:09 +0300

    add actor tests

--- tests/core/test_actor.py
+++ tests/core/test_actor.py
@@ -1,203 +1,229 @@
-# %%
+import asyncio
+
 import pytest
-from chatsky.pipeline import Pipeline
-from chatsky.script import (
-    TRANSITIONS,
-    RESPONSE,
-    GLOBAL,
-    LOCAL,
-    PRE_TRANSITIONS_PROCESSING,
-    PRE_RESPONSE_PROCESSING,
-    Context,
-    Message,
-)
-from chatsky.script.conditions import true
-from chatsky.script.labels import repeat
+
+from chatsky.core import BaseProcessing, BaseResponse
+from chatsky.core.node_label import AbsoluteNodeLabel
+from chatsky.core.service.actor import Actor, logger
+from chatsky.core.message import Message, MessageInitTypes
+from chatsky.core.context import Context
+from chatsky.core.script import Script
+from chatsky.core.keywords import RESPONSE, TRANSITIONS, PRE_TRANSITION, PRE_RESPONSE
 
 
-def positive_test(samples, custom_class):
-    results = []
-    for sample in samples:
-        try:
-            res = custom_class(**sample)
-            results += [res]
-        except Exception as exception:
-            raise Exception(f"sample={sample} gets exception={exception}")
-    return results
+@pytest.fixture()
+def object_factory():
+    def inner(**kwargs):
+        class Object:
+            pass
+
+        obj = Object()
+        obj.__dict__.update(kwargs)
+        return obj
+
+    return inner
 
 
-def negative_test(samples, custom_class):
-    for sample in samples:
-        try:
-            custom_class(**sample)
-        except Exception:  # TODO: special type of exceptions
-            continue
-        raise Exception(f"sample={sample} can not be passed")
-
-
-def std_func(ctx, pipeline):
-    pass
-
-
-def fake_label(ctx: Context, pipeline):
-    return ("flow", "node1", 1)
-
-
-def raised_response(ctx: Context, pipeline):
-    raise Exception("")
-
-
-@pytest.mark.asyncio
-async def test_actor():
-    try:
-        # fail of start label
-        Pipeline.from_script({"flow": {"node1": {}}}, start_label=("flow1", "node1"))
-        raise Exception("can not be passed: fail of start label")
-    except ValueError:
-        pass
-    try:
-        # fail of fallback label
-        Pipeline.from_script({"flow": {"node1": {}}}, start_label=("flow", "node1"), fallback_label=("flow1", "node1"))
-        raise Exception("can not be passed: fail of fallback label")
-    except ValueError:
-        pass
-    try:
-        # fail of missing node
-        Pipeline.from_script({"flow": {"node1": {TRANSITIONS: {"miss_node1": true()}}}}, start_label=("flow", "node1"))
-        raise Exception("can not be passed: fail of missing node")
-    except ValueError:
-        pass
-    try:
-        # fail of response returned Callable
-        pipeline = Pipeline.from_script(
-            {"flow": {"node1": {RESPONSE: lambda c, a: lambda x: 1, TRANSITIONS: {repeat(): true()}}}},
-            start_label=("flow", "node1"),
+class TestInit:
+    def test_successful_init(self):
+        actor = Actor(
+            script={"flow": {"start": {}, "fallback": {}}},
+            fallback_label=("flow", "fallback")
         )
-        ctx = Context()
-        await pipeline.actor(pipeline, ctx)
-        raise Exception("can not be passed: fail of response returned Callable")
-    except ValueError:
-        pass
 
-    # empty ctx stability
-    pipeline = Pipeline.from_script(
-        {"flow": {"node1": {TRANSITIONS: {"node1": true()}}}}, start_label=("flow", "node1")
-    )
-    ctx = Context()
-    await pipeline.actor(pipeline, ctx)
+        assert actor.fallback_label == AbsoluteNodeLabel(flow_name="flow", node_name="fallback")
 
-    # fake label stability
-    pipeline = Pipeline.from_script(
-        {"flow": {"node1": {TRANSITIONS: {fake_label: true()}}}}, start_label=("flow", "node1")
-    )
-    ctx = Context()
-    await pipeline.actor(pipeline, ctx)
+    def test_fallback_label_non_existent(self):
+        with pytest.raises(ValueError):
+            Actor(
+                script={"flow": {"start": {}, "fallback": {}}},
+                fallback_label=("flow", "node")
+            )
 
 
-limit_errors = {}
-
-
-def check_call_limit(limit: int = 1, default_value=None, label=""):
-    counter = 0
-
-    def call_limit_handler(ctx: Context, pipeline):
-        nonlocal counter
-        counter += 1
-        if counter > limit:
-            msg = f"calls are out of limits counterlimit={counter}/{limit} for {default_value} and {label}"
-            limit_errors[call_limit_handler] = msg
-        if default_value == "ctx":
-            return ctx
-        return default_value
-
-    return call_limit_handler
-
-
-@pytest.mark.asyncio
-async def test_call_limit():
-    script = {
-        GLOBAL: {
-            TRANSITIONS: {
-                check_call_limit(4, ("flow1", "node1", 0.0), "global label"): check_call_limit(4, True, "global cond")
-            },
-            PRE_TRANSITIONS_PROCESSING: {"tpg": check_call_limit(4, "ctx", "tpg")},
-            PRE_RESPONSE_PROCESSING: {"rpg": check_call_limit(4, "ctx", "rpg")},
-        },
-        "flow1": {
-            LOCAL: {
-                TRANSITIONS: {
-                    check_call_limit(2, ("flow1", "node1", 0.0), "local label for flow1"): check_call_limit(
-                        2, True, "local cond for flow1"
-                    )
-                },
-                PRE_TRANSITIONS_PROCESSING: {"tpl": check_call_limit(2, "ctx", "tpl")},
-                PRE_RESPONSE_PROCESSING: {"rpl": check_call_limit(3, "ctx", "rpl")},
-            },
+class TestRequestProcessing:
+    async def test_normal_execution(self, object_factory):
+        script = Script.model_validate({"flow": {
             "node1": {
-                RESPONSE: check_call_limit(1, Message("r1"), "flow1_node1"),
-                PRE_TRANSITIONS_PROCESSING: {"tp1": check_call_limit(1, "ctx", "flow1_node1_tp1")},
-                TRANSITIONS: {
-                    check_call_limit(1, ("flow1", "node2"), "cond flow1_node2"): check_call_limit(
-                        1,
-                        True,
-                        "cond flow1_node2",
-                    )
-                },
-                PRE_RESPONSE_PROCESSING: {"rp1": check_call_limit(1, "ctx", "flow1_node1_rp1")},
+                RESPONSE: "node1",
+                TRANSITIONS: [{"dst": "node2"}]
             },
             "node2": {
-                RESPONSE: check_call_limit(1, Message("r1"), "flow1_node2"),
-                PRE_TRANSITIONS_PROCESSING: {"tp1": check_call_limit(1, "ctx", "flow1_node2_tp1")},
-                TRANSITIONS: {
-                    check_call_limit(1, ("flow2", "node1"), "cond flow2_node1"): check_call_limit(
-                        1,
-                        True,
-                        "cond flow2_node1",
-                    )
-                },
-                PRE_RESPONSE_PROCESSING: {"rp1": check_call_limit(1, "ctx", "flow1_node2_rp1")},
+                RESPONSE: "node2",
+                TRANSITIONS: [{"dst": "node3"}]
             },
-        },
-        "flow2": {
-            LOCAL: {
-                TRANSITIONS: {
-                    check_call_limit(2, ("flow1", "node1", 0.0), "local label for flow2"): check_call_limit(
-                        2, True, "local cond for flow2"
-                    )
-                },
-                PRE_TRANSITIONS_PROCESSING: {"tpl": check_call_limit(2, "ctx", "tpl")},
-                PRE_RESPONSE_PROCESSING: {"rpl": check_call_limit(2, "ctx", "rpl")},
+            "node3": {
+                RESPONSE: "node3"
             },
-            "node1": {
-                RESPONSE: check_call_limit(1, Message("r1"), "flow2_node1"),
-                PRE_TRANSITIONS_PROCESSING: {"tp1": check_call_limit(1, "ctx", "flow2_node1_tp1")},
-                TRANSITIONS: {
-                    check_call_limit(1, ("flow2", "node2"), "label flow2_node2"): check_call_limit(
-                        1,
-                        True,
-                        "cond flow2_node2",
-                    )
-                },
-                PRE_RESPONSE_PROCESSING: {"rp1": check_call_limit(1, "ctx", "flow2_node1_rp1")},
-            },
-            "node2": {
-                RESPONSE: check_call_limit(1, Message("r1"), "flow2_node2"),
-                PRE_TRANSITIONS_PROCESSING: {"tp1": check_call_limit(1, "ctx", "flow2_node2_tp1")},
-                TRANSITIONS: {
-                    check_call_limit(1, ("flow1", "node1"), "label flow2_node2"): check_call_limit(
-                        1,
-                        True,
-                        "cond flow2_node2",
-                    )
-                },
-                PRE_RESPONSE_PROCESSING: {"rp1": check_call_limit(1, "ctx", "flow2_node2_rp1")},
-            },
-        },
-    }
-    # script = {"flow": {"node1": {TRANSITIONS: {"node1": true()}}}}
-    pipeline = Pipeline.from_script(script=script, start_label=("flow1", "node1"))
-    for i in range(4):
-        await pipeline._run_pipeline(Message("req1"), 0)
-    if limit_errors:
-        error_msg = repr(limit_errors)
-        raise Exception(error_msg)
+            "fallback": {
+                RESPONSE: "fallback"
+            }
+        }})
+
+        ctx = Context.init(start_label=("flow", "node1"))
+        actor = Actor(script, fallback_label=("flow", "fallback"))
+        ctx.framework_data.pipeline = object_factory(
+            parallelize_processing=True,
+            script=script
+        )
+
+        await actor(ctx)
+
+        assert ctx.labels == {
+            -1: AbsoluteNodeLabel(flow_name="flow", node_name="node1"),
+            0: AbsoluteNodeLabel(flow_name="flow", node_name="node2")
+        }
+        assert ctx.responses == {0: Message("node2")}
+
+    async def test_fallback_node(self, object_factory):
+        script = Script.model_validate({
+            "flow": {"node": {}, "fallback": {RESPONSE: "fallback"}}
+        })
+
+        ctx = Context.init(start_label=("flow", "node"))
+        actor = Actor(script, fallback_label=("flow", "fallback"))
+        ctx.framework_data.pipeline = object_factory(
+            parallelize_processing=True,
+            script=script
+        )
+
+        await actor(ctx)
+
+        assert ctx.labels == {
+            -1: AbsoluteNodeLabel(flow_name="flow", node_name="node"),
+            0: AbsoluteNodeLabel(flow_name="flow", node_name="fallback")
+        }
+        assert ctx.responses == {0: Message("fallback")}
+
+    @pytest.mark.parametrize("default_priority,result", [
+        (1, "node3"),
+        (2, "node2"),
+        (3, "node2"),
+    ])
+    async def test_default_priority(self, object_factory, default_priority, result):
+        script = Script.model_validate({
+            "flow": {"node1": {
+                TRANSITIONS: [{"dst": "node2"}, {"dst": "node3", "priority": 2}]
+            }, "node2": {}, "node3": {}, "fallback": {}}
+        })
+
+        ctx = Context.init(start_label=("flow", "node1"))
+        actor = Actor(script, fallback_label=("flow", "fallback"), default_priority=default_priority)
+        ctx.framework_data.pipeline = object_factory(
+            parallelize_processing=True,
+            script=script
+        )
+
+        await actor(ctx)
+        assert ctx.last_label.node_name == result
+
+    async def test_transition_exception_handling(self, object_factory, log_event_catcher):
+        log_list = log_event_catcher(logger, level="ERROR")
+
+        class MyProcessing(BaseProcessing):
+            async def func(self, ctx: Context) -> None:
+                ctx.framework_data.current_node = None
+
+        script = Script.model_validate({"flow": {"node": {PRE_TRANSITION: {"": MyProcessing()}}, "fallback": {}}})
+
+        ctx = Context.init(start_label=("flow", "node"))
+        actor = Actor(script, fallback_label=("flow", "fallback"))
+        ctx.framework_data.pipeline = object_factory(
+            parallelize_processing=True,
+            script=script
+        )
+
+        await actor(ctx)
+
+        assert ctx.last_label.node_name == "fallback"
+        assert log_list[0].msg == "Exception occurred during transition processing."
+        assert str(log_list[0].exc_info[1]) == "Current node is not set."
+
+    async def test_empty_response(self, object_factory, log_event_catcher):
+        log_list = log_event_catcher(logger, level="DEBUG")
+
+        script = Script.model_validate({"flow": {"node": {}}})
+
+        ctx = Context.init(start_label=("flow", "node"))
+        actor = Actor(script, fallback_label=("flow", "node"))
+        ctx.framework_data.pipeline = object_factory(
+            parallelize_processing=True,
+            script=script
+        )
+
+        await actor(ctx)
+
+        assert ctx.responses == {0: Message()}
+        assert log_list[-1].msg == "Node has empty response."
+
+    async def test_bad_response(self, object_factory, log_event_catcher):
+        log_list = log_event_catcher(logger, level="DEBUG")
+
+        class MyResponse(BaseResponse):
+            async def func(self, ctx: Context) -> MessageInitTypes:
+                return None
+
+        script = Script.model_validate({"flow": {"node": {RESPONSE: MyResponse()}}})
+
+        ctx = Context.init(start_label=("flow", "node"))
+        actor = Actor(script, fallback_label=("flow", "node"))
+        ctx.framework_data.pipeline = object_factory(
+            parallelize_processing=True,
+            script=script
+        )
+
+        await actor(ctx)
+
+        assert ctx.responses == {0: Message()}
+        assert log_list[-1].msg == "Response was not produced."
+
+    async def test_response_exception_handling(self, object_factory, log_event_catcher):
+        log_list = log_event_catcher(logger, level="ERROR")
+
+        class MyProcessing(BaseProcessing):
+            async def func(self, ctx: Context) -> None:
+                ctx.framework_data.current_node = None
+
+        script = Script.model_validate({"flow": {"node": {PRE_RESPONSE: {"": MyProcessing()}}}})
+
+        ctx = Context.init(start_label=("flow", "node"))
+        actor = Actor(script, fallback_label=("flow", "node"))
+        ctx.framework_data.pipeline = object_factory(
+            parallelize_processing=True,
+            script=script
+        )
+
+        await actor(ctx)
+
+        assert ctx.responses == {0: Message()}
+        assert log_list[0].msg == "Exception occurred during response processing."
+        assert str(log_list[0].exc_info[1]) == "Current node is not set."
+
+
+async def test_processing(object_factory):
+    contested_resource = {}
+
+    class Proc1(BaseProcessing):
+        async def func(self, ctx: Context) -> None:
+            await asyncio.sleep(0)
+            contested_resource[""] = 1
+
+    class Proc2(BaseProcessing):
+        async def func(self, ctx: Context) -> None:
+            contested_resource[""] = 2
+
+    procs = {"1": Proc1(), "2": Proc2()}
+
+    ctx = Context.init(start_label=("flow", "node"))
+
+    ctx.framework_data.pipeline = object_factory(
+        parallelize_processing=True,
+    )
+    await Actor._run_processing(procs, ctx)
+    assert contested_resource[""] == 1
+
+    ctx.framework_data.pipeline = object_factory(
+        parallelize_processing=False,
+    )
+    await Actor._run_processing(procs, ctx)
+    assert contested_resource[""] == 2
