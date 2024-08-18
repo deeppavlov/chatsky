16cb908200f15248a46887908f4e11bd829ce57f
Roman Zlobin <RLKRo@proton.me>
2024-08-19 00:51:18 +0300

    simplify response check_happy_path; add Message validation

--- chatsky/utils/testing/common.py
+++ chatsky/utils/testing/common.py
@@ -5,11 +5,11 @@ This module contains several functions which are used to run demonstrations in t
 """
 
 from os import getenv
-from typing import Callable, Tuple, Optional, Union
+from typing import Tuple
 from uuid import uuid4
 
-from chatsky.core import Context, Message, Pipeline
-from chatsky.utils.testing.response_comparers import default_comparer
+from chatsky.core import Message, Pipeline
+from chatsky.core.message import MessageInitTypes
 
 
 def is_interactive_mode() -> bool:  # pragma: no cover
@@ -31,10 +31,7 @@ def is_interactive_mode() -> bool:  # pragma: no cover
 
 def check_happy_path(
     pipeline: Pipeline,
-    happy_path: Tuple[Tuple[Union[str, Message], Union[str, Message]], ...],
-    # This optional argument is used for additional processing of candidate responses and reference responses
-    response_comparer: Callable[[Message, Message, Context], Optional[str]] = default_comparer,
-    printout_enable: bool = True,
+    happy_path: Tuple[Tuple[MessageInitTypes, MessageInitTypes], ...],
 ):
     """
     Running tutorial with provided pipeline for provided requests, comparing responses with correct expected responses.
@@ -44,40 +41,24 @@ def check_happy_path(
     :param pipeline: The Pipeline instance, that will be used for checking.
     :param happy_path: A tuple of (request, response) tuples, so-called happy path,
         its requests are passed to pipeline and the pipeline responses are compared to its responses.
-    :param response_comparer: A special comparer function that accepts received response, true response and context;
-        it returns `None` is two responses are equal and transformed received response if they are different.
-    :param printout_enable: A flag that enables requests and responses fancy printing (to STDOUT).
     """
-
     ctx_id = uuid4()  # get random ID for current context
     for step_id, (request_raw, reference_response_raw) in enumerate(happy_path):
-        request = Message(text=request_raw) if isinstance(request_raw, str) else request_raw
-        reference_response = (
-            Message(text=reference_response_raw) if isinstance(reference_response_raw, str) else reference_response_raw
-        )
+
+        request = Message.model_validate(request_raw)
+        reference_response = Message.model_validate(reference_response_raw)
+
         ctx = pipeline(request, ctx_id)
-        candidate_response = ctx.last_response
-        if printout_enable:
-            print(f"(user) >>> {repr(request)}")
-            print(f" (bot) <<< {repr(candidate_response)}")
-        if candidate_response is None:
-            raise Exception(
-                f"\n\npipeline = {pipeline.info_dict}\n\n"
-                f"ctx = {ctx}\n\n"
-                f"step_id = {step_id}\n"
-                f"request = {repr(request)}\n"
-                "Candidate response is None."
-            )
-        parsed_response_with_deviation = response_comparer(candidate_response, reference_response, ctx)
-        if parsed_response_with_deviation is not None:
-            raise Exception(
-                f"\n\npipeline = {pipeline.info_dict}\n\n"
-                f"ctx = {ctx}\n\n"
-                f"step_id = {step_id}\n"
-                f"request = {repr(request)}\n"
-                f"candidate_response = {repr(parsed_response_with_deviation)}\n"
-                f"reference_response = {repr(reference_response)}\n"
-                "candidate_response != reference_response"
+
+        actual_response = ctx.last_response
+
+        if reference_response != actual_response:
+            raise AssertionError(
+                f"""check_happy_path failed
+step id: {step_id}
+reference response: {reference_response}
+actual response: {actual_response}
+"""
             )
 
 
