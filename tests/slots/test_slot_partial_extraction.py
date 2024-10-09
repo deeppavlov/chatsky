218e8e951c1aecd6b10d802f1e6dcbfa317e0e16
askatasuna <andruhabobr@gmail.com>
2024-10-09 12:43:33 +0300

    Lint

--- tests/slots/test_slot_partial_extraction.py
+++ tests/slots/test_slot_partial_extraction.py
@@ -1,40 +1,22 @@
-from chatsky import (
-    RESPONSE,
-    TRANSITIONS,
-    PRE_TRANSITION,
-    PRE_RESPONSE,
-    GLOBAL,
-    LOCAL,
-    Pipeline,
-    Transition as Tr,
-    conditions as cnd,
-    processing as proc,
-    responses as rsp,
-)
-
 from chatsky.slots import RegexpSlot, GroupSlot
 from chatsky.slots.slots import SlotManager, ExtractedValueSlot, ExtractedGroupSlot
-from chatsky.core import Message, Context
-
-from chatsky.utils.testing import (
-    check_happy_path,
-)
+from chatsky.core import Message
 
 import pytest
 
 test_slot = GroupSlot(
-        person=GroupSlot(
-            username=RegexpSlot(
+    person=GroupSlot(
+        username=RegexpSlot(
             regexp=r"([a-z]+_[a-z]+)",
             match_group_idx=1,
         ),
-            email=RegexpSlot(
+        email=RegexpSlot(
             regexp=r"([a-z]+@[a-z]+\.[a-z]+)",
             match_group_idx=1,
         ),
-            allow_partially_extracted=True
-        )
+        allow_partially_extracted=True,
     )
+)
 
 
 extracted_slot_values_turn_1 = {
@@ -55,22 +37,26 @@ extracted_slot_values_turn_2 = {
     ),
 }
 
+
 @pytest.fixture(scope="function")
 def context_with_request_1(context):
     new_ctx = context.model_copy(deep=True)
     new_ctx.add_request(Message(text="I am test_name. My email is test@email.com"))
     return new_ctx
 
+
 @pytest.fixture(scope="function")
 def context_with_request_2(context):
     context.add_request(Message(text="I am new_name."))
     return context
+
 
 @pytest.fixture(scope="function")
 def empty_slot_manager():
     manager = SlotManager()
     manager.set_root_slot(test_slot)
     return manager
+
 
 @pytest.mark.parametrize(
     "slot_name,expected_slot_storage_1,expected_slot_storage_2",
@@ -92,7 +78,14 @@ def empty_slot_manager():
         ),
     ],
 )
-async def test_slot_extraction(slot_name, expected_slot_storage_1, expected_slot_storage_2, empty_slot_manager, context_with_request_1, context_with_request_2):
+async def test_slot_extraction(
+    slot_name,
+    expected_slot_storage_1,
+    expected_slot_storage_2,
+    empty_slot_manager,
+    context_with_request_1,
+    context_with_request_2,
+):
     await empty_slot_manager.extract_slot(slot_name, context_with_request_1, success_only=False)
     assert empty_slot_manager.slot_storage == expected_slot_storage_1
     await empty_slot_manager.extract_slot(slot_name, context_with_request_2, success_only=False)
