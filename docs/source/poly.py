7f4a153c9da90e17909c8ed36540fc787713312f
ZergLev <vasilev.sv@phystech.edu>
2024-10-14 20:15:44 +0300

    building docs only for latest tags in their respective groups

--- docs/source/poly.py
+++ docs/source/poly.py
@@ -1,3 +1,4 @@
+import re
 from pathlib import Path
 from datetime import datetime
 from functools import partial
@@ -55,18 +56,37 @@ def find_latest_tag(tag_list: list):
     return latest_tag
 
 
+# Filter func for building latest versions of each major tag.
+# Returns a dictionary of major tag groups as keys and latest tag's number as values
+# e.g. {(0, 6): 7, (1, 2): 3} standing for v0.6.7 and v1.2.3.
+def latest_tags_filter(tag_list: list):
+    regex = re.compile(r"^v\d*\.\d*\.\d*$")
+    tag_list = list(filter(regex.match, tag_list))
+    latest_tags = {}
+    for tag in tag_list:
+        tag = str(tag).replace('v', '').split(".")
+        tag_group = (tag[0], tag[1])
+        # Not building versions lower than v0.8.0
+        if not (int(tag[0]) == 0 and int(tag[1]) < 8):
+            # If there is a greater tag in this group, it will have priority over others
+            if int(tag[2]) > int(latest_tags.get(tag_group, -1)):
+                latest_tags[tag_group] = tag[2]
+    # Could return a dictionary, but it looks unclear.
+    tag_list = ['v' + x[0] + '.' + x[1] + '.' + latest_tags[x] for x in latest_tags.keys()]
+    return tag_list
+
+
 def create_tag_regex(tag_list: list):
-    tag_regex = r"("
     # Maybe could add a 'start_version' for building
     start_index = tag_list.index(first_built_version)
     tag_list = tag_list[start_index:]
-    # Sorts through tags for 'rc' and 'dev' strings
-    tag_list = [elem for elem in tag_list if (("rc" not in str(elem)) and ("dev" not in str(elem)))]
+    # Filter for latest tags in their respective groups
+    tag_list = latest_tags_filter(tag_list)
     # Creates the regex
+    tag_regex = r"("
     for tag in tag_list:
         tag_regex += str(tag) + "|"
     tag_regex = tag_regex[:-1] + ")"
-    print("created tag regex")
     return tag_regex
 
 
