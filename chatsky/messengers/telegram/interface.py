45df6cca935c16640774190f0e00a19b8232b2c8
Roman Zlobin <RLKRo@proton.me>
2024-10-23 15:03:47 +0300

    fix codestyle

--- chatsky/messengers/telegram/interface.py
+++ chatsky/messengers/telegram/interface.py
@@ -29,7 +29,12 @@ class LongpollingInterface(_AbstractTelegramInterface):
     """
 
     def __init__(
-        self, token: str, id: Optional[str] = None, attachments_directory: Optional[Path] = None, interval: int = 2, timeout: int = 20
+        self,
+        token: str,
+        id: Optional[str] = None,
+        attachments_directory: Optional[Path] = None,
+        interval: int = 2,
+        timeout: int = 20,
     ) -> None:
         super().__init__(token, id, attachments_directory)
         self.interval = interval
@@ -54,7 +59,12 @@ class WebhookInterface(_AbstractTelegramInterface):
     """
 
     def __init__(
-        self, token: str, id: Optional[str] = None, attachments_directory: Optional[Path] = None, host: str = "localhost", port: int = 844
+        self,
+        token: str,
+        id: Optional[str] = None,
+        attachments_directory: Optional[Path] = None,
+        host: str = "localhost",
+        port: int = 844,
     ):
         super().__init__(token, id, attachments_directory)
         self.listen = host
