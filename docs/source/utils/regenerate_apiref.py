8c3c6f720b1be790a031486127c6502c27a5bb48
ZergLev <vasilev.sv@phystech.edu>
2024-10-23 19:20:39 +0300

    building the 'latest' version only when necessary to save time

--- docs/source/utils/regenerate_apiref.py
+++ docs/source/utils/regenerate_apiref.py
@@ -2,8 +2,6 @@ from os.path import join
 from pathlib import Path
 from typing import List, Optional, Tuple, Dict
 
-from docs.source.conf import doc_version
-
 
 def generate_doc_container(file: Path, alias: str, includes: List[Path]):
     """
@@ -32,7 +30,6 @@ def generate_doc_container(file: Path, alias: str, includes: List[Path]):
     file.with_name(f"index_{file.name}").write_text(contents)
 
 
-# TODO: Add that dictionary from setup() here, then recursively add 'doc_version' into html metadata.
 def regenerate_apiref(paths: Optional[List[Tuple[str, str]]] = None, configs: dict = None):
     """
     Regenerate files in apiref root.
@@ -67,8 +64,10 @@ def regenerate_apiref(paths: Optional[List[Tuple[str, str]]] = None, configs: di
 
         with open(doc_file, "r+") as file:
             contents = file.read()
-            doc_file.write_text(f":source_name: {join(*doc_file.stem.split('.'))}\n"
-                                f":doc_version: {configs['doc_version']}\n\n{contents}")
+            doc_file.write_text(
+                f":source_name: {join(*doc_file.stem.split('.'))}\n"
+                f":doc_version: {configs['doc_version']}\n\n{contents}"
+            )
 
     for name, (alias, files) in doc_containers.items():
         generate_doc_container(source / Path(f"{name}.rst"), alias, files)
