ff8126712120b55d01899da1d1049b829acf3a72
NotBioWaste905 <andruhabobr@gmail.com>
2024-11-13 17:27:18 +0300

    Moved docstring, removed pipeline parameter

--- chatsky/conditions/llm.py
+++ chatsky/conditions/llm.py
@@ -3,18 +3,17 @@ from chatsky.core import BaseCondition, Context, Pipeline
 
 
 class LLMCondition(BaseCondition):
+    """
+    Basic function for using LLM in condition cases.
+
+    :param model_name: Key of the model from the `Pipeline.models` dictionary.
+    :param prompt: Prompt for the model to use on users input.
+    :param method: Method that takes models output and returns boolean.
+    """
     model_name: str
     prompt: str
     method: BaseMethod
-    pipeline: Pipeline
 
     async def call(self, ctx: Context) -> bool:
-        """
-        Basic function for using LLM in condition cases.
-
-        :param model_name: Key of the model from the `Pipeline.models` dictionary.
-        :param prompt: Prompt for the model to use on users input.
-        :param method: Method that takes models output and returns boolean.
-        """
-        model = self.pipeline.models[self.model_name]
+        model = ctx.pipeline.models[self.model_name]
         return await model.condition(self.prompt, self.method)
