9188b8972e7a60024efbee04bd91b0cd95dcaf9b
askatasuna <andruhabobr@gmail.com>
2024-11-14 12:48:46 +0300

    Fixes in filters

--- chatsky/llm/filters.py
+++ chatsky/llm/filters.py
@@ -1,5 +1,5 @@
 """
-Filters.
+Filters
 ---------
 This module contains a collection of basic functions for history filtering to avoid cluttering LLMs context window.
 """
@@ -20,10 +20,10 @@ class BaseFilter(BaseModel, abc.ABC):
     @abc.abstractmethod
     def __call__(self, ctx: Context, request: Message, response: Message, model_name: str) -> bool:
         """
-        :param Context ctx: Context object.
-        :param Message request: Request message.
-        :param Message response: Response message.
-        :param str model_name: Name of the model in the Pipeline.models.
+        :param ctx: Context object.
+        :param request: Request message.
+        :param response: Response message.
+        :param model_name: Name of the model in the Pipeline.models.
         """
         raise NotImplementedError
 
@@ -34,11 +34,11 @@ class IsImportant(BaseFilter):
     """
 
     def __call__(
-        self, ctx: Context = None, request: Message = None, response: Message = None, model_name: str = None
+        self, ctx: Context, request: Message = Message(), response: Message = Message(), model_name: str = ""
     ) -> bool:
-        if request and request.misc["important"]:
+        if request and request.misc.get("important", None):
             return True
-        if response and response.misc["important"]:
+        if response and response.misc.get("important", None):
             return True
         return False
 
@@ -49,10 +49,10 @@ class FromTheModel(BaseFilter):
     """
 
     def __call__(
-        self, ctx: Context = None, request: Message = None, response: Message = None, model_name: str = None
+        self, ctx: Context, request: Message = Message(), response: Message = Message(), model_name: str = ""
     ) -> bool:
-        if request is not None and request.annotation["__generated_by_model__"] == model_name:
+        if request is not None and request.annotation.get("__generated_by_model__", None) == model_name:
             return True
-        elif response is not None and response.annotation["__generated_by_model__"] == model_name:
+        elif response is not None and response.annotation.get("__generated_by_model__", None) == model_name:
             return True
         return False
