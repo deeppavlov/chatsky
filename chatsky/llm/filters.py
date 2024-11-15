1be58a05e0df66a1cedf2d124451f5259ed0c694
Roman Zlobin <RLKRo@proton.me>
2024-11-15 03:28:50 +0300

    check field not none in filters

--- chatsky/llm/filters.py
+++ chatsky/llm/filters.py
@@ -36,9 +36,9 @@ class IsImportant(BaseFilter):
     def __call__(
         self, ctx: Context, request: Message, response: Message, model_name: str
     ) -> bool:
-        if request and request.misc.get("important", None):
+        if request is not None and request.misc is not None and request.misc.get("important", None):
             return True
-        if response and response.misc.get("important", None):
+        if response is not None and response.misc is not None and response.misc.get("important", None):
             return True
         return False
 
@@ -51,8 +51,10 @@ class FromTheModel(BaseFilter):
     def __call__(
         self, ctx: Context, request: Message, response: Message, model_name: str
     ) -> bool:
-        if request is not None and request.annotation.get("__generated_by_model__", None) == model_name:
+        if (request is not None and request.annotations is not None and
+                request.annotations.get("__generated_by_model__") == model_name):
             return True
-        elif response is not None and response.annotation.get("__generated_by_model__", None) == model_name:
+        elif (response is not None and response.annotations is not None and
+              response.annotations.get("__generated_by_model__") == model_name):
             return True
         return False
