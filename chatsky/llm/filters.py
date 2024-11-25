47f38559b2509101adea13e27a14f048425368ed
NotBioWaste905 <andruhabobr@gmail.com>
2024-11-25 11:55:02 +0300

    Changes in BaseFilter class

--- chatsky/llm/filters.py
+++ chatsky/llm/filters.py
@@ -5,17 +5,29 @@ This module contains a collection of basic functions for history filtering to av
 """
 
 import abc
-
+from enum import Enum
 from pydantic import BaseModel
+
+from logging import Logger
 
 from chatsky.core.message import Message
 from chatsky.core.context import Context
+
+logger = Logger(name=__name__)
 
 
 class BaseFilter(BaseModel, abc.ABC):
     """
     Base class for all message history filters.
     """
+
+    class Return(Enum):
+        Request = 1
+        Response = 2
+        Turn = 3
+
+    def call(self, ctx: Context, request: Message, response: Message, model_name: str):
+        return self.Return.Turn
 
     def __call__(self, ctx: Context, request: Message, response: Message, model_name: str) -> bool:
         """
@@ -24,7 +36,27 @@ class BaseFilter(BaseModel, abc.ABC):
         :param response: Response message.
         :param model_name: Name of the model in the Pipeline.models.
         """
-        return True
+        try:
+            result = self.call(ctx, request, response, model_name)
+        except Exception as exc:
+            logger.warning(exc)
+            return []
+        if result == self.Return.Turn:
+            return [request, response]
+        if result == self.Return.Response:
+            return [response]
+        if result == self.Return.Request:
+            return [request]
+        return []
+
+
+class MessageFilter(BaseFilter):
+    @abc.abstractmethod
+    def call(self, ctx, message, model_name):
+        raise NotImplemented
+
+    def __call__(self, ctx, request, response, model_name):
+        return self.call(ctx, request, model_name) + self.call(ctx, response, model_name)
 
 
 class IsImportant(BaseFilter):
@@ -32,11 +64,11 @@ class IsImportant(BaseFilter):
     Filter that checks if the "important" field in a Message.misc is True.
     """
 
-    def __call__(self, ctx: Context, request: Message, response: Message, model_name: str) -> bool:
+    def call(self, ctx: Context, request: Message, response: Message, model_name: str) -> bool:
         if request is not None and request.misc is not None and request.misc.get("important", None):
-            return True
+            return self.Return.Request
         if response is not None and response.misc is not None and response.misc.get("important", None):
-            return True
+            return self.Return.Response
         return False
 
 
@@ -45,17 +77,17 @@ class FromModel(BaseFilter):
     Filter that checks if the message was sent by the model.
     """
 
-    def __call__(self, ctx: Context, request: Message, response: Message, model_name: str) -> bool:
+    def call(self, ctx: Context, request: Message, response: Message, model_name: str) -> bool:
         if (
             request is not None
             and request.annotations is not None
             and request.annotations.get("__generated_by_model__") == model_name
         ):
-            return True
+            return self.Return.Request
         elif (
             response is not None
             and response.annotations is not None
             and response.annotations.get("__generated_by_model__") == model_name
         ):
-            return True
+            return self.Return.Response
         return False
--- tests/llm/test_imports.py
+++ tests/llm/test_imports.py
@@ -7,9 +7,10 @@ try:
 except ImportError:
     pytest.skip(...)
 
+
 def test_imports(monkeypatch):
     monkeypatch.delattr(langchain_core.messages, "HumanMessage")
- 
+
     from chatsky.llm._langchain_imports import langchain_available, HumanMessage, check_langchain_available
 
     assert langchain_available is False
