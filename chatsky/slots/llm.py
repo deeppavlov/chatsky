34e5536b07d2f377cec3ee04faf6697fdc9e4d58
NotBioWaste905 <andruhabobr@gmail.com>
2024-11-26 22:27:08 +0300

    Lint

--- chatsky/slots/llm.py
+++ chatsky/slots/llm.py
@@ -12,7 +12,13 @@ import logging
 
 from pydantic import BaseModel, Field, create_model
 
-from chatsky.slots.slots import ValueSlot, SlotNotExtracted, GroupSlot, ExtractedGroupSlot, ExtractedValueSlot
+from chatsky.slots.slots import (
+    ValueSlot, 
+    SlotNotExtracted, 
+    GroupSlot, 
+    ExtractedGroupSlot, 
+    ExtractedValueSlot
+)
 
 if TYPE_CHECKING:
     from chatsky.core import Context
@@ -23,26 +29,28 @@ logger = logging.getLogger(__name__)
 
 class LLMSlot(ValueSlot, frozen=True):
     """
-    LLMSlot is a slot type that extract information described in `caption` parameter using LLM.
+    LLMSlot is a slot type that extract information described in
+    `caption` parameter using LLM.
     """
 
     caption: str
     return_type: type = str
-    model: Optional[Any] = None
+    model_name: str = ""
 
-    def __init__(self, caption, model=None):
-        super().__init__(caption=caption, model=model)
+    def __init__(self, caption, model_name=None):
+        super().__init__(caption=caption, model_name=model_name)
 
     async def extract_value(self, ctx: Context) -> Union[str, SlotNotExtracted]:
         request_text = ctx.last_request.text
         if request_text == "":
             return SlotNotExtracted()
+        model = ctx.pipeline.models[self.model_name]
 
         # Dynamically create a Pydantic model based on the caption
         class DynamicModel(BaseModel):
             value: self.return_type = Field(description=self.caption)
 
-        structured_model = self.model.with_structured_output(DynamicModel)
+        structured_model = model.with_structured_output(DynamicModel)
 
         result = await structured_model.ainvoke(request_text)
         return result.value
@@ -51,29 +59,34 @@ class LLMSlot(ValueSlot, frozen=True):
 class LLMGroupSlot(GroupSlot):
     """
     LLMSlots based :py:class:`~.GroupSlot` implementation.
-    Fetches data for all LLMSlots in a single API request contrary to :py:class:`~.GroupSlot`.
+    Fetches data for all LLMSlots in a single API request
+    contrary to :py:class:`~.GroupSlot`.
     """
 
     __pydantic_extra__: Dict[str, Union[LLMSlot, "LLMGroupSlot"]]
-    model: Any
+    model_name: str
 
     async def get_value(self, ctx: Context) -> ExtractedGroupSlot:
         flat_items = self.__flatten_llm_group_slot(self)
         captions = {}
         for child_name, slot_item in flat_items.items():
-            captions[child_name] = (slot_item.return_type, Field(description=slot_item.caption, default=None))
+            captions[child_name] = (slot_item.return_type, 
+                                    Field(description=slot_item.caption, 
+                                    default=None))
 
         logger.debug(f"Flattened group slot: {flat_items}")
         DynamicGroupModel = create_model("DynamicGroupModel", **captions)
         logger.debug(f"DynamicGroupModel: {DynamicGroupModel}")
 
-        structured_model = self.model.with_structured_output(DynamicGroupModel)
+        model = ctx.pipeline.models[self.model_name]
+        structured_model = model.with_structured_output(DynamicGroupModel)
         result = await structured_model.ainvoke(ctx.last_request.text)
         result_json = result.model_dump()
         logger.debug(f"Result JSON: {result_json}")
 
         res = {
-            name: ExtractedValueSlot.model_construct(is_slot_extracted=True, extracted_value=result_json[name])
+            name: ExtractedValueSlot.model_construct(is_slot_extracted=True, 
+                                                     extracted_value=result_json[name])
             for name in result_json
             if result_json[name] is not None or not self.allow_partial_extraction
         }
