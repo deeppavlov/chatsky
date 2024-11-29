51f44e206e4d9a7c726b87df10d5bb07d2d124db
ZergLev <vasilev.sv@phystech.edu>
2024-11-29 19:45:02 +0300

    fixed test_tutorials in GitHub CodeSpaces, testing

--- tests/tutorials/test_tutorials.py
+++ tests/tutorials/test_tutorials.py
@@ -34,7 +34,9 @@ def check_tutorial_dependencies(venv: "VirtualEnv", tutorial_source_code: str):
     versions_dict = InstallationCell.versions()
     for deps in re.findall(InstallationCell.pattern, tutorial_source_code):
         venv.run(
-            re.sub(r"chatsky", ".", f"python -m pip install {deps}".replace("=={chatsky}", "")).format(**versions_dict),
+            (f"python -m pip install {deps}".replace("=={chatsky}", "").replace("chatsky", ".")).format(
+                **versions_dict
+            ),
             check_rc=True,
             cd=os.getcwd(),
         )
