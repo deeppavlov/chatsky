24237fbee7c1d44c660cf0189c3711cf423062ab
NotBioWaste905 <andruhabobr@gmail.com>
2025-01-22 13:51:30 +0300

    Added some logging, WIP

--- chatsky/llm/langchain_context.py
+++ chatsky/llm/langchain_context.py
@@ -15,6 +15,9 @@ from chatsky.llm._langchain_imports import HumanMessage, SystemMessage, AIMessag
 from chatsky.llm.filters import BaseHistoryFilter
 from chatsky.llm.prompt import Prompt, DesaultPositionConfig
 
+logger = logging.getLogger(__name__)
+logger.debug("Loaded LLM Utils logger.")
+
 
 async def message_to_langchain(
     message: Message, ctx: Context, source: Literal["human", "ai", "system"] = "human", max_size: int = 1000
@@ -37,7 +40,7 @@ async def message_to_langchain(
     if message.text is None:
         content = []
     elif len(message.text) > max_size:
-        logging.warning("Message is too long.")
+        logger.warning("Message is too long.")
         content = []
     else:
         content = [{"type": "text", "text": message.text}]
@@ -53,7 +56,7 @@ async def message_to_langchain(
 
 
 async def context_to_history(
-    ctx: Context, length: int, filter_func: BaseHistoryFilter, model_name: str, max_size: int
+    ctx: Context, length: int, filter_func: BaseHistoryFilter, llm_model_name: str, max_size: int
 ) -> list[HumanMessage | AIMessage | SystemMessage]:
     """
     Convert context to list of langchain messages.
@@ -61,7 +64,7 @@ async def context_to_history(
     :param ctx: Current dialog context.
     :param length: Amount of turns to include in history. Set to `-1` to include all context.
     :param filter_func: Function to filter the context.
-    :param model_name: name of the model from the pipeline.
+    :param llm_model_name: name of the model from the pipeline.
     :param max_size: Maximum size of the message in symbols.
 
     :return: List of Langchain message objects.
@@ -72,14 +75,14 @@ async def context_to_history(
         [ctx.requests[x] for x in range(1, len(ctx.requests) + 1)],
         [ctx.responses[x] for x in range(1, len(ctx.responses) + 1)],
     )
-    logging.debug(f"Dialogue turns: {pairs}")
+    logger.debug(f"Dialogue turns: {pairs}")
     pairs_list = list(pairs)
     filtered_pairs = filter(
-        lambda x: filter_func(ctx, x[0], x[1], model_name), pairs_list[-length:] if length != -1 else pairs_list
+        lambda x: filter_func(ctx, x[0], x[1], llm_model_name), pairs_list[-length:] if length != -1 else pairs_list
     )
 
     for req, resp in filtered_pairs:
-        logging.debug(f"This pair is valid: {req, resp}")
+        logger.debug(f"This pair is valid: {req, resp}")
         history.append(await message_to_langchain(req, ctx=ctx, max_size=max_size))
         history.append(await message_to_langchain(resp, ctx=ctx, source="ai", max_size=max_size))
     return history
@@ -88,7 +91,7 @@ async def context_to_history(
 # get a list of messages to pass to LLM from context and prompts
 # called in LLM_API
 async def get_langchain_context(
-    system_prompt: Prompt,
+    system_prompt: Message,
     ctx: Context,
     call_prompt,
     prompt_misc_filter: str = r"prompt",  # r"prompt" -> extract misc prompts
@@ -98,10 +101,19 @@ async def get_langchain_context(
     """
     Get a list of Langchain messages using the context and prompts.
     """
-    history = context_to_history(ctx, history_args)
-    prompts = [(system_prompt, system_prompt.position)]
+    logger.debug(f"History args: {history_args}")
+    history = await context_to_history(ctx, **history_args)
+    prompts: list[tuple[list[Union[HumanMessage, AIMessage, SystemMessage]], float]] = [
+        ([await message_to_langchain(system_prompt, ctx, source="system")], postition_config.system_prompt.position)
+    ]
+    logger.debug(f"System prompt: {prompts[0]}")
     misc_prompts = []
-
+    
+    #TODO:
+    # put lists of langchain messages in prompts
+    # history in prompts also has a position
+    # await prompts (they are BaseResponses)
+    
     for element in ctx.current_node.misc:
         if re.match(prompt_misc_filter, element):
             misc_prompts.append(ctx.current_node.misc[element], ctx.current_node.misc[element].position)
@@ -113,14 +125,15 @@ async def get_langchain_context(
         if prompt[1] is not None:
             prompt[1] = postition_config.get_position(prompt[1])
 
+    logger.debug(f"Prompts: {prompts}")
     prompts = sorted(prompts, key=lambda x: x.position)
 
-    logging.debug(f"Sorted prompts: {prompts}")
+    logger.debug(f"Sorted prompts: {prompts}")
 
     # merge prompts and history
     langchain_context = []
     for prompt in prompts:
-        langchain_context.append(await message_to_langchain(prompt, ctx, source="system"))
+        langchain_context.append(await message_to_langchain(prompt, ctx, source="human"))
 
     langchain_context.extend(history)
     return langchain_context
