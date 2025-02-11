f4eb2497c8b7646648e7d3b8d58c5ebc0d99e71f
pseusys <aleksandr.sergeev.ad@gmail.com>
2025-02-11 02:17:51 +0100

    assignment changed

--- chatsky/context_storages/database.py
+++ chatsky/context_storages/database.py
@@ -235,16 +235,16 @@ class DBContextStorage(ABC):
         joined_field_info = dict()
         logger.debug(f"Updating context for {ctx_id}...")
         for field, added, deleted in field_info:
-            field = self._validate_field_name(field)
+            field_info = joined_field_info.setdefault(self._validate_field_name(field), list())
             if len(added) == 0:
                 logger.debug(f"\tNo fields to add in {field}!")
             else:
-                joined_field_info.setdefault(field, list()) += added
+                field_info += added
                 logger.debug(f"\tAdding fields for {field}: {collapse_num_list(list(k for k, _ in added))}...")
             if len(deleted) == 0:
                 logger.debug(f"\tNo fields to delete in {field}!")
             else:
-                joined_field_info.setdefault(field, list()) += [(k, None) for k in deleted]
+                field_info += [(k, None) for k in deleted]
                 logger.debug(f"\tDeleting fields for {field}: {collapse_num_list(deleted)}...")
         await self._update_context(ctx_id, ctx_info, list(joined_field_info.items()))
         logger.debug(f"Context updated for {ctx_id}")
