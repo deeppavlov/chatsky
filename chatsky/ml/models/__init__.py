29815b663a52a9ac31a23ca6cb23a96b07f2d49e
NotBioWaste905 <andruhabobr@gmail.com>
2025-02-19 16:09:38 +0300

    Moved models from remote_api directory

--- chatsky/ml/models/__init__.py
+++ chatsky/ml/models/__init__.py
@@ -1,3 +1,3 @@
-from .remote_api.google_dialogflow_model import GoogleDialogFlowModel  # noqa: F401
-from .remote_api.rasa_model import RasaModel  # noqa: F401
-from .remote_api.hf_api_model import HFAPIModel  # noqa: F401
+from .google_dialogflow_model import GoogleDialogFlowModel  # noqa: F401
+from .rasa_model import RasaModel  # noqa: F401
+from .hf_api_model import HFAPIModel  # noqa: F401
