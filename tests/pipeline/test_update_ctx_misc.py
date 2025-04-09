67fc248412f892a6c4e884638e99adb5e2ad2d67
NotBioWaste905 <andruhabobr@gmail.com>
2025-04-09 12:34:10 +0300

    remove async marking, simplify test

--- tests/pipeline/test_update_ctx_misc.py
+++ tests/pipeline/test_update_ctx_misc.py
@@ -1,9 +1,7 @@
 import asyncio
 import pytest
-import time
-from chatsky import Context, GLOBAL, conditions
+from chatsky import Context, GLOBAL, conditions, destinations as dst
 from chatsky.core import Message, RESPONSE, TRANSITIONS, Pipeline, Transition as Tr, BaseCondition, BaseResponse
-
 
 @pytest.mark.asyncio
 async def test_update_ctx_misc():
@@ -36,38 +34,27 @@ async def test_update_ctx_misc():
     assert ctx.last_response.text == "failure"
 
 
-@pytest.mark.asyncio
 async def test_context_order():
 
     class LongResponse(BaseResponse):
-        def call(self, ctx: Context):
-            time.sleep(3)
-            return Message(text="Slept for 3 seconds")
-
-    class LongerResponse(BaseResponse):
-        def call(self, ctx: Context):
-            time.sleep(5)
-            return Message(text="Slept for 5 seconds")
+        async def call(self, ctx: Context):
+            await asyncio.sleep(int(ctx.last_request.text))
+            return Message(text=ctx.last_request.text)
 
     toy_script = {
-        GLOBAL: {
-            TRANSITIONS: [
-                Tr(dst=("root", "5_sec"), cnd=conditions.ExactMatch("5"), priority=1.2),
-                Tr(dst=("root", "3_sec"), cnd=conditions.ExactMatch("3"), priority=1.2),
-            ]
-        },
         "root": {
-            "start": {TRANSITIONS: [Tr(dst="success", cnd=True)]},
-            "5_sec": {RESPONSE: LongerResponse(), TRANSITIONS: [Tr(dst="start", cnd=True)]},
-            "3_sec": {RESPONSE: LongResponse(), TRANSITIONS: [Tr(dst="start", cnd=True)]},
+            "start": {RESPONSE: "", TRANSITIONS: [Tr(dst="result", cnd=True)]},
+            "result": {RESPONSE: LongResponse(), TRANSITIONS: [Tr(dst="result", cnd=True)]},
             "failure": {RESPONSE: "failure"},
         },
     }
 
     pipeline = Pipeline(script=toy_script, start_label=("root", "start"), fallback_label=("root", "failure"))
     res = await asyncio.gather(
-        pipeline._run_pipeline(Message("5"), ctx_id=0),
-        pipeline._run_pipeline(Message("3"), ctx_id=0),
+        pipeline._run_pipeline(Message("0.03"), ctx_id=0),
+        pipeline._run_pipeline(Message("0.01"), ctx_id=1),
+        pipeline._run_pipeline(Message("0.02"), ctx_id=0),
     )
-    assert res[0].last_response == Message(text="Slept for 5 seconds")
-    assert res[1].last_response == Message(text="Slept for 3 seconds")
+    assert res[0].last_response == Message(text="0.01")
+    assert res[1].last_response == Message(text="0.03")
+    assert res[2].last_response == Message(text="0.02")
