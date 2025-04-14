762cf9f5c78bbebcc3b9f55a5c6d2a0bc0ad4635
Alexandra Dolidze <44212238+rock-n-shrimproll@users.noreply.github.com>
2025-04-14 15:55:31 +0300

    feat: save transition in Framework (#417)
    Save last transition object in framework data
    
    ---------
    
    Co-authored-by: Roman Zlobin <RLKRo@proton.me>

--- chatsky/core/ctx_utils.py
+++ chatsky/core/ctx_utils.py
@@ -21,6 +21,7 @@ if TYPE_CHECKING:
     from chatsky.core.service import ComponentExecutionState
     from chatsky.core.script import Node
     from chatsky.core.pipeline import Pipeline
+    from chatsky.core.transition import Transition
 
 
 class ContextError(Exception):
@@ -64,6 +65,15 @@ class FrameworkData(BaseModel, arbitrary_types_allowed=True):
     "Enables complex stats collection across multiple turns."
     slot_manager: SlotManager = Field(default_factory=SlotManager)
     "Stores extracted slots."
+    transition: Optional[Transition] = Field(default=None, exclude=True)
+    """
+    Stores transition made during this turn.
+
+    Can be ``None`` if either is true:
+
+    - no transition has been made during this turn yet (e.g. the turn is in the pre-transition step);
+    - no valid transition has been found (i.e. transitioned to fallback node).
+    """
 
 
 class ContextMainInfo(BaseModel):
