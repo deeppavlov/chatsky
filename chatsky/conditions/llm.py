0dcc5fad7bb7a26a9f100fc2e9ae82954f20981b
NotBioWaste905 <andruhabobr@gmail.com>
2025-04-17 20:13:08 +0300

    rename `context_window` to `dialog_turns`

--- chatsky/conditions/llm.py
+++ chatsky/conditions/llm.py
@@ -29,7 +29,7 @@ class LLMCondition(BaseCondition):
     """
     Condition prompt.
     """
-    context_window: int = 1
+    dialog_turns: int = 1
     """
     Number of dialogue turns aside from the current one to keep in history. `-1` to put all messages into the context.
     """
@@ -67,7 +67,7 @@ class LLMCondition(BaseCondition):
                 call_prompt=Prompt(message=self.prompt),
                 prompt_misc_filter=self.prompt_misc_filter,
                 position_config=self.position_config or model.position_config,
-                length=self.context_window,
+                length=self.dialog_turns,
                 filter_func=self.filter_func,
                 llm_model_name=self.llm_model_name,
                 max_size=self.max_size,
--- chatsky/responses/llm.py
+++ chatsky/responses/llm.py
@@ -30,7 +30,7 @@ class LLMResponse(BaseResponse):
     """
     Response prompt.
     """
-    context_window: int = 5
+    dialog_turns: int = 5
     """
     Number of dialogue turns aside from the current one to keep in history. `-1` to put all messages into the context.
     """
@@ -68,7 +68,7 @@ class LLMResponse(BaseResponse):
                 call_prompt=self.prompt,
                 prompt_misc_filter=self.prompt_misc_filter,
                 position_config=self.position_config or model.position_config,
-                length=self.context_window,
+                length=self.dialog_turns,
                 filter_func=self.filter_func,
                 llm_model_name=self.llm_model_name,
                 max_size=self.max_size,
