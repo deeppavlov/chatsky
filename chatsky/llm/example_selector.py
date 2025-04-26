2c2e3ef11f7eb4c0809194236161c11ca64bd36a
Kiriltsev Daniil <3layerperceptron@gmail.com>
2025-04-26 16:06:12 +0300

    Signatures fixed; Redundant test cases (add_example) removed

--- chatsky/llm/example_selector.py
+++ chatsky/llm/example_selector.py
@@ -20,10 +20,10 @@ class Example(BaseModel):
     """output may be in form of a string or a custom pydantic model derived from the BaseModel"""
 
 
-def to_langchain_context(example_selector: BaseExampleSelector) -> None:
+def to_langchain_context(example_selector: BaseExampleSelector, input_variables: Dict[str, str]) -> None:
 
     result = []
-    for example in example_selector.select_examples():
+    for example in example_selector.select_examples(input_variables):
 
         result.append(HumanMessage(content=example["input"]))
         result.append(AIMessage(content=example["output"]))
@@ -35,13 +35,13 @@ class StaticExampleSelector(BaseExampleSelector, RootModel):
 
     root: List[Example]
 
-    def add_example(self, example: Example) -> Any:
+    def add_example(self, example: Dict[str, str]) -> Any:
         self.root.append(Example.model_validate(example))
 
     def unpack_model(self, example_part: str | BaseModel) -> str:
         return str(example_part.model_dump_json()) if isinstance(example_part, BaseModel) else example_part
 
-    def select_examples(self) -> List[Dict[str, str]]:
+    def select_examples(self, input_variables: Dict[str, str]) -> List[dict]:
         return [
             {"input": self.unpack_model(example.input), "output": self.unpack_model(example.output)}
             for example in self.root
--- tests/llm/test_example_selector.py
+++ tests/llm/test_example_selector.py
@@ -3,7 +3,7 @@ from typing import Any, Dict, List
 import numpy as np
 from langchain_core.example_selectors.base import BaseExampleSelector
 from langchain_core.messages import AIMessage, HumanMessage
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, RootModel
 
 from chatsky import MISC, RESPONSE
 from chatsky.llm.example_selector import (
@@ -31,20 +31,18 @@ class RequestModel(BaseModel):
     operand_2: float = Field(description="The second operand")
 
 
-class MockCustomExampleSelector(BaseExampleSelector, BaseModel):
+class MockCustomExampleSelector(BaseExampleSelector, RootModel):
 
-    examples: List[Example]
-    k: int = 3
-    replace: bool = False
+    root: List[Example]
 
-    def add_example(self, example: Example) -> Any:
-        self.examples.append(Example.model_validate(example))
+    def add_example(self, example: Dict[str, str]) -> Any:
+        self.root.append(Example.model_validate(example))
 
     def unpack_model(self, example_part: str | BaseModel) -> str:
         return str(example_part.model_dump_json()) if isinstance(example_part, BaseModel) else example_part
 
-    def select_examples(self, k=k, replace=replace) -> List[Dict[str, str]]:
-        subset = np.random.choice(self.examples, size=k, replace=replace)
+    def select_examples(self, input_variables: Dict[str, str]) -> List[dict]:
+        subset = np.random.choice(self.root, input_variables["size"], input_variables["replace"])
         return [
             {"input": self.unpack_model(example.input), "output": self.unpack_model(example.output)}
             for example in subset
@@ -67,14 +65,6 @@ class TestStaticExampleSelector:
         ),
     }
 
-    def test_add_example(self):
-        before_len = len(self.node["MISC"].examples.root)
-        self.node["MISC"].examples.add_example(
-            {"input": RequestModel(operand_1=-1.0, operand_2=1.0), "output": ResponseModel(sum=0.0, prod=-1.0)}
-        )
-        after_len = len(self.node["MISC"].examples.root)
-        assert after_len - before_len == 1
-
     def test_select_examples(self):
 
         ground_truth = [
@@ -82,9 +72,8 @@ class TestStaticExampleSelector:
             {"input": '{"operand_1":5.0,"operand_2":6.0}', "output": '{"sum":11.0,"prod":30.0}'},
             {"input": '{"operand_1":0.0,"operand_2":8.0}', "output": '{"sum":8.0,"prod":0.0}'},
             {"input": '{"operand_1":-1.0,"operand_2":-2.0}', "output": '{"sum":-3.0,"prod":2.0}'},
-            {"input": '{"operand_1":-1.0,"operand_2":1.0}', "output": '{"sum":0.0,"prod":-1.0}'},
         ]
-        assert self.node["MISC"].examples.select_examples() == self.node["MISC"].examples.select_examples()
+        assert self.node["MISC"].examples.select_examples(input_variables={}) == ground_truth
 
     def test_to_langchain_context(self):
         ground_truth = [
@@ -96,10 +85,8 @@ class TestStaticExampleSelector:
             AIMessage(content='{"sum":8.0,"prod":0.0}', additional_kwargs={}, response_metadata={}),
             HumanMessage(content='{"operand_1":-1.0,"operand_2":-2.0}', additional_kwargs={}, response_metadata={}),
             AIMessage(content='{"sum":-3.0,"prod":2.0}', additional_kwargs={}, response_metadata={}),
-            HumanMessage(content='{"operand_1":-1.0,"operand_2":1.0}', additional_kwargs={}, response_metadata={}),
-            AIMessage(content='{"sum":0.0,"prod":-1.0}', additional_kwargs={}, response_metadata={}),
         ]
-        assert to_langchain_context(self.node["MISC"].examples) == ground_truth
+        assert to_langchain_context(self.node["MISC"].examples, input_variables={}) == ground_truth
 
 
 class TestCustomExampleSelector:
@@ -108,7 +95,7 @@ class TestCustomExampleSelector:
         RESPONSE: LLMResponse(llm_model_name="my_model", prompt="Add numbers and return an answer."),
         MISC: ExamplePrompt(
             examples=MockCustomExampleSelector(
-                examples=[
+                [
                     {"input": '{"operand_1":3.0,"operand_2":4.0}', "output": '{"sum":7.0,"prod":12.0}'},
                     {"input": RequestModel(operand_1=5.0, operand_2=6.0), "output": '{"sum":11.0,"prod":30.0}'},
                     {"input": '{"operand_1":0.0,"operand_2":8.0}', "output": ResponseModel(sum=8.0, prod=0.0)},
@@ -121,30 +108,22 @@ class TestCustomExampleSelector:
         ),
     }
 
-    def test_add_example(self):
-        before_len = len(self.node["MISC"].examples.examples)
-        self.node["MISC"].examples.add_example(
-            {"input": RequestModel(operand_1=-1.0, operand_2=1.0), "output": ResponseModel(sum=0.0, prod=-1.0)}
-        )
-        after_len = len(self.node["MISC"].examples.examples)
-        assert after_len - before_len == 1
-
     def test_select_examples(self):
         ground_truth = [
             {"input": '{"operand_1":3.0,"operand_2":4.0}', "output": '{"sum":7.0,"prod":12.0}'},
             {"input": '{"operand_1":5.0,"operand_2":6.0}', "output": '{"sum":11.0,"prod":30.0}'},
             {"input": '{"operand_1":0.0,"operand_2":8.0}', "output": '{"sum":8.0,"prod":0.0}'},
             {"input": '{"operand_1":-1.0,"operand_2":-2.0}', "output": '{"sum":-3.0,"prod":2.0}'},
-            {"input": '{"operand_1":-1.0,"operand_2":1.0}', "output": '{"sum":0.0,"prod":-1.0}'},
         ]
         cnt = 0
-        for example in self.node["MISC"].examples.select_examples():
+        input_variables={"size" : 3, "replace" : False}
+        for example in self.node["MISC"].examples.select_examples(input_variables=input_variables):
             for true_example in ground_truth:
                 if example == true_example:
                     cnt += 1
                     break
 
-        assert cnt == self.node["MISC"].examples.k
+        assert cnt == input_variables["size"]
 
     def test_to_langchain_context(self):
         ground_truth = [
@@ -156,16 +135,15 @@ class TestCustomExampleSelector:
             AIMessage(content='{"sum":8.0,"prod":0.0}', additional_kwargs={}, response_metadata={}),
             HumanMessage(content='{"operand_1":-1.0,"operand_2":-2.0}', additional_kwargs={}, response_metadata={}),
             AIMessage(content='{"sum":-3.0,"prod":2.0}', additional_kwargs={}, response_metadata={}),
-            HumanMessage(content='{"operand_1":-1.0,"operand_2":1.0}', additional_kwargs={}, response_metadata={}),
-            AIMessage(content='{"sum":0.0,"prod":-1.0}', additional_kwargs={}, response_metadata={}),
         ]
 
         cnt = 0
-        selected_examples = to_langchain_context(self.node["MISC"].examples)
+        input_variables={"size" : 3, "replace" : False}
+        selected_examples = to_langchain_context(self.node["MISC"].examples, input_variables=input_variables)
         for i in range(0, len(selected_examples), 2):
             for j in range(0, len(ground_truth), 2):
                 if selected_examples[i] == ground_truth[j] and selected_examples[i + 1] == ground_truth[j + 1]:
                     cnt += 1
                     break
 
-        assert cnt == self.node["MISC"].examples.k
+        assert cnt == input_variables["size"]
