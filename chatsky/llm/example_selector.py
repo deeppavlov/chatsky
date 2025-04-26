19493f3e5281408fa0738dbdaca6f8d1c6e5a873
Kiriltsev Daniil <3layerperceptron@gmail.com>
2025-04-26 15:26:33 +0300

    imports have been formatted

--- chatsky/llm/example_selector.py
+++ chatsky/llm/example_selector.py
@@ -4,28 +4,27 @@ Example selection.
 This module provides support for example guided generation.
 """
 
-from typing import List, Dict, Any
-
-from pydantic import BaseModel, Field, RootModel
+from typing import Any, Dict, List
 
 from langchain_core.example_selectors.base import BaseExampleSelector
-from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.messages import AIMessage, HumanMessage
+from pydantic import BaseModel, Field, RootModel
 
 
 class Example(BaseModel):
-    '''Class that holds an example'''
+    """Class that holds an example"""
 
-    input : str | BaseModel
-    '''input may be in form of a string or a custom pydantic model derived from the BaseModel'''
+    input: str | BaseModel
+    """input may be in form of a string or a custom pydantic model derived from the BaseModel"""
     output: str | BaseModel
-    '''output may be in form of a string or a custom pydantic model derived from the BaseModel'''
+    """output may be in form of a string or a custom pydantic model derived from the BaseModel"""
 
 
 def to_langchain_context(example_selector: BaseExampleSelector) -> None:
 
     result = []
     for example in example_selector.select_examples():
-        
+
         result.append(HumanMessage(content=example["input"]))
         result.append(AIMessage(content=example["output"]))
 
@@ -33,14 +32,17 @@ def to_langchain_context(example_selector: BaseExampleSelector) -> None:
 
 
 class StaticExampleSelector(BaseExampleSelector, RootModel):
-		
-        root: List[Example]
 
-        def add_example(self, example: Example) -> Any:
-            self.root.append(Example.model_validate(example))
-        
-        def unpack_model(self, example_part: str | BaseModel) -> str:
-            return str(example_part.model_dump_json()) if isinstance(example_part, BaseModel) else example_part
-        
-        def select_examples(self) -> List[Dict[str, str]]:
-            return [{"input": self.unpack_model(example.input), "output": self.unpack_model(example.output)} for example in self.root]
+    root: List[Example]
+
+    def add_example(self, example: Example) -> Any:
+        self.root.append(Example.model_validate(example))
+
+    def unpack_model(self, example_part: str | BaseModel) -> str:
+        return str(example_part.model_dump_json()) if isinstance(example_part, BaseModel) else example_part
+
+    def select_examples(self) -> List[Dict[str, str]]:
+        return [
+            {"input": self.unpack_model(example.input), "output": self.unpack_model(example.output)}
+            for example in self.root
+        ]
